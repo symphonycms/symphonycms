373357fb7a08300ffd6241adeb1bb3e7ce357426
Rowan Lewis <rowanlewis@rowan.randb.com.au>
2008-12-03 18:24:18 +1000

    Fixed bug #250 and rewrote buildDSRetrivalSQL to sanitize user input.

--- symphony/lib/toolkit/fields/field.taglist.php
+++ symphony/lib/toolkit/fields/field.taglist.php
@@ -3,6 +3,7 @@
 	
 	
 	Class fieldTagList extends Field{
+		protected $_key = 0;
 		
 		function __construct(&$parent){
 			parent::__construct($parent);
@@ -202,34 +203,61 @@
 			);
 		}		
 
-		function buildDSRetrivalSQL($data, &$joins, &$where, $andOperation=false){
-			
+		public function buildDSRetrivalSQL($data, &$joins, &$where, $andOperation = false) {
 			$field_id = $this->get('id');
 			
-			if(self::isFilterRegex($data[0])):
+			if (self::isFilterRegex($data[0])) {
+				$this->_key++;
+				$pattern = str_replace('regexp:', '', $this->Database->cleanValue($data[0]));
+				$joins .= "
+					LEFT JOIN
+						`tbl_entries_data_{$field_id}` AS t{$field_id}{$this->_key}
+						ON (e.id = t{$field_id}{$this->_key}.entry_id)
+				";
+				$where .= "
+					AND (
+						t{$field_id}{$this->_key}.value REGEXP '{$pattern}'
+						OR t{$field_id}{$this->_key}.handle REGEXP '{$pattern}')
+				";
 				
-				$pattern = str_replace('regexp:', '', $data[0]);
-				$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id` ON (`e`.`id` = `t$field_id`.entry_id) ";
-				$where .= " AND (`t$field_id`.value REGEXP '$pattern' OR `t$field_id`.handle REGEXP '$pattern') ";
-
-			
-			elseif($andOperation):
-			
-				foreach($data as $key => $bit){
-					$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id$key` ON (`e`.`id` = `t$field_id$key`.entry_id) ";
-					$where .= " AND (`t$field_id$key`.value = '$bit' OR `t$field_id$key`.handle = '$bit') ";
+			} elseif ($andOperation) {
+				foreach ($data as $value) {
+					$this->_key++;
+					$value = $this->Database->cleanValue($value);
+					$joins .= "
+						LEFT JOIN
+							`tbl_entries_data_{$field_id}` AS t{$field_id}{$this->_key}
+							ON (e.id = t{$field_id}{$this->_key}.entry_id)
+					";
+					$where .= "
+						AND (
+							t{$field_id}{$this->_key}.value = '$value'
+							OR t{$field_id}{$this->_key}.handle = '$value')
+					";
 				}
-							
-			else:
-			
-				$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id` ON (`e`.`id` = `t$field_id`.entry_id) ";
-				$where .= " AND (`t$field_id`.value IN ('".@implode("', '", $data)."') OR `t$field_id`.handle IN ('".@implode("', '", $data)."')) ";
-						
-			endif;
+				
+			} else {
+				if (!is_array($data)) $data = array($data);
+				
+				foreach ($data as &$value) {
+					$value = $this->Database->cleanValue($value);
+				}
+				
+				$this->_key++;
+				$data = implode("', '", $data);
+				$joins .= "
+					LEFT JOIN
+						`tbl_entries_data_{$field_id}` AS t{$field_id}{$this->_key}
+						ON (e.id = t{$field_id}{$this->_key}.entry_id)
+				";
+				$where .= "
+					AND (
+						t{$field_id}{$this->_key}.value IN ('{$data}')
+						OR t{$field_id}{$this->_key}.handle IN ('{$data}'))
+				";
+			}
 			
 			return true;
-			
 		}
-
 	}
 
