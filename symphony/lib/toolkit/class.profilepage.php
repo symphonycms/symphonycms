486b558f60aeb2b3ec61ba836d5adfd8cd93b268
pointybeard <alistair@symphony21.com>
2008-12-08 21:46:09 +1000

    Updated profile page markup.

--- symphony/lib/toolkit/class.profilepage.php
+++ symphony/lib/toolkit/class.profilepage.php
@@ -21,6 +21,19 @@
 			return $ul;
 		}
 		
+		private static function __appendNavigationItem($name, $link, $active=false){
+			
+			$li = new XMLElement('li');
+			$anchor = Widget::Anchor($name,  $link);
+			if($active == true){
+				$anchor->setAttribute('class', 'active');
+			}			
+			$li->appendChild($anchor);
+			
+			return $li;
+						
+		}
+				
 		function generate($page, $profiler, $database){
 			
 			$this->addHeaderToPage('Content-Type', 'text/html; charset=UTF-8');
@@ -44,128 +57,133 @@
 			
 			$jump = new XMLElement('ul', NULL, array('id' => 'jump'));
 			
-			## General
-			$records = $profiler->retrieveGroup('General');
+			$records = array();
 			
-			if(is_array($records) && !empty($records)){
-				$li = new XMLElement('li');
-				$li->appendChild(Widget::Anchor('General Details',  '#general')); 
-				$jump->appendChild($li);
-				
-				$dl = new XMLElement('dl', NULL, array('id' => 'general'));
-				
-				foreach($records as $r){
-					$dl->appendChild(new XMLElement('dt', $r[0]));
-					$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));				
-				}
-				
-				$this->Body->appendChild($dl);
-			
-			}
-			###
-
-			
-			## Data sources
-			$records = $profiler->retrieveGroup('Datasource');			
-			$ds_total = 0;
-			
-			if(is_array($records) && !empty($records)){
-				$li = new XMLElement('li');
-				$li->appendChild(Widget::Anchor('Data Source Execution',  '#data-sources')); 
-				$jump->appendChild($li);
-				
-				$dl = new XMLElement('dl', NULL, array('id' => 'data-sources'));
-				
-				foreach($records as $r){
-					$dl->appendChild(new XMLElement('dt', $r[0]));
-					$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));
-					$ds_total += $r[1];			
-				}
-				
-				$this->Body->appendChild($dl);
-			
-			}			
-			
-			###
-
-			## Events
-			$records = $profiler->retrieveGroup('Event');			
-			$event_total = 0;
-			
-			if(is_array($records) && !empty($records)){
-				$li = new XMLElement('li');
-				$li->appendChild(Widget::Anchor('Event Execution',  '#events')); 
-				$jump->appendChild($li);
-				
-				$dl = new XMLElement('dl', NULL, array('id' => 'events'));
-				
-				foreach($records as $r){
-					$dl->appendChild(new XMLElement('dt', $r[0]));
-					$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));
-					$event_total += $r[1];			
-				}
-				
-				$this->Body->appendChild($dl);
-			
-			}			
-			
-			###
-			
-			## Full Page Render Statistics		
 			$dbstats = $database->getStatistics();
+	
+			$profile_group = (strlen(trim($_GET['profile'])) == 0 ? 'general' : $_GET['profile']);	
 			
-			$xml_generation = $profiler->retrieveByMessage('XML Generation');
-			$xsl_transformation = $profiler->retrieveByMessage('XSLT Transformation');
-			
-			$records = array(
-				
-				array('Total Database Queries', $dbstats['queries'], NULL, NULL, false),
-				array('Slow Queries (> 0.09s)', count($dbstats['slow-queries']), NULL, NULL, false),
-				array('Total Time Spent on Queries', $dbstats['total-query-time']),
-				array('Time Triggering All Events', $event_total),
-				array('Time Running All Data Sources', $ds_total),
-				array('XML Generation Function', $xml_generation[1]),
-				array('XSLT Generation', $xsl_transformation[1]),
-				array('Output Creation Time', $profiler->retrieveTotalRunningTime()),
-			);
-			
-			$li = new XMLElement('li');
-			$li->appendChild(Widget::Anchor('Full Page Render Statistics',  '#render-statistics')); 
-			$jump->appendChild($li);
-			
-			$dl = new XMLElement('dl', NULL, array('id' => 'render-statistics'));
-			
-			foreach($records as $r){
-				$dl->appendChild(new XMLElement('dt', $r[0]));
-				$dl->appendChild(new XMLElement('dd', $r[1] . (isset($r[4]) && $r[4] == false ? '' : ' s')));
+			$records['general'] = $profiler->retrieveGroup('General');
+			if(is_array($records['general']) && !empty($records['general'])){
+				$jump->appendChild(self::__appendNavigationItem('General Details', '?profile=general', ($profile_group == 'general')));
 			}
 			
-			$this->Body->appendChild($dl);						
-			
-			###
+			$records['data-sources'] = $profiler->retrieveGroup('Datasource');	
+			if(is_array($records['data-sources']) && !empty($records['data-sources'])){
+				$jump->appendChild(self::__appendNavigationItem('Data Source Execution', '?profile=data-sources', ($profile_group == 'data-sources')));
+			}
 
-			## Slow Queries
-			if(is_array($dbstats['slow-queries']) && !empty($dbstats['slow-queries'])){
-				
-				$records = array();
-				
-				foreach($dbstats['slow-queries'] as $q) $records[] = array($q['time'], $q['query'], NULL, NULL, false);
-			
-				$li = new XMLElement('li');
-				$li->appendChild(Widget::Anchor('Slow Query Details',  '#slow-queries')); 
-				$jump->appendChild($li);
-			
-				$dl = new XMLElement('dl', NULL, array('id' => 'slow-queries'));
-			
-				foreach($records as $r){
-					$dl->appendChild(new XMLElement('dt', $r[0]));
-					$dl->appendChild(new XMLElement('dd', $r[1] . (isset($r[4]) && $r[4] == false ? '' : ' s')));
-				}
-			
-				$this->Body->appendChild($dl);						
+			$records['events'] = $profiler->retrieveGroup('Event');			
+			if(is_array($records['events']) && !empty($records['events'])){
+				$jump->appendChild(self::__appendNavigationItem('Event Execution', '?profile=events', ($profile_group == 'events')));
 			}
-			###
+
+			$jump->appendChild(self::__appendNavigationItem('Full Page Render Statistics', '?profile=render-statistics', ($profile_group == 'render-statistics')));
 			
+			if(is_array($dbstats['slow-queries']) && !empty($dbstats['slow-queries'])){
+				$records['slow-queries'] = array();
+				foreach($dbstats['slow-queries'] as $q) $records['slow-queries'][] = array($q['time'], $q['query'], NULL, NULL, false);
+
+				$jump->appendChild(self::__appendNavigationItem('Slow Query Details', '?profile=slow-queries', ($profile_group == 'slow-queries')));
+							
+			}
+
+			
+			switch($profile_group){
+
+				case 'general':
+				
+					$dl = new XMLElement('dl', NULL, array('id' => 'general'));
+			
+					foreach($records['general'] as $r){
+						$dl->appendChild(new XMLElement('dt', $r[0]));
+						$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));				
+					}
+			
+					$this->Body->appendChild($dl);
+							
+					break;
+					###
+
+			
+				case 'data-sources':
+							
+					$ds_total = 0;
+
+					$dl = new XMLElement('dl', NULL, array('id' => 'data-sources'));
+			
+					foreach($records['data-sources'] as $r){
+						$dl->appendChild(new XMLElement('dt', $r[0]));
+						$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));
+						$ds_total += $r[1];			
+					}
+			
+					$this->Body->appendChild($dl);
+									
+					break;
+					###
+					
+				case 'events':
+
+					$event_total = 0;
+
+					$dl = new XMLElement('dl', NULL, array('id' => 'events'));
+			
+					foreach($records['events'] as $r){
+						$dl->appendChild(new XMLElement('dt', $r[0]));
+						$dl->appendChild(new XMLElement('dd', $r[1] . ' s'));
+						$event_total += $r[1];			
+					}
+			
+					$this->Body->appendChild($dl);
+
+					break;
+					###
+			
+			
+				case 'render-statistics':		
+				
+			
+					$xml_generation = $profiler->retrieveByMessage('XML Generation');
+					$xsl_transformation = $profiler->retrieveByMessage('XSLT Transformation');
+			
+					$records = array(
+				
+						array('Total Database Queries', $dbstats['queries'], NULL, NULL, false),
+						array('Slow Queries (> 0.09s)', count($dbstats['slow-queries']), NULL, NULL, false),
+						array('Total Time Spent on Queries', $dbstats['total-query-time']),
+						array('Time Triggering All Events', $event_total),
+						array('Time Running All Data Sources', $ds_total),
+						array('XML Generation Function', $xml_generation[1]),
+						array('XSLT Generation', $xsl_transformation[1]),
+						array('Output Creation Time', $profiler->retrieveTotalRunningTime()),
+					);
+
+					$dl = new XMLElement('dl', NULL, array('id' => 'render-statistics'));
+			
+					foreach($records as $r){
+						$dl->appendChild(new XMLElement('dt', $r[0]));
+						$dl->appendChild(new XMLElement('dd', $r[1] . (isset($r[4]) && $r[4] == false ? '' : ' s')));
+					}
+			
+					$this->Body->appendChild($dl);						
+					break;
+					###
+
+				case 'slow-queries':
+
+					$dl = new XMLElement('dl', NULL, array('id' => 'slow-queries'));
+			
+					foreach($records as $r){
+						$dl->appendChild(new XMLElement('dt', $r[0]));
+						$dl->appendChild(new XMLElement('dd', $r[1] . (isset($r[4]) && $r[4] == false ? '' : ' s')));
+					}
+			
+					$this->Body->appendChild($dl);						
+					break;
+					###
+			}
+				
 			$this->Body->appendChild($jump);
 			
 			return parent::generate();
@@ -174,4 +192,3 @@
 		
 	}
 	
-?>
