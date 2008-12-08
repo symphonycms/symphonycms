f4be708d3f81cc79e290c86c3c0fdacac44fa35e
pointybeard <alistair@symphony21.com>
2008-12-08 18:15:55 +1000

    Automatic redirection no longer happens if the URL doesnt match exactly what is in the config. E.G www.site.com vs site.com

--- symphony/lib/core/class.frontend.php
+++ symphony/lib/core/class.frontend.php
@@ -18,28 +18,6 @@
 			$this->Profiler->sample('Engine Initialisation');
 
 			$this->_env = array();
-			
-			## To prevent users that are logged in from getting maintenance pages, ensure the URL matches
-			## the one speficied in the config file.
-			/*$url_bits = parse_url(URL);
-
-			if($_SERVER['HTTP_HOST'] != $url_bits['host'] && $_SERVER['HTTP_HOST'] != ($url_bits['host'] . ':' . $url_bits['port'])){
-				
-				##Clean up the query string
-				$query = str_replace('page=' . $_REQUEST['page'], NULL, $_SERVER['QUERY_STRING']);
-				$query = ltrim($query, '&');
-
-				##Reconstruct the correct URL and redirect them there
-				$destination = URL . '/' . $_REQUEST['page'] . '/' . ($query != '' ? "?$query" : '');
-				$destination = rtrim($destination, '/') . '/';
-
-				##Let the browser know its a 301 page
-				header('HTTP/1.1 301 Moved Permanently');
-				redirect($destination);
-				exit();
-				
-			}*/	
-			##
 		}
 
 		public function isLoggedIn(){
