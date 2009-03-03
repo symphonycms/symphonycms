938da4bd64e83c8b5bd7882dbfdb019c81a6cdd0
Alistair Kearney <alistair@21degrees.com.au>
2009-03-03 13:26:39 +1000

    [Issue #163] Unused 'DateTimeObj::getRelativeDate()' function removed

--- symphony/lib/core/class.datetimeobj.php
+++ symphony/lib/core/class.datetimeobj.php
@@ -1,11 +1,9 @@
 <?php
 
-		
-
 	Class DateTimeObj{
 
 		public static function setDefaultTimezone($timezone){
-			if(!@date_default_timezone_set($timezone)) trigger_error(E_USER_WARNING, "Invalid timezone '$timezone'");
+			if(!@date_default_timezone_set($timezone)) trigger_error(E_USER_WARNING, "Invalid timezone '{$timezone}'");
 		}
 		
 		public static function getGMT($format, $timestamp=NULL){
@@ -13,7 +11,7 @@
 		}
 		
 		public static function get($format, $timestamp=NULL, $timezone=NULL){
-			if(!$timestamp || $timestamp == 'now') $timestamp = time(); //"@$timestamp";
+			if(!$timestamp || $timestamp == 'now') $timestamp = time();
 			if(!$timezone) $timezone = date_default_timezone_get();
 			
 			$current_timezone = date_default_timezone_get();
@@ -25,31 +23,6 @@
 			
 			return $ret;
 			
-			//$dateTime = new DateTime($timestamp);
-			//$dateTime->setTimeZone(new DateTimeZone($timezone));
-			//return $dateTime->format($format);
-		}
-		
-		public static function getRelativeDate($timestamp, $timezone=NULL, $now=NULL){
-			
-			$date = strtotime(self::get('c', $timestamp));
-			$now = strtotime(self::get('c', $now));
-			
-			$diff = $now - $date;
-			
-			if($diff < (60 * 60)) return 'Less than an hour ago';
-			elseif($diff < (24 * 60 * 60)){
-				$hours = round(($diff * (1/60)) * (1/60));
-				return $hours . ' hour' . ($hours > 1 ? 's' : NULL) . ' ago';
-			}
-			
-			$age = ($diff * (1/60) * (1/60));
-
-			if($age >= 24 && $age < 48) return 'Yesterday';
-			elseif(ceil($age * (1/24)) < 7) return ceil($age * (1/24)) . ' days ago';
-			
-			return ceil(($age * (1/24)) * (1/7)) . ' week' . (ceil(($age * (1/24)) * (1/7)) > 1 ? 's' : '') . ' ago';			
-
 		}
 	
 	}
