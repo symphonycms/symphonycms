088282d80cd417ab550a6396183ed928c1a586ce
Alistair Kearney <alistair@21degrees.com.au>
2009-03-05 14:19:57 +1000

    Datasource params that use DS output from multiple DS's will correctly generate. Priviously it would only consider the first -XXX value and ignore others, so '{-XXX},{-YYY}...' would not work properly.

--- symphony/lib/toolkit/class.datasource.php
+++ symphony/lib/toolkit/class.datasource.php
@@ -93,7 +93,7 @@
 		function __noRecordsFound(){
 			return new XMLElement('error', __('No records found.'));
 		}
-		
+
 		function __processParametersInString($value, $env, $includeParenthesis=true, $escape=false){
 			if(trim($value) == '') return NULL;
 
@@ -121,8 +121,8 @@
 						$replacement = $this->__findParameterInEnv($param, $env);
 						
 						if(is_array($replacement)){
-							if(count($replacement) > 1) return $replacement;
-							$replacement = end($replacement);
+							if(count($replacement) > 1) $replacement = implode(',', $replacement);
+							else $replacement = end($replacement);
 						}
 						
 						if(!empty($replacement)) break;
