91e432a59d9bb8a63d7119ca19bc4fb2df93b602
ahwayakchih <ahwayakchih@gmail.com>
2009-03-07 20:13:31 +0800

    Fix rendering total time of events and datasources on 'Full Page Render Statistics' profile page.
    Signed-off-by: Alistair Kearney <alistair@21degrees.com.au>

--- symphony/lib/toolkit/class.profilepage.php
+++ symphony/lib/toolkit/class.profilepage.php
@@ -155,7 +155,13 @@
 			
 					$xml_generation = $profiler->retrieveByMessage('XML Generation');
 					$xsl_transformation = $profiler->retrieveByMessage('XSLT Transformation');
-			
+
+					$event_total = 0;
+					foreach($records['events'] as $r) $event_total += $r[1];
+
+					$ds_total = 0;
+					foreach($records['data-sources'] as $r) $ds_total += $r[1];
+
 					$records = array(
 				
 						array(__('Total Database Queries'), $dbstats['queries'], NULL, NULL, false),
