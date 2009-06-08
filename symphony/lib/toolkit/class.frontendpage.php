e0da2bc81fb2dc04a588006b771df9df6c2f398a
Nick Dunn <nickdunn@nick-dunn-2.local>
2009-06-08 17:19:52 +0800

    Additional delegate for once the entire param pool has been resolved. Existing "FrontendParamsResolve" executes before Data Source output parameters have been created.
    Signed-off-by: Rowan Lewis <rowan@pixelcarnage.com>

--- symphony/lib/toolkit/class.frontendpage.php
+++ symphony/lib/toolkit/class.frontendpage.php
@@ -239,6 +239,12 @@
 				}
 			}
 			
+			####
+			# Delegate: FrontendParamsPostResolve
+			# Description: Access to the resolved param pool, including additional parameters provided by Data Source outputs
+			# Global: Yes
+			$this->ExtensionManager->notifyMembers('FrontendParamsPostResolve', '/frontend/', array('params' => $this->_param));
+			
 			## TODO: Add delegate for adding/removing items in the params
 
 			$xsl = '<?xml version="1.0" encoding="UTF-8"?>
