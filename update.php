2da454785270601747429531a91497003b2e149f
Allen Chang <Allen@Allens-iMac.local>
2009-06-24 14:10:29 +1000

    updater update

--- update.php
+++ update.php
@@ -44,8 +44,8 @@
 	error_reporting(E_ALL ^ E_NOTICE);
 	set_error_handler('__errorHandler');
 
-	define('kBUILD', '375');
-	define('kVERSION', '2.0.2');
+	define('kBUILD', '515');
+	define('kVERSION', '2.0.3');
 	define('kINSTALL_ASSET_LOCATION', './symphony/assets/installer');	
 	define('kINSTALL_FILENAME', basename(__FILE__));
 
