f63ecb65a2d7a436fef8ef7ff7238b000e4ad297
Rowan Lewis <rowan@randb.com.au>
2010-02-26 14:51:14 +1000

    Added Navigation Data Source Template.

---
+++ extensions/ds_template_navigation/extension.driver.php
@@ -0,0 +1,76 @@
+<?php
+	
+	class Extension_DS_Template_Navigation extends Extension {
+		public function about() {
+			return array(
+				'name'			=> 'Data Source Template: Navigation',
+				'version'		=> '1.0.0',
+				'release-date'	=> '2010-02-26',
+				'author'		=> array(
+					'name'			=> 'Rowan Lewis',
+					'website'		=> 'http://rowanlewis.com/',
+					'email'			=> 'me@rowanlewis.com'
+				),
+				'description'	=> 'Create data sources from page navigation data.'
+			);
+		}
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'NewDataSourceAction',
+					'callback'	=> 'action'
+				),
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'NewDataSourceForm',
+					'callback'	=> 'form'
+				),
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'EditDataSourceAction',
+					'callback'	=> 'action'
+				),
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'EditDataSourceForm',
+					'callback'	=> 'form'
+				)
+			);
+		}
+		
+		protected function getTemplate() {
+			$file = EXTENSIONS . '/ds_template_navigation/templates/datasource.php';
+			
+			if (!file_exists($file)) {
+				throw new Exception(sprintf("Unable to find template '%s'.", $file));
+			}
+			
+			return file_get_contents($file);
+		}
+		
+		public function action($context = array()) {
+			/*
+			$context = array(
+				'type'		=> '',			// Type of datasource
+				'data'		=> array(),		// Array of post data
+				'errors'	=> null			// Instance of MessageStack to be filled with errors
+			);
+			*/
+		}
+		
+		public function form($context = array()) {
+			/*
+			$context = array(
+				'type'		=> '',			// Type of datasource
+				'data'		=> array(),		// Array of post data
+				'errors'	=> null			// Instance of MessageStack to be checked for errors
+				'wrapper'	=> null			// XMLElement so additional fieldsets can be added
+			);
+			*/
+			$template = $this->getTemplate();
+		}
+	}
+	
+?>
---
+++ extensions/ds_template_navigation/templates/datasource.php
@@ -0,0 +1,28 @@
+<?php
+	
+	require_once EXTENSIONS . '/ds_template_navigation/lib/navigationdatasource.php';
+	
+	class DataSource%s extends UsersDataSource {
+		public function about() {
+			return array(
+				'name'			=> %s,
+				'author'		=> array(
+					'name'			=> %s,
+					'website'		=> %s,
+					'email'			=> %s
+				),
+				'version'		=> %s,
+				'release-date'	=> %s
+			);	
+		}
+		
+		public $dsParamROOTELEMENT = %s;
+		public $dsParamORDER = %s;
+		public $dsParamREDIRECTONEMPTY = %s;
+		
+		public function allowEditorToParse() {
+			return true;
+		}
+	}
+	
+?>
