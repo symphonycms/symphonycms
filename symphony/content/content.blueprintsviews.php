5fc253e1977619161dcda1277720a6263e642964
craig zheng <cz@mongrl.com>
2010-03-02 13:29:00 -0500

    Convert View config/template buttons to tabs via appendViewOptions()

--- symphony/content/content.blueprintsviews.php
+++ symphony/content/content.blueprintsviews.php
@@ -183,7 +183,16 @@
 					$filename
 				)
 			));
-			$this->appendSubheading(__($filename ? $filename : __('Untitled')), Widget::Anchor(__('Edit Configuration'), ADMIN_URL . '/blueprints/views/edit/' . $view_pathname . '/', __('Edit View Configuration'), 'button'));
+			$this->appendSubheading(__($filename ? $filename : __('Untitled')));
+			
+			$viewoptions = array(
+				'subnav'	=> array(
+					__('Configuration')			=>	ADMIN_URL . '/blueprints/views/edit/' . $view_pathname . '/',
+					__('Template')				=>	Administration::instance()->getCurrentPageURL()
+				)
+			);
+			
+			$this->appendViewOptions($viewoptions);
 			
 			if(!empty($_POST)){
 				$view->template = $_POST['fields']['template'];
@@ -413,9 +422,16 @@
 			if($existing instanceof View){
 				$template_name = $fields['handle'];
 				$this->appendSubheading(
-					__($title ? $title : __('Untitled')), 
-					Widget::Anchor(__('Edit Template'), sprintf('%s/blueprints/views/template/%s/', ADMIN_URL, $view_pathname), __('Edit View Template'), 'button')
+					__($title ? $title : __('Untitled'))
 				);
+				$viewoptions = array(
+					'subnav'	=> array(
+						__('Configuration')		=>	Administration::instance()->getCurrentPageURL(),
+						__('Template')			=>	sprintf('%s/blueprints/views/template/%s/', ADMIN_URL, $view_pathname)
+				)
+			);
+			
+			$this->appendViewOptions($viewoptions);
 			}
 			else {
 				$this->appendSubheading(($title ? $title : __('Untitled')));
