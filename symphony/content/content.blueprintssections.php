9ebb3f2669b29788d09bc390d9fae13af5317f32
Alistair Kearney <alistair@21degrees.com.au>
2010-04-14 12:55:52 +1000

    Changed query referencing 'section_id' to 'section'

--- symphony/content/content.blueprintssections.php
+++ symphony/content/content.blueprintssections.php
@@ -40,7 +40,7 @@
 							FROM
 								`tbl_entries` AS e
 							WHERE
-								e.section_id = '%s'
+								e.section = '%s'
 						",
 						array($s->handle)
 					);
@@ -54,7 +54,7 @@
 					$td2 = Widget::TableData(Widget::Anchor((string)$entry_count, ADMIN_URL . "/publish/{$s->handle}/"));
 					$td3 = Widget::TableData($s->{'navigation-group'});
 					
-					$td3->appendChild(Widget::Input('items['.$s->handle.']', 'on', 'checkbox'));
+					$td3->appendChild(Widget::Input("items[{$s->handle}]", 'on', 'checkbox'));
 					
 					// Add a row to the body array, assigning each cell to the row
 					$aTableBody[] = Widget::TableRow(array($td1, $td2, $td3));
