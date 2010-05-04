04e8c66c2c7dce8b53cd075c9a058548e0f3e305
Alistair Kearney <alistair@21degrees.com.au>
2010-05-04 11:44:55 +1000

    Moved cache preventing headers from bundle.php to index.php. Allows scripts to include the Symphony engine without getting unwanted headers set.

--- symphony/lib/boot/bundle.php
+++ symphony/lib/boot/bundle.php
@@ -13,11 +13,6 @@
 	}
 	
 	set_magic_quotes_runtime(0);
-
-	header('Expires: Mon, 12 Dec 1982 06:14:00 GMT');
-	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-	header('Cache-Control: no-cache, must-revalidate, max-age=0');
-	header('Pragma: no-cache');
 		
 	require_once(DOCROOT . '/symphony/lib/boot/func.utilities.php');	
 	require_once(DOCROOT . '/symphony/lib/boot/defines.php');
