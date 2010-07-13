7ad963a1940156ed6b9c622aff03d36b8cb9781e
Brendan Abbott <brendan@bloodbone.ws>
2010-07-13 10:04:04 +1000

    Ability for Cookies to be HttpOnly, Properly trigger the Session::destroy function, Don't write empty Sessions

--- symphony/lib/core/class.cookie.php
+++ symphony/lib/core/class.cookie.php
@@ -5,24 +5,26 @@
 	require_once(CORE . '/class.session.php');
 
 	Class Cookie{
-		
+
 		private $_index;
 		private $_session;
 
 		private $_timeout;
 		private $_path;
 		private $_domain;
-		
-		public function __construct($index, $timeout = 0, $path = '/', $domain = NULL) {
+		private $_httpOnly;
+
+		public function __construct($index, $timeout = 0, $path = '/', $domain = NULL, $httpOnly = false) {
 			$this->_index = $index;
 			$this->_timeout = $timeout;
 			$this->_path = $path;
 			$this->_domain = $domain;
+			$this->_httpOnly = $httpOnly;
 
 			// Symphony->__construct() creates Cookie before Database is created. So we need to start session AFTER Cookie is created.
 			$this->_session = false;
 		}
-		
+
 		public function set($name, $value) {
 			if (!$this->_session) {
 				$this->__init();
@@ -30,7 +32,7 @@
 
 			$_SESSION[$this->_index][$name] = $value;
 		}
-		
+
 		public function get($name) {
 			if (!$this->_session) {
 				$this->__init();
@@ -38,21 +40,23 @@
 
 			return $_SESSION[$this->_index][$name];
 		}
-		
+
 		public function expire() {
 			if (!$this->_session) {
 				$this->__init();
 			}
 
-			if(!is_array($_SESSION[$this->_index]) || empty($_SESSION[$this->_index])) return;
+			if(isset($_SESSION[$this->_index]) || !is_array($_SESSION[$this->_index])) return;
 
 			unset($_SESSION[$this->_index]);
+
+			session_destroy();
 		}
 
 		private function __init() {
 			if ($this->_session) return $this->_session;
 
-			$this->_session = Session::start($this->_timeout, $this->_path, $this->_domain);
+			$this->_session = Session::start($this->_timeout, $this->_path, $this->_domain, $this->_httpOnly);
 			if (!$this->_session) return false;
 
 			if (!isset($_SESSION[$this->_index])) $_SESSION[$this->_index] = array();
@@ -62,7 +66,7 @@
 
 			return $this->_session;
 		}
-	
+
 	}
 
 ?>
