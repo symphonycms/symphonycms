3cf67213c460dffb87c9fd434f941bae263d2e83
Nils Hörrmann <post@nilshoerrmann.de>
2010-11-02 19:33:45 +0100

    Fix merge issue in the installer and fix translation handling

--- install.php
+++ install.php
@@ -61,42 +61,29 @@
 		return true;
 	}
 
-		try{
-			Lang::load('./symphony/lib/lang/lang.%s.php', $lang);
-		}
-		catch(Exception $s){
-			return NULL;
-		}
-
-		define('__LANG__', $lang);
-		return $lang;
-	}
-
-
 	/***********************
 	         TESTS
 	************************/
 
 	// Check for PHP 5.2+
-
 	if(version_compare(phpversion(), '5.2', '<=')){
 
 		$code = '<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>Outstanding Requirements</title>
-		<link rel="stylesheet" type="text/css" href="'.kINSTALL_ASSET_LOCATION.'/main.css"/>
-		<script type="text/javascript" src="'.kINSTALL_ASSET_LOCATION.'/main.js"></script>
+		<title>' . __('Outstanding Requirements') . '</title>
+		<link rel="stylesheet" type="text/css" href="' . kINSTALL_ASSET_LOCATION . '/main.css"/>
+		<script type="text/javascript" src="' . kINSTALL_ASSET_LOCATION . '/main.js"></script>
 	</head>
 		<body>
-			<h1>Install Symphony <em>Version '.kVERSION.'</em></h1>
-			<h2>Outstanding Requirements</h2>
-			<p>Symphony needs the following requirements satisfied before installation can proceed.</p>
+			<h1>' . __('Install Symphony') .  '<em>' . __('Version') . ' ' . kVERSION)) . '</em></h1>
+			<h2>' . __('Outstanding Requirements') . '</h2>
+			<p>' . __('Symphony needs the following requirements satisfied before installation can proceed.') . '</p>
 
 			<dl>
-				<dt><abbr title="PHP: Hypertext Pre-processor">PHP</abbr> 5.2 or above</dt>
-				<dd>Symphony needs a recent version of <abbr title="PHP: Hypertext Pre-processor">PHP</abbr>.</dd>
+				<dt>' . __('%s 5.2 or above', array('<abbr title="PHP: Hypertext Pre-processor">PHP</abbr>')) . '</dt>
+				<dd>' . __('Symphony needs a recent version of %s.', array('<abbr title="PHP: Hypertext Pre-processor">PHP</abbr>')) . '</dd>
 			</dl>
 
 		</body>
@@ -114,14 +101,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>Outstanding Requirements</title>
-		<link rel="stylesheet" type="text/css" href="'.kINSTALL_ASSET_LOCATION.'/main.css"/>
-		<script type="text/javascript" src="'.kINSTALL_ASSET_LOCATION.'/main.js"></script>
+		<title>' . __('Outstanding Requirements') . '</title>
+		<link rel="stylesheet" type="text/css" href="' . kINSTALL_ASSET_LOCATION . '/main.css"/>
+		<script type="text/javascript" src="' . kINSTALL_ASSET_LOCATION . '/main.js"></script>
 	</head>
 		<body>
-			<h1>Install Symphony <em>Version '.kVERSION.'</em></h1>
-			<h2>Outstanding Requirements</h2>
-			<p>Symphony needs at least one language file to be present before installation can proceed.</p>
+			<h1>' . __('Install Symphony') .  '<em>' . __('Version') . ' ' . kVERSION)) . '</em></h1>
+			<h2>' . __('Outstanding Requirements') . '</h2>
+			<p>' . __('Symphony needs at least one language file to be present before installation can proceed.') . '</p>
 
 		</body>
 
@@ -138,14 +125,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>'.__('Missing File').'</title>
-		<link rel="stylesheet" type="text/css" href="'.kINSTALL_ASSET_LOCATION.'/main.css"/>
-		<script type="text/javascript" src="'.kINSTALL_ASSET_LOCATION.'/main.js"></script>
+		<title>' . __('Missing File') . '</title>
+		<link rel="stylesheet" type="text/css" href="' . kINSTALL_ASSET_LOCATION . '/main.css"/>
+		<script type="text/javascript" src="' . kINSTALL_ASSET_LOCATION . '/main.js"></script>
 	</head>
 		<body>
-			<h1>'.__('Install Symphony <em>Version %s</em>', array(kVERSION)).'</h1>
-			<h2>'.__('Missing File').'</h2>
-			<p>'.__('It appears that <code>install.sql</code> is either missing or not readable. This is required to populate the database and must be uploaded before installation can commence. Ensure that <code>PHP</code> has read permissions.').'</p>
+			<h1>' . __('Install Symphony') .  '<em>' . __('Version') . ' ' . kVERSION)) . '</em></h1>
+			<h2>' . __('Missing File') . '</h2>
+			<p>' . __('It appears that <code>install.sql</code> is either missing or not readable. This is required to populate the database and must be uploaded before installation can commence. Ensure that <code>PHP</code> has read permissions.') . '</p>
 
 		</body>
 
@@ -162,14 +149,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>'.__('Existing Installation').'</title>
-		<link rel="stylesheet" type="text/css" href="'.kINSTALL_ASSET_LOCATION.'/main.css"/>
-		<script type="text/javascript" src="'.kINSTALL_ASSET_LOCATION.'/main.js"></script>
+		<title>' . __('Existing Installation') . '</title>
+		<link rel="stylesheet" type="text/css" href="' . kINSTALL_ASSET_LOCATION . '/main.css"/>
+		<script type="text/javascript" src="' . kINSTALL_ASSET_LOCATION . '/main.js"></script>
 	</head>
 		<body>
-			<h1>'.__('Install Symphony <em>Version %s</em>', array(kVERSION)).'</h1>
-			<h2>'.__('Existing Installation').'</h2>
-			<p>'.__('It appears that Symphony has already been installed at this location.').'</p>
+			<h1>' . __('Install Symphony') .  '<em>' . __('Version') . ' ' . kVERSION)) . '</em></h1>
+			<h2>' . __('Existing Installation') . '</h2>
+			<p>' . __('It appears that Symphony has already been installed at this location.') . '</p>
 
 		</body>
 
