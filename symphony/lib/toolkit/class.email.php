47b7bd4d641565864f0975741aeb7762b9f96b7a
michael-e <info@michael-eichelsdoerfer.de>
2010-11-12 20:06:19 +0100

    Introducing quoted-printable encoding.
    We used to simply wordwrap message lines without encoding anything, which does not comply with RFC standards. Using a comprehensive quoted-printable-encoding method we are finally enhancing email quality!

--- symphony/lib/toolkit/class.email.php
+++ symphony/lib/toolkit/class.email.php
@@ -75,6 +75,7 @@
 				'X-Priority'	=> '3',
 				'MIME-Version'	=> '1.0',
 				'Content-Type'	=> 'text/plain; charset=UTF-8',
+				'Content-Transfer-Encoding' => 'quoted-printable',
 			);
 
 			foreach($default_headers as $key => $value){
@@ -90,7 +91,13 @@
 				$headers[] = sprintf('%s: %s', $header, $value);
 			}
 
-			$result = mail($this->recipient, $this->subject, @wordwrap($this->message, 70), @implode("\r\n", $headers) . "\r\n", "-f{$this->sender_email_address}");
+			// Quoted-printable encoding of the message body (RFC 2045)
+			$this->message = $this->quoted_printable_encode($this->message);
+
+			// The PHP mail() function requires 'linefeed only' for the message body;
+			$this->message = str_replace("\r\n", "\n", $this->message);
+
+			$result = mail($this->recipient, $this->subject, $this->message, @implode("\r\n", $headers) . "\r\n", "-f{$this->sender_email_address}");
 
 			if($result !== true){
 				throw new EmailException('Email failed to send. Please check input.');
@@ -129,6 +136,111 @@
 		        }
 		    }
 		    return $input;
+		}
+
+		/**
+		 * quoted-printable body encoding
+		 *
+		 * method to quoted-printable-encode an email message body according to RFC 2045;
+		 * includes line wrapping according to RFC 822/2822 (using CRLF) as required by RFC 2045;
+		 * for PHP >= 5.3 we might use the built-in quoted_printable_encode() function instead,
+		 * but we should keep in mind that the (human-)readabilty of its output is much worse;
+		 *
+		 * http://php.net/manual/en/function.quoted-printable-encode.php
+		 * http://php.net/manual/en/function.quoted-printable-decode.php
+		 *
+		 * @param string $input
+		 * @param string $line_max; maximum line length
+		 * @return string
+		 */
+		public static function quoted_printable_encode($input, $line_max = 76) {
+			$hex = array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F');
+			$lines = preg_split("/(?:\r\n|\r|\n)/", $input);
+			$linebreak = "\r\n";
+			$softlinebreak = "=\r\n";
+
+			$line_max = $line_max - strlen($softlinebreak);
+
+			$output = "";
+			$cur_conv_line = "";
+			$length = 0;
+			$whitespace_pos = 0;
+			$addtl_chars = 0;
+
+			// iterate lines
+			for ($j=0; $j < count($lines); $j++) {
+				$line = $lines[$j];
+				$linlen = strlen($line);
+
+				// iterate chars
+				for ($i = 0; $i < $linlen; $i++) {
+					$c = substr($line, $i, 1);
+					$dec = ord($c);
+					$length++;
+
+					// watch out for spaces
+					if ($dec == 32) {
+						// space occurring at end of line, need to encode
+						if (($i == ($linlen - 1))) {
+							$c = "=20";
+							$length += 2;
+						}
+						$addtl_chars = 0;
+						$whitespace_pos = $i;
+					// characters to be encoded
+					} elseif ( ($dec == 61) || ($dec < 32 ) || ($dec > 126) ) {
+						$h2 = floor($dec/16);
+						$h1 = floor($dec%16);
+						$c = "=" . $hex["$h2"] . $hex["$h1"];
+						$length += 2;
+						$addtl_chars += 2;
+						$enc_pos = $i;
+					}
+
+					// length for wordwrap exceeded, get a newline into the text
+					if ($length >= $line_max) {
+						$cur_conv_line .= $c;
+
+						// read only up to the whitespace for the current line
+						$whitesp_diff = $i - $whitespace_pos + $addtl_chars;
+						$enc_diff = $i - $enc_pos + $addtl_chars;
+						/*
+						 * the text after the whitespace will have to
+						 * be read again ( + any additional characters
+						 * that came into existence as a result of the
+						 * encoding process after the whitespace)
+						 *
+						 * Also, do not start at 0, if there was *no*
+						 * whitespace in the whole line
+						 */
+						if (($i + $addtl_chars) > $whitesp_diff) {
+							$output .= substr($cur_conv_line, 0,
+							(strlen($cur_conv_line) - $whitesp_diff)) . $softlinebreak;
+							$i = $i - $whitesp_diff + $addtl_chars;
+						} else {
+							$output .= $cur_conv_line . $softlinebreak;
+						}
+
+						$cur_conv_line = "";
+						$length = 0;
+						$whitespace_pos = 0;
+					} else {
+						// length for wordwrap not reached, continue reading
+						$cur_conv_line .= $c;
+					}
+				}
+
+				$length = 0;
+				$whitespace_pos = 0;
+				$output .= $cur_conv_line;
+				$cur_conv_line = "";
+
+				if ($j <= count($lines)-1) {
+					$output .= $linebreak;
+				}
+			}
+
+			return trim($output);
 		}
 
 		public function __set($name, $value){
