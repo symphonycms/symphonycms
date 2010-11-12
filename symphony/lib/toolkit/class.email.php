d02755f439cae08cd627e1b5c549a8c495dff3e3
michael-e <info@michael-eichelsdoerfer.de>
2010-11-12 20:31:51 +0100

    Fixed typo in Return-path.

--- symphony/lib/toolkit/class.email.php
+++ symphony/lib/toolkit/class.email.php
@@ -63,7 +63,7 @@
 			$this->sender_name = self::encodeHeader($this->sender_name, 'UTF-8');
 
 			$default_headers = array(
-				'Return-Path'	=> "<{$this->sender_email_address}>",
+				'Return-path'	=> "<{$this->sender_email_address}>",
 				'From'			=> "{$this->sender_name} <{$this->sender_email_address}>",
 		 		'Reply-To'		=> $this->sender_email_address,
 				'Message-ID'	=> sprintf('<%s@%s>', md5(uniqid(time())), $_SERVER['SERVER_NAME']),
