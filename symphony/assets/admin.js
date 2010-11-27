3ecd33e6cd706b96aea1cde27cbcad3a574b1edd
Nils Hörrmann <post@nilshoerrmann.de>
2010-11-27 20:44:26 +0100

    Add picker for email-gateways preferences

--- symphony/assets/admin.js
+++ symphony/assets/admin.js
@@ -194,7 +194,7 @@ var Symphony = {};
 	
 
 /*-----------------------------------------------------------------------------
-	DOM interactions
+	Things to be cleaned up
 -----------------------------------------------------------------------------*/
 
 	// Sortable lists
@@ -551,7 +551,35 @@ var Symphony = {};
 		});
 		Symphony.Message.timer();
 
+		/**
+		 * Selection picker, show and hide elements based on select box values
+		 */
+		var pickers = $('.picker');
+		var selectables = $('.selectable');
+		
+		pickers.each(function() {
+			var picker = $(this);
+			var select = picker.find('select');
+			var options = select.find('option');
 
+			// Multiple items
+			if(options.size() > 1) {
+				options.each(function() {
+					selectables.filter('#' + $(this).val()).hide();
+				});
+				select.click(function() {
+					selectables.hide().filter('#' + $(this).val()).show();
+				}).click();
+			}
+			
+			// Single item
+			else {
+				picker.hide();
+				selectables.filter('#' + select.val()).removeClass('selectable');
+			}
+
+		});
+		
 	});
 	
 })(jQuery.noConflict());
