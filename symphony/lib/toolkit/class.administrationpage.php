5c577242b6704de063b5bcb30d27c1730922fbc2
Nils Hörrmann <post@nilshoerrmann.de>
2010-11-28 13:15:07 +0100

    Don't forget to add symphony.picker.js to backend pages

--- symphony/lib/toolkit/class.administrationpage.php
+++ symphony/lib/toolkit/class.administrationpage.php
@@ -67,6 +67,7 @@
 			$this->addScriptToHead(URL . '/symphony/assets/symphony.orderable.js', 61);
 			$this->addScriptToHead(URL . '/symphony/assets/symphony.duplicator.js', 62);
 			$this->addScriptToHead(URL . '/symphony/assets/symphony.tags.js', 63);
+			$this->addScriptToHead(URL . '/symphony/assets/symphony.picker.js', 63);
 			$this->addScriptToHead(URL . '/symphony/assets/admin.js', 70);
 			$this->addElementToHead(
 				new XMLElement(
