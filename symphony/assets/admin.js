d0d48a72aaa3e117b928b91b5c11e5d1d64e35d7
Nils Hörrmann <post@nilshoerrmann.de>
2010-12-09 17:58:45 +0100

    Update XSLT utility selection

--- symphony/assets/admin.js
+++ symphony/assets/admin.js
@@ -460,7 +460,27 @@ var Symphony = {};
 			time.html(time.html().replace(Symphony.Language.get('at') + ' ', ''));
 		});
 		Symphony.Message.timer();
+
+		// XSLT utilities
+		$('textarea').blur(function() {
+			var source = $(this).val(),
+				utilities = $('#utilities li');
 				
+			// Remove current selection
+			utilities.removeClass('selected');
+				
+			// Get utitities names
+			utilities.find('a').each(function() {
+				var utility = $(this),
+					expression = new RegExp('href=["\']?(?:\\.{2}/utilities/)?' + utility.text());
+				
+				// Check for utility occurrences
+				if(expression.test(source)) {
+					utility.parent().addClass('selected');
+				}
+			});
+		}).blur();
+						
 	});
 
 
@@ -529,18 +549,7 @@ var Symphony = {};
 				return i > 0 && !o.hasClass('confirm') || confirm(t);
 			});
 		}
-
-		// XSLT utilities
-		$('#utilities a').each(function() {
-			var a = $(this.parentNode),
-			    r = new RegExp('href=["\']?(?:\\.{2}/utilities/)?' + $(this).text());
-
-			$('textarea').blur(function() {
-				a[r.test(this.value) ? 'addClass' : 'removeClass']('selected');
-			});
-		});
-
-		$('textarea').blur();
+		
 
 		// Data source switcheroo
 		$('select.filtered > optgroup').each(function() {
