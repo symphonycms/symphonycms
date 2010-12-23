cd5dd48e816e5c9e4bd4ede4410d3d60d470d563
Brendan Abbott <brendan@bloodbone.ws>
2010-12-23 11:46:19 +1000

    Cleanup of Blueprints Components

--- symphony/content/content.blueprintscomponents.php
+++ symphony/content/content.blueprintscomponents.php
@@ -1,51 +1,54 @@
 <?php
 
+	/**
+	 * @package content
+	 */
+
+	/**
+	 * The Components page lists Datasources, Events and XSLT Utilities
+	 * in this Symphony installation
+	 */
 	require_once(TOOLKIT . '/class.administrationpage.php');
 	require_once(TOOLKIT . '/class.eventmanager.php');
 	require_once(TOOLKIT . '/class.datasourcemanager.php');
 
 	Class contentBlueprintsComponents extends AdministrationPage{
 
-		function __construct(&$parent){
+		public function __construct(&$parent){
 			parent::__construct($parent);
 			$this->setPageType('forms');
 			$this->setTitle(__('%1$s &ndash; %2$s', array(__('Symphony'), __('Components'))));
 		}
 
-		function view(){
-		
+		public function view(){
+
 			$this->appendSubheading(__('Components'));
-					
+
 			$utilities = General::listStructure(UTILITIES, array('xsl'), false, 'asc', UTILITIES);
 			$utilities = $utilities['filelist'];
-			
+
 			$ul = new XMLElement('ul');
 			$ul->setAttribute('id', 'components');
 			$ul->setAttribute('class', 'triple group');
 
 			### EVENTS ###
-			
+
 			$EventManager = new EventManager($this->_Parent);
 			$events = $EventManager->listAll();
-			
+
 			$li = new XMLElement('li');
 			$h3 = new XMLElement('h3', __('Events'));
-			$h3->appendChild(Widget::Anchor(__('Create New'), URL . '/symphony/blueprints/events/new/', __('Create a new event'), 'create button'));
+			$h3->appendChild(Widget::Anchor(__('Create New'), SYMPHONY_URL . '/blueprints/events/new/', __('Create a new event'), 'create button'));
 			$li->appendChild($h3);
 
 			$list = new XMLElement('ul');
 			$list->setSelfClosingTag(false);
-			
-			if(is_array($events) && !empty($events)){	
+
+			if(is_array($events) && !empty($events)){
 				foreach($events as $e){
-
 					$item = new XMLElement('li');
-
-					$item->appendChild(Widget::Anchor($e['name'], URL . '/symphony/blueprints/events/'.($e['can_parse'] ? 'edit' : 'info').'/' . strtolower($e['handle']) . '/', 'event.' . $e['handle'] . '.php'));	
-					$item->setAttribute('class', 'external');			
-
+					$item->appendChild(Widget::Anchor($e['name'], SYMPHONY_URL . '/blueprints/events/'.($e['can_parse'] ? 'edit' : 'info').'/' . strtolower($e['handle']) . '/', 'event.' . $e['handle'] . '.php'));
 					$list->appendChild($item);
-
 				}
 			}
 
@@ -55,29 +58,29 @@
 			######
 
 			### DATASOURCES ###
-			
+
 			$DSManager = new DatasourceManager($this->_Parent);
-			$datasources = $DSManager->listAll();			
-			
+			$datasources = $DSManager->listAll();
+
 			$li = new XMLElement('li');
 
 			$h3 = new XMLElement('h3', __('Data Sources'));
-			$h3->appendChild(Widget::Anchor(__('Create New'), URL . '/symphony/blueprints/datasources/new/', __('Create a new data source'), 'create button'));
+			$h3->appendChild(Widget::Anchor(__('Create New'), SYMPHONY_URL . '/blueprints/datasources/new/', __('Create a new data source'), 'create button'));
 			$li->appendChild($h3);
 
 			$list = new XMLElement('ul');
 			$list->setSelfClosingTag(false);
-			
-			if(is_array($datasources) && !empty($datasources)){	
+
+			if(is_array($datasources) && !empty($datasources)){
 				foreach($datasources as $ds){
 
 					$item = new XMLElement('li');
 
-					if($ds['can_parse']) 
-						$item->appendChild(Widget::Anchor($ds['name'], URL . '/symphony/blueprints/datasources/edit/' . strtolower($ds['handle']) . '/', 'data.' . $ds['handle'] . '.php'));
+					if($ds['can_parse'])
+						$item->appendChild(Widget::Anchor($ds['name'], SYMPHONY_URL . '/blueprints/datasources/edit/' . strtolower($ds['handle']) . '/', 'data.' . $ds['handle'] . '.php'));
 
 					else{
-						$item->appendChild(Widget::Anchor($ds['name'], URL . '/symphony/blueprints/datasources/info/' . strtolower($ds['handle']) . '/', 'data.' . $ds['handle'] . '.php'));	
+						$item->appendChild(Widget::Anchor($ds['name'], SYMPHONY_URL . '/blueprints/datasources/info/' . strtolower($ds['handle']) . '/', 'data.' . $ds['handle'] . '.php'));
 					}
 
 					$list->appendChild($item);
@@ -89,24 +92,22 @@
 			$ul->appendChild($li);
 
 			######
-			
+
 			### UTILITIES ###
 			$li = new XMLElement('li');
 
 			$h3 = new XMLElement('h3', __('Utilities'));
-			$h3->appendChild(Widget::Anchor(__('Create New'), URL . '/symphony/blueprints/utilities/new/', __('Create a new utility'), 'create button'));
+			$h3->appendChild(Widget::Anchor(__('Create New'), SYMPHONY_URL . '/blueprints/utilities/new/', __('Create a new utility'), 'create button'));
 			$li->appendChild($h3);
 
 			$list = new XMLElement('ul');
 			$list->setSelfClosingTag(false);
-			
-			if(is_array($utilities) && !empty($utilities)){	
+
+			if(is_array($utilities) && !empty($utilities)){
 				foreach($utilities as $u){
-
 					$item = new XMLElement('li');
-					$item->appendChild(Widget::Anchor($u, URL . '/symphony/blueprints/utilities/edit/' . str_replace('.xsl', '', $u) . '/'));
+					$item->appendChild(Widget::Anchor($u, SYMPHONY_URL . '/blueprints/utilities/edit/' . str_replace('.xsl', '', $u) . '/'));
 					$list->appendChild($item);
-
 				}
 			}
 
@@ -115,10 +116,10 @@
 
 			######
 
-			$this->Form->appendChild($ul);	
-		
+			$this->Form->appendChild($ul);
+
 		}
-		
+
 	}
-	
+
 ?>
