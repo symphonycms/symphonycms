f8911816e6997a8412a8f2c6649259ab90c37844
Simone Economo <my.ekoes@gmail.com>
2010-12-23 14:24:13 +0100

    Multiple authors for extensions

--- symphony/content/content.systemextensions.php
+++ symphony/content/content.systemextensions.php
@@ -36,40 +36,40 @@
 				foreach($extensions as $name => $about){
 
 					## Setup each cell
-					$td1 = Widget::TableData((!empty($about['table-link']) && $about['status'] == EXTENSION_ENABLED ? Widget::Anchor($about['name'], $this->_Parent->getCurrentPageURL() . 'extension/' . trim($about['table-link'], '/') . '/') : $about['name']));			
+					$td1 = Widget::TableData((!empty($about['table-link']) && $about['status'] == EXTENSION_ENABLED ? Widget::Anchor($about['name'], $this->_Parent->getCurrentPageURL() . 'extension/' . trim($about['table-link'], '/') . '/') : $about['name']));
 					$td2 = Widget::TableData(($about['status'] == EXTENSION_ENABLED ? __('Yes') : __('No')));
 					$td3 = Widget::TableData($about['version']);
-					
-					if ($about['author'][0] && is_array($about['author'][0])) {
-						$value = "";
+					$td4 = Widget::TableData();
 
-						for($i = 0; $i < count($about['author']);  ++$i) {
-							$author = $about['author'][$i];
-							$link = $author['name'];
+					if($about['author'][0] && is_array($about['author'][0])) {
+						foreach($about['author'] as $i => $author) {
 
 							if(isset($author['website']))
 								$link = Widget::Anchor($author['name'], General::validateURL($author['website']));
-
-							elseif(isset($author['email']))
+							else if(isset($author['email']))
 								$link = Widget::Anchor($author['name'], 'mailto:' . $author['email']);
+							else
+								$link = $author['name'];
 
-							$comma = ($i != count($about['author']) - 1) ? ", " : "";
-							$value .= $link->generate() . $comma;
+							$value .= ($link instanceof XMLElement ? $link->generate() : $link)
+							        . ($i != count($about['author']) - 1 ? ", " : "");
 						}
 
 						$td4->setValue($value);
 					}
-					else {
-						$link = $about['author']['name'];
+					else{
 
 						if(isset($about['author']['website']))
 							$link = Widget::Anchor($about['author']['name'], General::validateURL($about['author']['website']));
+						else if(isset($about['author']['email']))
+							$link = Widget::Anchor($about['author']['name'], 'mailto:' . $about['author']['email']);
+						else
+							$link = $about['author']['name'];
 
-						elseif(isset($about['author']['email']))
-							$link = Widget::Anchor($about['author']['name'], 'mailto:' . $about['author']['email']);	
-						
-						$td4 = Widget::TableData($link);
+						$td4->setValue($link instanceof XMLElement ? $link->generate() : $link);
 					}
+
+					$td4->appendChild(Widget::Input('items['.$name.']', 'on', 'checkbox'));
 
 					## Add a row to the body array, assigning each cell to the row
 					$aTableBody[] = Widget::TableRow(array($td1, $td2, $td3, $td4), ($about['status'] == EXTENSION_NOT_INSTALLED ? 'inactive' : NULL));		
