7b50482af2f03aa8a6ae62721ec2f9bf4fcd156e
Rainer Borene <rainerborene@gmail.com>
2011-01-04 20:18:15 -0200

    fatal errors should be displayed

--- install.php
+++ install.php
@@ -1,5 +1,7 @@
 <?php
 
+	ini_set('display_errors', 1);
+
 	header('Expires: Mon, 12 Dec 1982 06:14:00 GMT');
 	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 	header('Cache-Control: no-cache, must-revalidate, max-age=0');
