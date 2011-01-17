5e988f5128cc100e8083837dcfed35073a4a465f
Rainer Borene <rainerborene@gmail.com>
2011-01-17 12:49:17 -0200

    refactoring templates

--- symphony/lib/core/class.symphony.php
+++ symphony/lib/core/class.symphony.php
@@ -198,7 +198,6 @@
 		 * weeks.
 		 */
 		public function initialiseCookie(){
-
 			$cookie_path = @parse_url(URL, PHP_URL_PATH);
 			$cookie_path = '/' . trim($cookie_path, '/');
 
@@ -325,7 +324,6 @@
 		 *  True if the Author was logged in, false otherwise
 		 */
 		public function login($username, $password, $isHash=false){
-
 			$username = self::$Database->cleanValue($username);
 			$password = self::$Database->cleanValue($password);
 
@@ -425,7 +423,6 @@
 		 * @see core.Cookie#expire()
 		 */
 		public function isLoggedIn(){
-
 			// Ensures that we're in the real world.. Also reduces three queries from database
 			// We must return true otherwise exceptions are not shown
 			if (is_null(self::$_instance)) return true;
@@ -434,7 +431,6 @@
 				return true;
 			}
 			else{
-
 				$username = self::$Database->cleanValue($this->Cookie->get('username'));
 				$password = self::$Database->cleanValue($this->Cookie->get('pass'));
 
@@ -653,7 +649,6 @@
 		 *  that the template may want to expose, such as custom Headers etc.
 		 */
 		public function __construct($message, $heading='Symphony Fatal Error', $template='error', array $additional=NULL){
-
 			if($message instanceof XMLElement){
 				$this->_messageObject = $message;
 				$message = $this->_messageObject->generate();
@@ -725,7 +720,6 @@
 		 *  An HTML string
 		 */
 		public static function render($e){
-
 			$trace = NULL;
 			$odd = true;
 
@@ -761,103 +755,12 @@
 				}
 			}
 
-			return sprintf('<html>
-<head>
-	<title>Symphony Fatal Error</title>
-	<style type="text/css" media="all">
-		*{
-			margin: 0; padding: 0;
-		}
-
-		body{
-			margin: 20px auto;
-			width: 95%%;
-			min-width: 950px;
-			font-family: Helvetica, "MS Trebuchet", Arial, sans-serif;
-			background-color: #ccc;
-			font-size: 12px;
-		}
-
-		.bubble{
-			background-color: white;
-			padding: 22px;
-
-			-webkit-border-radius: 20px;
-			-moz-border-radius: 20px;
-			border-radius: 20px;
-
-			border: 2px solid #bbb;
-		}
-
-		h1{
-			font-size: 34px;
-			text-shadow: 2px 2px 2px #999;
-			margin-bottom: 10px;
-		}
-
-		h2, h3{
-			text-shadow: 2px 2px 2px #ccc;
-		}
-
-		code{
-			font-size: 11px;
-			font-family: Monaco, "Courier New", Courier;
-		}
-
-		ul{
-			list-style: none;
-			color: #111;
-			margin: 20px;
-			border-left: 5px solid #bbb;
-		}
-
-		li{
-			background-color: #dedede;
-			padding: 1px 5px;
-
-			border-left: 1px solid #ddd;
-		}
-
-		li.odd{
-			background-color: #efefef;
-		}
-
-		li#error{
-			background-color: #E8CACA;
-			color: #B9191A;
-		}
-
-		li small{
-			font-size: 10px;
-			color: #666;
-		}
-
-	</style>
-</head>
-<body>
-	<h1>Symphony Fatal Database Error</h1>
-	<div class="bubble">
-		<h2>%s</h2>
-		<p>An error occurred while attempting to execute the following query</p>
-		<ul>
-			<li>%s</li>
-		</ul>
-
-		<h3>Backtrace:</h3>
-		<ul>%s</ul>
-
-		<h3>Database Query Log:</h3>
-		<ul>%s</ul>
-
-	</div>
-</body>
-<html>',
-
+			return sprintf(@file_get_contents(TEMPLATE . '/fatalerror.tpl'),
 				$e->getDatabaseErrorMessage(),
 				$e->getQuery(),
 				$trace,
 				$queries
 			);
-
 		}
+
 	}
