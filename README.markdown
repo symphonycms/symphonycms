33792e186faba487f6e9957cffd8605a2c3a7ad3
craig zheng <craig.zheng@gmail.com>
2011-01-24 22:03:47 -0500

    2.2beta4 version bump

--- README.markdown
+++ README.markdown
@@ -1,6 +1,6 @@
 # Symphony 2 #
 
-- Version: 2.2beta3
+- Version: 2.2beta4
 - Date: 19th Jan 2011
 - Release Notes: <http://symphony-cms.com/download/releases/version/2.2/>
 - Github Repository: <http://github.com/symphonycms/symphony-2/tree/2.2>
--- update.php
+++ update.php
@@ -62,7 +62,7 @@
 
 	set_error_handler('__errorHandler');
 
-	define('kVERSION', '2.2beta3');
+	define('kVERSION', '2.2beta4');
 	define('kCHANGELOG', 'http://symphony-cms.com/download/releases/version/'.kVERSION.'/');
 	define('kINSTALL_ASSET_LOCATION', './symphony/assets/installer');
 	define('kINSTALL_FILENAME', basename(__FILE__));
