8625a4994b72358a0b6368fe8f39262a3202bf8e
craig zheng <craig.zheng@gmail.com>
2011-01-29 17:48:52 -0500

    RC1 Version bump

--- install.php
+++ install.php
@@ -31,7 +31,7 @@
 	}
 
 	// Defines
-	define('kVERSION', '2.2.0beta4');
+	define('kVERSION', '2.2RC1');
 	define('kINSTALL_ASSET_LOCATION', './symphony/assets/installer');
 	define('kINSTALL_FILENAME', basename(__FILE__));
 	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
