936bdcfc0e008d9725b54016c70f6bb0eee4558a
michael-e <info@michael-eichelsdoerfer.de>
2011-02-16 10:10:32 +0100

    Added missing localisation strings (finally fixes #566, hopefully).

--- symphony/lib/toolkit/email-gateways/email.smtp.php
+++ symphony/lib/toolkit/email-gateways/email.smtp.php
@@ -240,11 +240,11 @@
 			$div = new XMLElement('div');
 			$div->setAttribute('class', 'group');
 
-			$label = Widget::Label('From Name');
+			$label = Widget::Label(__('From Name'));
 			$label->appendChild(Widget::Input('settings[email_smtp][from_name]', $this->_sender_name));
 			$div->appendChild($label);
 
-			$label = Widget::Label('From Email Address');
+			$label = Widget::Label(__('From Email Address'));
 			$label->appendChild(Widget::Input('settings[email_smtp][from_address]', $this->_sender_email_address));
 			$div->appendChild($label);
 
