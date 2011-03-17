ffedd7b56fff4e4459968e7c6775ee38b944a448
Brendan Abbott <brendan@randb.com.au>
2011-03-17 10:26:59 +1000

    Use a PHP5.2 fallback for the new DateTimeObj::format function

--- symphony/lib/core/class.datetimeobj.php
+++ symphony/lib/core/class.datetimeobj.php
@@ -9,7 +9,7 @@
 	  * Symphony will set the default timezone of the system using it's configuration
 	  * values.
 	  */
-	Class DateTimeObj{
+	Class DateTimeObj {
 
 		/**
 		 * Uses PHP's date_default_timezone_set function to set the system
@@ -19,7 +19,7 @@
 		 * @link http://php.net/manual/en/function.date-default-timezone-set.php
 		 * @link http://www.php.net/manual/en/timezones.php
 		 * @param string $timezone
-		 *  A valid timezone identifier, such as UTC or Europe/Lisbon
+		 *	A valid timezone identifier, such as UTC or Europe/Lisbon
 		 */
 		public static function setDefaultTimezone($timezone){
 			if(!@date_default_timezone_set($timezone)) trigger_error(__("Invalid timezone '{$timezone}'"), E_USER_WARNING);
@@ -27,35 +27,35 @@
 
 		/**
 		 * Given a `$format`, and a `$timestamp`,
-		 * return the date in the format provided. This function is a basic 
+		 * return the date in the format provided. This function is a basic
 		 * wrapper for PHP's DateTime object. If the `$timestamp` is omitted,
 		 * the current timestamp will be used. Optionally, you pass a
 		 * timezone identifier with this function to localise the output
 		 *
-		 * If you like to display a date in the backend, please make use 
+		 * If you like to display a date in the backend, please make use
 		 * of `DateTimeObj::format()` which allows date and time localization
 		 *
 		 * @see class.datetimeobj.php#format()
 		 * @link http://www.php.net/manual/en/book.datetime.php
 		 * @param string $format
-		 *  A valid PHP date format
+		 *	A valid PHP date format
 		 * @param integer $timestamp (optional)
-		 *  A unix timestamp to format. 'now' or omitting this parameter will
-		 *  result in the current time being used
+		 *	A unix timestamp to format. 'now' or omitting this parameter will
+		 *	result in the current time being used
 		 * @param string $timezone (optional)
-		 *  The timezone associated with the timestamp
+		 *	The timezone associated with the timestamp
 		 * @return string
-		 *  The formatted date
+		 *	The formatted date
 		 */
 		public static function get($format, $timestamp = 'now', $timezone = null) {
-			
+
 			// Format date
 			return DateTimeObj::format($timestamp, $format, false, $timezone);
 		}
-		
+
 		/**
 		 * Formats the given date and time `$string` based on the given `$format`.
-		 * Optionally the result will be localized and respect a timezone differing 
+		 * Optionally the result will be localized and respect a timezone differing
 		 * from the system default. The default output is ISO 8601.
 		 *
 		 * @since Symphony 2.2.1
@@ -68,10 +68,10 @@
 		 * @param string $timezone (optional)
 		 *	The timezone associated with the timestamp
 		 * @return string
-		 *	The formatted date	 
-		 */		
+		 *	The formatted date
+		 */
 		public static function format($string = 'now', $format = 'c', $localize = false, $timezone = null) {
-			
+
 			// Current date and time
 			if($string == 'now' || empty($string)) {
 				$date = new DateTime();
@@ -79,34 +79,36 @@
 
 			// Timestamp
 			elseif(ctype_digit($string)) {
-				$date = DateTime::createFromFormat('U', $string);
+				$date = new Datetime(date(DateTime::RFC822, $string));
 			}
-		
+
 			// Custom date string
 			else {
-		
+
 				// Standardize date
 				$string = Lang::standardizeDate($string);
-			
-				// Apply system date format
-				$date = DateTime::createFromFormat(
-				    Symphony::$Configuration->get('date_format', 'region') . 
-				    Symphony::$Configuration->get('datetime_separator', 'region') . 
-				    Symphony::$Configuration->get('time_format', 'region'), 
-				    $string
-				);
-				
+
+				// Apply Symphony date format using `createFromFormat`
+				// if it exists, or fallbacking back to `strptime`
+				if(!method_exists('DateTime', 'createFromFormat')) {
+					$date = DateTime::createFromFormat(__SYM_DATETIME_FORMAT__, $string);
+				}
+				else {
+					$date = strptime($string, DateTimeObj::dateFormatToStrftime(__SYM_DATETIME_FORMAT__));
+					$date = new DateTime($string);
+				}
+
 				// Handle non-standard dates
 				if($date === false) {
-				    $date = new DateTime($string);
+					$date = new DateTime($string);
 				}
 			}
-			
+
 			// Timezone
 			if($timezone !== null) {
 				$date->setTimezone(new DateTimeZone($timezone));
 			}
-			
+
 			// Format date
 			$date = $date->format($format);
 
@@ -114,21 +116,56 @@
 			if($localize === true) {
 				$date = Lang::localizeDate($date);
 			}
-			
+
 			// Return custom formatted date, use ISO 8601 date by default
 			return $date;
+		}
+
+		/**
+		 * Convert a date format to a strftime format
+		 *
+		 * Timezone conversion is done for unix. Windows users must exchange %z and %Z.
+		 *
+		 * Unsupported date formats : S, n, t, L, B, G, u, e, I, P, Z, c, r
+		 * Unsupported strftime formats : %U, %W, %C, %g, %r, %R, %T, %X, %c, %D, %F, %x
+		 *
+		 * @since Symphony 2.2.1
+		 * @link http://www.php.net/manual/en/function.strftime.php#96424
+		 * @param string $dateFormat a date format
+		 * @return string
+		 */
+		public static function dateFormatToStrftime($dateFormat) {
+
+			$caracs = array(
+				// Day - no strf eq : S
+				'd' => '%d', 'D' => '%a', 'j' => '%e', 'l' => '%A', 'N' => '%u', 'w' => '%w', 'z' => '%j',
+				// Week - no date eq : %U, %W
+				'W' => '%V',
+				// Month - no strf eq : n, t
+				'F' => '%B', 'm' => '%m', 'M' => '%b',
+				// Year - no strf eq : L; no date eq : %C, %g
+				'o' => '%G', 'Y' => '%Y', 'y' => '%y',
+				// Time - no strf eq : B, G, u; no date eq : %r, %R, %T, %X
+				'a' => '%P', 'A' => '%p', 'g' => '%l', 'h' => '%I', 'H' => '%H', 'i' => '%M', 's' => '%S',
+				// Timezone - no strf eq : e, I, P, Z
+				'O' => '%z', 'T' => '%Z',
+				// Full Date / Time - no strf eq : c, r; no date eq : %c, %D, %F, %x
+				'U' => '%s'
+			);
+
+			return strtr((string)$dateFormat, $caracs);
 		}
 
 		/**
 		 * A wrapper for get, this function will force the GMT timezone.
 		 *
 		 * @param string $format
-		 *  A valid PHP date format
+		 *	A valid PHP date format
 		 * @param integer $timestamp (optional)
-		 *  A unix timestamp to format. Omitting this parameter will
-		 *  result in the current time being used
+		 *	A unix timestamp to format. Omitting this parameter will
+		 *	result in the current time being used
 		 * @return string
-		 *  The formatted date in GMT
+		 *	The formatted date in GMT
 		 */
 		public static function getGMT($format, $timestamp = 'now'){
 			return self::get($format, $timestamp, 'GMT');
@@ -142,9 +179,9 @@
 		 * dynamically update how long ago the action took place using Javascript.
 		 *
 		 * @param string $format
-		 *  A valid PHP date format
+		 *	A valid PHP date format
 		 * @return string
-		 *  A HTML string of an `<abbr>` element with a class of 'timeago' and the current
+		 *	A HTML string of an `<abbr>` element with a class of 'timeago' and the current
 		 * date (RFC 2822) as the title element. The value is the current time as
 		 * specified by the `$format`.
 		 */
