ecfa5043fce055a3b9f870709c75cf7024a9be2c
Brendan Abbott <brendan@randb.com.au>
2011-05-26 10:31:17 +1000

    Fix Navigation DS with DS_FILTER_AND, closes #633

--- symphony/lib/toolkit/data-sources/datasource.navigation.php
+++ symphony/lib/toolkit/data-sources/datasource.navigation.php
@@ -11,12 +11,22 @@
 	}
 
 	if(!function_exists('__processNavigationTypeFilter')){
-		function __processNavigationTypeFilter($filter, $database, $filtertype=DS_FILTER_OR){
-
-			$types = preg_split('/'.($filtertype == DS_FILTER_AND ? '\+' : ',').'\s*/', $filter, -1, PREG_SPLIT_NO_EMPTY);
+		function __processNavigationTypeFilter($filter, $filter_type = DS_FILTER_OR) {
+			$types = preg_split('/'.($filter_type == DS_FILTER_AND ? '\+' : '(?<!\\\\),').'\s*/', $filter, -1, PREG_SPLIT_NO_EMPTY);
 			$types = array_map('trim', $types);
 
-			return $types;
+			$types = array_map(array('Datasource', 'removeEscapedCommas'), $types);
+
+			if($filter_type == DS_FILTER_OR) {
+				$type_sql = " AND pt.type IN ('" . implode("', '", $types) . "')";
+			}
+			else {
+				foreach($types as $type) {
+					$type_sql = " AND pt.type = '" . $type . "'";
+				}
+			}
+
+			return $type_sql;
 		}
 	}
 
@@ -46,24 +56,31 @@
 	### BEGIN XML GENERATION CODE ###
 
 	$result = new XMLElement($this->dsParamROOTELEMENT);
-	$types = $parent_sql = null;
+	$type_sql = $parent_sql = null;
 
 	if(trim($this->dsParamFILTERS['type']) != '') {
-		$types = __processNavigationTypeFilter($this->dsParamFILTERS['type'], Symphony::Database(), $this->__determineFilterType($this->dsParamFILTERS['type']));
+		$type_sql = __processNavigationTypeFilter($this->dsParamFILTERS['type'], $this->__determineFilterType($this->dsParamFILTERS['type']));
 	}
 
 	if(trim($this->dsParamFILTERS['parent']) != '') {
 		$parent_sql = __processNavigationParentFilter($this->dsParamFILTERS['parent']);
 	}
 
-	$sql = "SELECT DISTINCT p.* FROM `tbl_pages` AS p LEFT JOIN `tbl_pages_types` AS pt ON(p.id = pt.page_id) WHERE 1 = 1";
-	$sql .= !is_null($parent_sql) ? $parent_sql : " AND p.parent IS NULL ";
-
-	if(!is_null($types)) $sql .= " AND pt.type IN ('" . implode("', '", $types) . "')";
-
-	$sql .= " ORDER BY p.`sortorder` ASC";
-
-	$pages = Symphony::Database()->fetch($sql);
+	// Build the Query appending the Parent and/or Type WHERE clauses
+	$pages = Symphony::Database()->fetch(sprintf("
+			SELECT DISTINCT p.*
+			FROM `tbl_pages` AS p
+			LEFT JOIN `tbl_pages_types` AS pt ON (p.id = pt.page_id)
+			WHERE 1 = 1
+			%s
+			%s
+			ORDER BY p.`sortorder` ASC
+		",
+		// Add Parent SQL
+		!is_null($parent_sql) ? $parent_sql : " AND p.parent IS NULL ",
+		// Add Types SQL
+		!is_null($type_sql) ? $type_sql : ""
+	));
 
 	if((!is_array($pages) || empty($pages))){
 		if($this->dsParamREDIRECTONEMPTY == 'yes'){
