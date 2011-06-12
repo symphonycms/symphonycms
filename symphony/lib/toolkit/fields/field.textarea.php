symphonycms/symphonycms

commit a9398978c0cd3f20eb0b5ce878f65ed933d2644a

Optimise FieldTextarea->__applyFormatting function, should speed up saving sections with large amounts of textareas

symphony/lib/toolkit/fields/field.textarea.php
@@ -6,7 +6,7 @@
 	/**
 	 * A simple Textarea field that essentially maps to HTML's `<textarea/>`.
 	 */
-	
+
 	Class fieldTextarea extends Field {
 		function __construct(&$parent){
 
@@ -37,7 +37,7 @@
 
 			/**
 			 * Allows developers modify the textarea before it is rendered in the publish forms
-			 * 
+			 *
 			 * @delegate ModifyTextareaFieldPublishWidget
 			 * @param string $context
 			 * '/backend/'
@@ -46,8 +46,8 @@
 			 * @param Widget $textarea
 			 */
 			Symphony::ExtensionManager()->notifyMembers('ModifyTextareaFieldPublishWidget', '/backend/', array(
-			    'field' => &$this, 
-			    'label' => &$label, 
+			    'field' => &$this,
+			    'label' => &$label,
 			    'textarea' => &$textarea
 			));
 
@@ -153,29 +153,21 @@
 
 		protected function __applyFormatting($data, $validate=false, &$errors=NULL){
 
-			if($this->get('formatter')){
+			if($this->get('formatter')) {
 				$tfm = new TextformatterManager($this->_engine);
 				$formatter = $tfm->create($this->get('formatter'));
 
-				$result = $formatter->run($data);
+				$result = $formatter->run(General::sanitize($data));
 			}
 
-			if($validate === true){
-
+			if($validate === true) {
 				include_once(TOOLKIT . '/class.xsltprocess.php');
 
+				$result = html_entity_decode($result, ENT_QUOTES, 'UTF-8');
+				$result = $this->__replaceAmpersands($result);
+
 				if(!General::validateXML($result, $errors, false, new XsltProcess)){
-					$result = html_entity_decode($result, ENT_QUOTES, 'UTF-8');
-					$result = $this->__replaceAmpersands($result);
-
-					if(!General::validateXML($result, $errors, false, new XsltProcess)){
-
-						$result = $formatter->run(General::sanitize($data));
-
-						if(!General::validateXML($result, $errors, false, new XsltProcess)){
-							return false;
-						}
-					}
+					return false;
 				}
 			}
 

commit f83bc2fe6231b34bc7e425b6ae1555b1d0914a02

Fix for regression introduced in a9398978. Fixes #654

symphony/lib/toolkit/fields/field.textarea.php
@@ -64,11 +64,13 @@
 			if($validate === true) {
 				include_once(TOOLKIT . '/class.xsltprocess.php');
 
-				$result = html_entity_decode($result, ENT_QUOTES, 'UTF-8');
-				$result = $this->__replaceAmpersands($result);
-
 				if(!General::validateXML($result, $errors, false, new XsltProcess)){
-					return false;
+					$result = html_entity_decode($result, ENT_QUOTES, 'UTF-8');
+					$result = $this->__replaceAmpersands($result);
+
+					if(!General::validateXML($result, $errors, false, new XsltProcess)){
+						return false;
+					}
 				}
 			}
 
@@ -218,10 +220,8 @@
 				}
 
 				else {
-					$value = $data['value'];
+					$value = $this->__replaceAmpersands($data['value']);
 				}
-
-				$value = $this->__replaceAmpersands($value);
 
 				$attributes = array();
 				if ($mode == 'formatted') $attributes['mode'] = $mode;