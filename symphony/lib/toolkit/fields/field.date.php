dc092f649eb93c4eac0f1f6c658ea3bc13eb40de
Brendan Abbott <brendan@bloodbone.ws>
2011-06-27 14:43:24 +1000

    Remove Date field's gmt and local columns, they are useless. RE: #693

--- symphony/lib/toolkit/fields/field.date.php
+++ symphony/lib/toolkit/fields/field.date.php
@@ -65,8 +65,6 @@
 				  `id` int(11) unsigned NOT NULL auto_increment,
 				  `entry_id` int(11) unsigned NOT NULL,
 				  `value` varchar(80) default NULL,
-				  `local` int(11) default NULL,
-				  `gmt` int(11) default NULL,
 				  PRIMARY KEY  (`id`),
 				  UNIQUE KEY `entry_id` (`entry_id`),
 				  KEY `value` (`value`)
@@ -267,8 +265,10 @@
 			if($andOperation) {
 				foreach($data as $date) {
 					$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id".$this->key."` ON `e`.`id` = `t$field_id".$this->key."`.entry_id ";
-					$where .= " AND (DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s') >= '" . DateTimeObj::get('Y-m-d H:i:s', $date['start']) . "'
-								AND DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s') <= '" . DateTimeObj::get('Y-m-d H:i:s', $date['end']) . "') ";
+					$where .= " AND (
+									DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s')
+									BETWEEN '" . DateTimeObj::get('Y-m-d H:i:s', $date['start']) . "' AND '" . DateTimeObj::get('Y-m-d H:i:s', $date['end']) . "'
+								) ";
 
 					$this->key++;
 				}
@@ -278,8 +278,10 @@
 				$tmp = array();
 
 				foreach($data as $date) {
-					$tmp[] = "(DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s') >= '" . DateTimeObj::get('Y-m-d H:i:s', $date['start']) . "'
-								AND DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s') <= '" . DateTimeObj::get('Y-m-d H:i:s', $date['end']) . "') ";
+					$tmp[] = "(
+								DATE_FORMAT(`t$field_id".$this->key."`.value, '%Y-%m-%d %H:%i:%s')
+								BETWEEN '" . DateTimeObj::get('Y-m-d H:i:s', $date['start']) . "' AND '" . DateTimeObj::get('Y-m-d H:i:s', $date['end']) . "'
+							) ";
 				}
 
 				$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id".$this->key."` ON `e`.`id` = `t$field_id".$this->key."`.entry_id ";
@@ -421,18 +423,14 @@
 			// Valid date
 			if(!is_null($timestamp)) {
 				return array(
-					'value' => DateTimeObj::get('c', $timestamp),
-					'local' => strtotime(DateTimeObj::get('c', $timestamp)),
-					'gmt' => strtotime(DateTimeObj::getGMT('c', $timestamp))
+					'value' => DateTimeObj::get('c', $timestamp)
 				);
 			}
 
 			// Invalid date
 			else {
 				return array(
-					'value' => null,
-					'local' => null,
-					'gmt' => null
+					'value' => null
 				);
 			}
 		}
