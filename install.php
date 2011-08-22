cb2a94d5993c9be042c56cc0f32a1d0ff0b8777a
Brendan Abbott <brendan@bloodbone.ws>
2011-08-22 21:54:53 +1000

    Update to 2.3pre

--- install.php
+++ install.php
@@ -37,7 +37,7 @@
 	}
 
 	// Defines
-	define('kVERSION', '2.2.3');
+	define('kVERSION', '2.3pre');
 	define('kINSTALL_ASSET_LOCATION', './symphony/assets/installer');
 	define('kINSTALL_FILENAME', basename(__FILE__));
 	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
