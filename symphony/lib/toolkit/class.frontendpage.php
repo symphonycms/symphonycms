0e23840f5fe7ac05698af10f93c7035c5add7ddc
John Porter <john.porter@designermonkey.co.uk>
2011-10-23 19:31:14 +0100

    Additional #826 Fixes - Trim the trailing slash, and add Query-string parameter

--- symphony/lib/toolkit/class.frontendpage.php
+++ symphony/lib/toolkit/class.frontendpage.php
@@ -305,8 +305,9 @@
 			$root_page = strpos('/', $page['path']) !== false ? array_shift(explode('/', $page['path'])) : '';
 			$current_path = explode(dirname($_SERVER['SCRIPT_NAME']), $_SERVER['REQUEST_URI'], 2);
 			$current_path = '/' . ltrim(end($current_path), '/');
-			$current_path = explode('?', $current_path, 2);
-			$current_path = current($current_path);
+			$split_path = explode('?', $current_path, 3);
+			$current_path = rtrim(current($split_path), '/');
+			$querystring = '?' . next($split_path);
 
 			// Get max upload size from php and symphony config then choose the smallest
 			$upload_size_php = ini_size_to_bytes(ini_get('upload_max_filesize'));
