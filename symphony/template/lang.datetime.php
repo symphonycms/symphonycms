5a7fad5504031a39b7fa3688c1d42d9e8e3f8845
Simone Economo <my.ekoes@gmail.com>
2011-10-26 20:57:14 +0200

    Slightly improved comments for

--- symphony/template/lang.datetime.php
+++ symphony/template/lang.datetime.php
@@ -11,13 +11,14 @@
 		'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
 		'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
 
-		// Time span
+		// Units of time
 		'sec', 'second', 'min', 'minute', 'hour', 'day', 'fortnight', 'forthnight', 'month', 'year',
 		'secs', 'seconds', 'mins', 'minutes', 'hours', 'days', 'fortnights', 'forthnights', 'months', 'years',
 		'weekday', 'weekdays', 'week', 'weeks',
 
 		// Order
-		'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'next', 'last', 'previous', 'this'
+		'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth',
+		'next', 'last', 'previous', 'this'
 
 	);
 
