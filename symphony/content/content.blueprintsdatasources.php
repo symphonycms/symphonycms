a928c10539a14557092c7a3d6280959c4f50fd9e
Nils Werner <nils.werner@gmail.com>
2011-10-26 14:44:02 +0200

    Search in WORKSPACE/template/ for datasource-, event-, and pages-templates

--- symphony/content/content.blueprintsdatasources.php
+++ symphony/content/content.blueprintsdatasources.php
@@ -1068,7 +1068,7 @@
 
 			if(empty($this->_errors)){
 
-				$dsShell = file_get_contents(TEMPLATE . '/blueprints.datasource.tpl');
+				$dsShell = file_get_contents($this->getTemplate('blueprints.datasource'));
 
 				$params = array(
 					'rootelement' => $rootelement,
