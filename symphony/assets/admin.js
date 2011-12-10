629c8791e6527156bd33091be040af22d5f13176
Brendan Abbott <brendan@bloodbone.ws>
2011-12-10 13:26:04 +1000

    Tweaks/fixes for the hide/show functionality introduced for the Datasource/Events index. RE: #954

--- symphony/assets/admin.js
+++ symphony/assets/admin.js
@@ -303,35 +303,45 @@
 		});
 
 		// Datasource collapsable links
-		$('#blueprints-datasources.index table tr, #blueprints-events.index table tr').each(function(index){
+		$('#blueprints-datasources.index table tr, #blueprints-events.index table tr').each(function() {
 			var links = [];
 			$('td:eq(2) a', this).each(function(){
-				links.push($(this).remove());
+				links.push($(this));
 			});
+
+			// If there is less than 3, show them all by default
+			if(links.length <= 3) return;
+
+			// Clear the field and append the links
 			$('td:eq(2)', this).html('');
-			for(var i=0, l=links.length; i<l; i++)
-			{
+			for(var i=0, l=links.length; i<l; i++) {
 				$('td:eq(2)', this).append(links[i]).append('<span>, </span>');
 			}
 			$("td:eq(2) a:gt(2)", this).each(function(){
 				$(this).hide().next().hide();
 			});
-			if(links.length > 3)
-			{
-				$('td:eq(2)', this).append('<a href="#" id="expand' + index + '" class="expand">(' + (links.length - 3) +
-					' <span class="more">more</span><span class="less">less</span>...)</a>');
-				$('#expand' + index).click(function(){
-					if($(this).hasClass('expanded'))
-					{
-						$(">a:gt(2), >span:gt(2)", $(this).parent()).not('.expand').hide(500);
-						$(this).removeClass('expanded');
-					} else {
-						$(">a, >span", $(this).parent()).show(500);
-						$(this).addClass('expanded');
-					}
-					return false;
-				});
-			}
+
+			$('td:eq(2)', this).append(
+				'<a href="#" class="expand">' +
+				' <span class="more">' + (links.length - 3) + ' more</span>' +
+				' <span class="less">less</span>&hellip;</a>'
+			);
+
+			// Listen for click on the 'expand' links, to hide/show
+			$(this).on('click', '.expand', function() {
+				var $parent = $(this).parent();
+
+				if($(this).hasClass('expanded')) {
+					$(">a:gt(2), >span:gt(2)", $parent).not('.expand').hide();
+					$(this).removeClass('expanded');
+				}
+				else {
+					$(">a, >span", $parent).show();
+					$(this).addClass('expanded');
+				}
+
+				return false;
+			});
 		});
 
 		// Data source manager options
