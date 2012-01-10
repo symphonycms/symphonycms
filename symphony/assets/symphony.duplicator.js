e3be7d16ab790e6b5ad68f04237a62139b5bf5cc
Brendan Abbott <brendan@bloodbone.ws>
2012-01-10 22:50:40 +1000

    Revert icon to 'Remove Item' text, whitespace cleanup. RE: #907

--- symphony/assets/symphony.duplicator.js
+++ symphony/assets/symphony.duplicator.js
@@ -36,7 +36,7 @@
 				instances:			'> li:not(.template)',
 				templates:			'> li.template',
 				headers:			'> :first-child',
-				preselect:			false, 
+				preselect:			false,
 				orderable:			false,
 				collapsible:		false,
 				constructable:		true,
@@ -57,7 +57,7 @@
 		});
 
 	/*-----------------------------------------------------------------------*/
-	
+
 		objects.each(function() {
 			var object = $(this),
 				instances = object.find(settings.instances).addClass('instance'),
@@ -68,7 +68,7 @@
 				controls = $('<div class="controls" />'),
 				selector = $('<select />'),
 				constructor = $('<a class="constructor">' + Symphony.Language.get('Add item') + '</a>');
-			
+
 		/*-------------------------------------------------------------------*/
 
 			// Construct instances
@@ -78,11 +78,11 @@
 				instance.trigger('constructstart.duplicator');
 				instance.trigger('construct.duplicator'); /* deprecated */
 				instance.hide().appendTo(object);
-				
+
 				// Set speed
 				if(!speed) {
 					speed = settings.speed;
-				}	
+				}
 
 				// Show instance
 				instance.trigger('constructshow.duplicator');
@@ -93,7 +93,7 @@
 					instance.trigger('constructstop.duplicator');
 				});
 			});
-			
+
 			// Destruct instances
 			duplicator.on('click.duplicator', 'a.destructor:not(.disabled)', function(event) {
 				var instance = $(this).parents('.instance:first');
@@ -102,24 +102,24 @@
 				instance.trigger('destruct.duplicator'); /* deprecated */
 				instance.slideUp(settings.speed, function() {
 					$(this).remove();
-					instance.trigger('destructstop.duplicator');				
+					instance.trigger('destructstop.duplicator');
 				});
 			});
-			
+
 			// Lock constructor
 			duplicator.on('constructstop.duplicator', '.instance', function() {
 				if(duplicator.find('.instance').length >= settings.maximum) {
 					constructor.addClass('disabled');
 				}
 			});
-			
+
 			// Unlock constructor
 			duplicator.on('destructstart.duplicator', '.instance', function() {
 				if(duplicator.find('.instance').length <= settings.maximum) {
 					constructor.removeClass('disabled');
 				}
 			});
-			
+
 			// Lock destructor
 			duplicator.on('destructstart.duplicator', '.instance', function() {
 				if(duplicator.find('.instance').length - 1 == settings.minimum) {
@@ -133,24 +133,24 @@
 					duplicator.find('a.destructor').removeClass('disabled');
 				}
 			});
-			
+
 			// Lock unique instances
 			duplicator.on('constructstop.duplicator', '.instance', function(event) {
 				var instance = $(this);
 
 				if(instance.is('.unique')) {
 					selector.find('option[value="' + instance.attr('data-type') + '"]').attr('disabled', true);
-					
+
 					// Preselect first available instance
 					selector.find('option').attr('selected', false).filter(':not(:disabled):first').attr('selected', true);
-					
+
 					// All selected
 					if(selector.find('option:not(:disabled)').length == 0) {
 						selector.attr('disabled', 'disabled');
 					}
 				}
 			});
-			
+
 			// Unlock unique instances
 			duplicator.on('destructstart.duplicator', '.instance', function(event) {
 				var instance = $(this),
@@ -158,26 +158,26 @@
 
 				if(instance.is('.unique')) {
 					option = selector.attr('disabled', false).find('option[value="' + instance.attr('data-type') + '"]').attr('disabled', false);
-					
+
 					// Preselect instance if it's the only active one
 					if(selector.find('option:not(:disabled)').length == 1) {
 						option.attr('selected', true);
 					}
 				}
 			});
-			
+
 			// Update title descriptions in header
 			duplicator.on('keyup.duplicator', '.instance input[name*="[label]"]', function(event) {
 				var input = $(this),
 					instance = input.parents('.instance:first'),
 					title = instance.find(settings.headers).find('span:first'),
 					description = title.find('i');
-					
+
 				// Create description
 				if(description.length == 0) {
 					description = $('<i />').appendTo(title);
 				}
-				
+
 				// Update description
 				description.text($.trim(input.val()));
 			});
@@ -186,20 +186,20 @@
 			duplicator.on('constructstop.duplicator refresh.duplicator', '.instance', function(event) {
 				var instance = $(this),
 					position = duplicator.find('.instance').index(instance);
-			
+
 				// Loop over named fields
 				instance.find('*[name]').each(function() {
 					var field = $(this),
-						exp = /\[\-?[0-9]+\]/,						
+						exp = /\[\-?[0-9]+\]/,
 						name = field.attr('name');
 
 					// Set index
 					if(exp.test(name)) {
 						field.attr('name', name.replace(exp, '[' + position + ']'));
 					}
-				});			
+				});
 			});
-			
+
 			// Refresh field indexes
 			duplicator.on('orderstop.orderable', function(event) {
 				duplicator.find('.instance').trigger('refresh.duplicator');
@@ -211,17 +211,17 @@
 					duplicator.trigger('activate.collapsible');
 				}
 			});
-			
+
 			// Deactivate controls
 			duplicator.on('destructstart.duplicator', '.instance', function(event) {
 				if(duplicator.find('.instance').length == 1) {
 					duplicator.trigger('deactivate.collapsible');
 				}
 			});
-						
+
 		/*-------------------------------------------------------------------*/
 
-			// Build interface			
+			// Build interface
 			duplicator.insertBefore(object).prepend(object);
 			headers.wrapInner('<span />').each(function() {
 				$(this).nextAll().wrapAll('<div class="content" />');
@@ -237,10 +237,10 @@
 					var template = $(this),
 						title = template.find(settings.headers).text(),
 						value = template.attr('data-type');
-						
+
 					template.trigger('constructstart.duplicator');
 
-					// Check type connection 
+					// Check type connection
 					if(!value) {
 						value = title;
 						template.attr('data-type', value);
@@ -251,34 +251,34 @@
 						text: title,
 						value: value
 					}).appendTo(selector);
-					
+
 					// Check uniqueness
 					template.trigger('constructstop.duplicator');
 				}).removeClass('template').addClass('instance').remove();
 			}
-				
+
 			// Select default
 			if(settings.preselect != false) {
 				selector.find('option[value="' + settings.preselect + '"]').attr('selected', true);
 			}
-			
+
 			// Single template
 			if(templates.length <= 1) {
 				selector.hide();
-				
+
 				// Single unique template
 				if(templates.is('.unique')) {
 					controls.find('a.constructor').trigger('click.duplicator', [0]);
 					controls.hide();
 				}
 			}
-			
+
 			// Destructable interface
 			if(settings.destructable === true) {
 				duplicator.addClass('destructable');
 				headers.append('<a class="destructor">' + Symphony.Language.get('Remove item') + '</a>')
 			}
-				
+
 			// Collapsible interface
 			if(settings.collapsible) {
 				duplicator.symphonyCollapsible({
@@ -286,7 +286,7 @@
 					handles: '.header span'
 				});
 			}
-	
+
 			// Orderable interface
 			if(settings.orderable) {
 				duplicator.symphonyOrderable({
@@ -294,13 +294,13 @@
 					handles: '.header',
 					ignore: 'span'
 				});
-			}			
-			
+			}
+
 			// Initialise existing instances
 			instances.trigger('constructstop.duplicator');
 			instances.find('input[name*="[label]"]').trigger('keyup.duplicator');
-		});				
-		
+		});
+
 	/*-----------------------------------------------------------------------*/
 
 		return objects;
