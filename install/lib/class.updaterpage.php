331d2c70c7cde07a408d696588129d73d44d37f8
Brendan Abbott <brendan@bloodbone.ws>
2012-03-11 02:21:26 +1000

    Fixed fatal error when appending h1 to updater page

--- install/lib/class.updaterpage.php
+++ install/lib/class.updaterpage.php
@@ -27,7 +27,7 @@
 
 			// Add Release Notes for the latest migration
 			if(isset($this->_params['release-notes'])){
-				$h1 = end($this->Body->getChildrenByName('h1'));
+				$h1 = end($this->Form->getChildrenByName('h1'));
 				if($h1 instanceof XMLElement) {
 					$h1->appendChild(
 						new XMLElement(
