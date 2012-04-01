bfa25f00fedab3f99726ef6d2d3e5221f8019c83
Giel Berkers <info@gielberkers.com>
2012-04-01 23:21:05 +0200

    added installation script for lookup tables

---
+++ sym_lookup_tables.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `sym_lookup_pages`
+--
+
+CREATE TABLE `sym_lookup_pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
