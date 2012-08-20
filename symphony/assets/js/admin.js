3200371ed7202169969471c8488c27c7cf22df4d
Nils Hörrmann <post@nilshoerrmann.de>
2012-08-20 10:37:54 +0200

    Simplify Picker

--- symphony/assets/js/admin.js
+++ symphony/assets/js/admin.js
@@ -86,11 +86,7 @@
 		contents.find('.tags').symphonyTags();
 
 		// Pickers
-		contents.find('select[name="settings[Email][default_gateway]"]').symphonyPickable();
-		contents.find('select[name="fields[source]"]', '#blueprints-events').symphonyPickable();
-		contents.find('select[name="fields[dynamic_xml][format]"]').symphonyPickable({
-			pickables: '#xml'
-		});
+		contents.find('select.picker').symphonyPickable();
 
 		// Selectable
 		contents.find('table.selectable').symphonySelectable();
