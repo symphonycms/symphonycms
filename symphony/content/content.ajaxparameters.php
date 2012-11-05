ec12ae589c264b5cc2d7a6c51b1b15d60087bf03
Nils Hörrmann <post@nilshoerrmann.de>
2012-11-05 12:42:45 +0100

    Get default Symphony and page parameters as well

--- symphony/content/content.ajaxparameters.php
+++ symphony/content/content.ajaxparameters.php
@@ -3,17 +3,28 @@
 	 * @package content
 	 */
 	/**
-	 * The AjaxParameters returns an JSON array of all available Data Source output parameters.
+	 * The AjaxParameters returns an JSON array of all available parameters.
 	 */
 	require_once(TOOLKIT . '/class.datasourcemanager.php');
 
 	Class contentAjaxParameters extends AjaxPage {
 
 		public function view() {
-			$params = array();
-			$datasources = DatasourceManager::listAll();
+			$params = array('{$today}', '{$current-time}', '{$this-year}', '{$this-month}', '{$this-day}', '{$timezone}', '{$website-name}', '{$page-title}', '{$root}', '{$workspace}', '{$root-page}', '{$current-page}', '{$current-page-id}', '{$current-path}', '{$current-query-string}', '{$current-url}', '{$cookie-username}', '{$cookie-pass}', '{$page-types}', '{$upload-limit}');
 			
-			// Get Data Sources
+			// Get page parameters
+			$pages = PageManager::fetch(true, array('params'));
+			foreach($pages as $key => $pageparams) {
+				if(!empty($pageparams['params'])) {
+					$pageparams = explode('/', $pageparams['params']);
+					foreach($pageparams as $pageparam) {
+						$params[] = '{$' . $pageparam . '}';
+					}
+				}
+			}
+			
+			// Get Data Sources output parameters
+			$datasources = DatasourceManager::listAll();
 			foreach($datasources as $datasource) {
 				$current = DatasourceManager::create($datasource['handle']);
 				$prefix = '{$ds-' . $datasource['handle'] . '.';
@@ -22,11 +33,12 @@
 				// Get parameters
 				if(is_array($current->dsParamPARAMOUTPUT)) {
 					foreach($current->dsParamPARAMOUTPUT as $id => $param) {
-						$params[] = $prefix . $param . $suffix;
+						$params[] = $prefix . Lang::createHandle($param) . $suffix;
 					}
 				}
 			}
 			
+			sort($params);
 			$this->_Result = json_encode($params);
 		}
 
