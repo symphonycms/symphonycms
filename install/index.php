2f44ee0a27b26a6910e8c337916ca512bde9f892
Brendan Abbott <brendan@bloodbone.ws>
2012-12-12 22:54:38 +1000

    Tweak the constant defintions so that https will be respected in the installer

--- install/index.php
+++ install/index.php
@@ -1,28 +1,12 @@
 <?php
 
+	ini_set('display_errors', 1);
+
 	// Set the current timezone, should that not be available
 	// default to GMT.
 	if(!date_default_timezone_set(date_default_timezone_get())) {
 		date_default_timezone_set('GMT');
 	}
-
-	// Defines some constants
-	$clean_url = rtrim($_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']), '/\\');
-	$clean_url = preg_replace(array('/\/{2,}/i', '/\/install$/i'), array('/', NULL), $clean_url);
-
-	define('DOMAIN', $clean_url);
-	define('URL', 'http://' . $clean_url);
-	define('INSTALL_URL', URL . '/install');
-
-	$clean_path = rtrim(dirname($_SERVER['SCRIPT_FILENAME']), '/\\');
-	$clean_path = preg_replace(array('/\/{2,}/i', '/\/install$/i'), array('/', NULL), $clean_path);
-
-	define('DOCROOT', $clean_path);
-
-	// Required boot components
-	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
-
-	ini_set('display_errors', 1);
 
 	// Show PHP Info
 	if(isset($_REQUEST['info'])){
@@ -30,9 +14,22 @@
 		exit;
 	}
 
+	// Defines some constants
+	$clean_url = rtrim($_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']), '/\\');
+	$clean_url = preg_replace(array('/\/{2,}/i', '/\/install$/i'), array('/', NULL), $clean_url);
+	define('DOMAIN', $clean_url);
+
+	$clean_path = rtrim(dirname(__FILE__), '/\\');
+	$clean_path = preg_replace(array('/\/{2,}/i', '/\/install$/i'), array('/', NULL), $clean_path);
+	define('DOCROOT', $clean_path);
+
+	// Required boot components
+	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
+
 	define('VERSION', '2.3.1');
 	define('INSTALL', DOCROOT . '/install');
 	define('INSTALL_LOGS', MANIFEST . '/logs');
+	define('INSTALL_URL', URL . '/install');
 
 	// If prompt to remove, delete the entire `/install` directory
 	// and then redirect to Symphony
