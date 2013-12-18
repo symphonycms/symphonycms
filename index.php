69d824bd22c6165be070b85ee73360b434d43ff6
Jens Scherbl <jens@scherbl.net>
2013-12-18 18:38:20 +0100

    Added better support for internationalized domain names

--- index.php
+++ index.php
@@ -3,7 +3,19 @@
 	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
 	define('PATH_INFO', isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : NULL);
 	define('DOMAIN_PATH', dirname(rtrim($_SERVER['PHP_SELF'], PATH_INFO)));
-	define('DOMAIN', rtrim(rtrim($_SERVER['HTTP_HOST'], '\\/') . DOMAIN_PATH, '\\/'));
+
+	if (!function_exists('idn_to_utf8')) {
+
+	    require_once DOCROOT . '/symphony/lib/boot/idna.php';
+
+		$idn = new idna_convert();
+
+		define('DOMAIN', $idn->decode(rtrim(rtrim($_SERVER['HTTP_HOST'], '\\/') . DOMAIN_PATH, '\\/')));
+
+	} else {
+
+		define('DOMAIN', idn_to_utf8(rtrim(rtrim($_SERVER['HTTP_HOST'], '\\/') . DOMAIN_PATH, '\\/')));
+	}
 
 	require(DOCROOT . '/symphony/lib/boot/bundle.php');
 
