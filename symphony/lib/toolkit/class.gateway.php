034d1513d781f3c49c823a4ea4cc071b1ed807ab
Brendan Abbott <brendan@bloodbone.ws>
2014-01-12 15:15:44 +1000

    Ensure that the Gateway returns an integer for the status code. RE: symphonycms/jit_image_manipulation#78

--- symphony/lib/toolkit/class.gateway.php
+++ symphony/lib/toolkit/class.gateway.php
@@ -440,7 +440,7 @@
 			$this->_info_last = array(
 				'url' => $this->_url,
 				'content_type' => $content_type,
-				'http_code' => $status,
+				'http_code' => (int)$status,
 				'total_time' => $end
 			);
 
