4a0b386cc3785d340e905fa4243c607adc298aeb
Brendan Abbott <brendan@bloodbone.ws>
2014-01-12 18:53:16 +1000

    Integrate UTF-8 table conversion script into the updater. Closes #1626

--- install/migrations/2.4.0.php
+++ install/migrations/2.4.0.php
@@ -31,12 +31,33 @@
 		}
 
 		static function upgrade() {
-			// Update to include Admin Path configuration #702
+			// [#702] Update to include Admin Path configuration
 			if(version_compare(self::$existing_version, '2.4a1', '<=')) {
 				// Add missing config value for index view string length
 				Symphony::Configuration()->set('cell_truncation_length', '75', 'symphony');
 				// Add admin-path to configuration
 				Symphony::Configuration()->set('admin-path', 'symphony', 'symphony');
+			}
+
+			// [#1626] Update all tables to be UTF-8 encoding/collation
+			// @link https://gist.github.com/michael-e/5789168
+			$tables = Symphony::Database()->fetch("SHOW TABLES");
+			if(is_array($tables) && !empty($tables)){
+				foreach($tables as $table){
+					$table = current($table);
+
+					// If it's not a Symphony table, ignore it
+					if(!preg_match('/^' . Symphony::Database()->getPrefix() . '/', $table)) continue;
+
+					Symphony::Database()->query(sprintf(
+						"ALTER TABLE `%s` CHARACTER SET utf8 COLLATE utf8_unicode_ci",
+						$table
+					));
+					Symphony::Database()->query(sprintf(
+						"ALTER TABLE `%s` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci",
+						$table
+					));
+				}
 			}
 
 			// Update the version information
