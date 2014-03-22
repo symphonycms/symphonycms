6caebe8d22936c36c198f7def2a908dd3cd538f7
Nicolas Brassard <nico@nitriques.com>
2014-03-22 15:43:51 -0400

    Refactored contentAjaxHandle in extends JSONPage

--- symphony/content/content.ajaxhandle.php
+++ symphony/content/content.ajaxhandle.php
@@ -6,24 +6,14 @@
 	 * The AjaxHandle page is used for generating handles on the fly
 	 * that are used in Symphony's javascript
 	 */
-	Class contentAjaxHandle extends AjaxPage{
-
-		public function handleFailedAuthorisation(){
-			$this->setHttpStatus(self::HTTP_STATUS_UNAUTHORIZED);
-			$this->_Result = json_encode(array('status' => __('You are not authorised to access this page.')));
-		}
-
+	
+	require_once(TOOLKIT . '/class.jsonpage.php');
+	
+	Class contentAjaxHandle extends JSONPage{
+		
 		public function view(){
 			$string = $_GET['string'];
-
-			$this->_Result = json_encode(Lang::createHandle($string, 255, '-', true));
+			$this->_Result['handle'] = Lang::createHandle($string, 255, '-', true);
 		}
-
-		public function generate($page = null){
-			header('Content-Type: application/json');
-			echo $this->_Result;
-			exit;
-		}
-
 	}
 
