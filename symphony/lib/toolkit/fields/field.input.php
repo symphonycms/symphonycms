symphonycms/symphonycms

commit 3c240fe19174385e710186d65b2a25dae8b1ab65

Make core fields' has-data check more consistent.
As discussed in #1978.

symphony/lib/toolkit/fields/field.input.php
@@ -131,7 +131,7 @@
 				$data = $data['value'];
 			}
 
-			if($this->get('required') == 'yes' && 0 === strlen(trim($data))){
+			if($this->get('required') == 'yes' && strlen(trim($data) == 0)){
 				$message = __('‘%s’ is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}

symphony/lib/toolkit/fields/field.textarea.php
@@ -162,7 +162,7 @@
 		public function checkPostFieldData($data, &$message, $entry_id=NULL){
 			$message = NULL;
 
-			if($this->get('required') == 'yes' && 0 === strlen(trim($data))){
+			if($this->get('required') == 'yes' && strlen(trim($data) == 0)){
 				$message = __('‘%s’ is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}

symphony/lib/toolkit/fields/field.taglist.php
@@ -206,7 +206,7 @@
 		public function checkPostFieldData($data, &$message, $entry_id = null){
 			$message = NULL;
 
-			if($this->get('required') == 'yes' && strlen($data) == 0){
+			if($this->get('required') == 'yes' && strlen(trim($data)) == 0){
 				$message = __('‘%s’ is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}

commit e43046e05df511a7cf29d93be93e339cfae48831

Fix bug introduced in 3c240fe19174385e710186d65b2a25dae8b1ab65

symphony/lib/toolkit/fields/field.input.php
@@ -130,7 +130,7 @@
 				$data = $data['value'];
 			}
 
-			if($this->get('required') == 'yes' && strlen(trim($data) == 0)){
+			if($this->get('required') == 'yes' && strlen(trim($data)) == 0){
 				$message = __('‘%s’ is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}

symphony/lib/toolkit/fields/field.textarea.php
@@ -159,7 +159,7 @@
 		public function checkPostFieldData($data, &$message, $entry_id=NULL){
 			$message = NULL;
 
-			if($this->get('required') == 'yes' && strlen(trim($data) == 0)){
+			if($this->get('required') == 'yes' && strlen(trim($data)) == 0){
 				$message = __('‘%s’ is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}