813e1aa6de6141d72489d8a80ffdef892189bdcc
Brendan Abbott <brendan@bloodbone.ws>
2014-04-01 20:51:10 +1000

    Add Publish Filtering updates to the migrator. Closes #1997

--- install/migrations/2.4.0.php
+++ install/migrations/2.4.0.php
@@ -2,6 +2,8 @@
 
 	Class migration_240 extends Migration {
 
+		static $publish_filtering_disabled = false;
+
 		static function getVersion(){
 			return '2.4beta2';
 		}
@@ -12,7 +14,7 @@
 
 		static function upgrade() {
 			// [#702] Update to include Admin Path configuration
-			if(version_compare(self::$existing_version, '2.4', '<=')) {
+			if(version_compare(self::$existing_version, '2.4beta2', '<=')) {
 				// Add missing config value for index view string length
 				Symphony::Configuration()->set('cell_truncation_length', '75', 'symphony');
 				// Add admin-path to configuration
@@ -40,14 +42,28 @@
 				}
 			}
 
-			// Change date field to be a varchar instead of an ENUM to support prepopulation
+			// [#1420] Change date field to be a varchar instead of an ENUM to support prepopulation
 			try {
 				Symphony::Database()->query('
-					ALTER TABLE `tbl_fields_date` 
+					ALTER TABLE `tbl_fields_date`
 					CHANGE `pre_populate` `pre_populate` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL;
 				');
 			}
 			catch (Exception $ex) {}
+
+			// [#1997] Add filtering column to the Sections table
+			if(!Symphony::Database()->tableContainsField('tbl_sections', 'filter')) {
+				Symphony::Database()->query("
+					ALTER TABLE `tbl_sections`
+					ADD `filter` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes';
+				");
+			}
+
+			$installed_extensions = Symphony::ExtensionManager()->listInstalledHandles();
+			if(in_array('publishfiltering', $installed_extensions)) {
+				Symphony::ExtensionManager()->uninstall('publishfiltering');
+				self::$publish_filtering_disabled = true;
+			}
 
 			// Update the version information
 			Symphony::Configuration()->set('version', self::getVersion(), 'symphony');
@@ -59,6 +75,23 @@
 			else {
 				return true;
 			}
+		}
+
+		static function preUpdateNotes(){
+			return array(
+				__("Symphony 2.4 is a major release that contains breaking changes from previous versions. It is highly recommended to review the releases notes and make a complete backup of your installation before updating as these changes may affect the functionality of your site."),
+				__("This release will automatically convert all existing Symphony database tables to %s.", array("<code>utf8_unicode_ci</code>"))
+			);
+		}
+
+		static function postUpdateNotes(){
+			$notes = array();
+
+			if(self::$publish_filtering_disabled) {
+				$notes[] = __("As Symphony 2.4 adds the Publish Filtering extension into the core, the standalone extension has been uninstalled. You can remove it from your installation at any time.");
+			}
+
+			return $notes;
 		}
 
 	}
