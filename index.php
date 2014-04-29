cc675dcde69b400999c922a306716f912c38ede8
Brendan Abbott <brendan@bloodbone.ws>
2014-04-29 18:38:18 +1000

    Allow the Mod Rewrite error fallback to resolve correctly when a non /symphony path is used

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
 	// #1808
 	if(isset($_SERVER['HTTP_MOD_REWRITE'])) {
 		$output = file_get_contents(GenericExceptionHandler::getTemplate('fatalerror.rewrite'));
+		$output = str_replace('{APPLICATION_URL}', APPLICATION_URL, $output);
 		$output = str_replace('{SYMPHONY_URL}', SYMPHONY_URL, $output);
 		$output = str_replace('{URL}', URL, $output);
 		echo $output;
