3f7c6846361e0fb7349240d96904bb4f558efa62
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-10 10:52:06 +0200

    Store association interface information

--- install/index.php
+++ install/index.php
@@ -30,7 +30,7 @@
 	// Required boot components
 	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
 
-	define('VERSION', '2.4');
+	define('VERSION', '2.4.1dev');
 	define('INSTALL', DOCROOT . '/install');
 	define('INSTALL_LOGS', MANIFEST . '/logs');
 	define('INSTALL_URL', URL . '/install');
