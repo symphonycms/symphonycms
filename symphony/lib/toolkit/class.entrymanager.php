symphonycms/symphonycms

commit 7b74a6e68c89d92c0f7e37daaae821cbe89c3816

Be stricter when constructing queries throughout the core. RE: #2190

symphony/lib/toolkit/class.entrymanager.php
@@ -116,7 +116,9 @@ class EntryManager
                 continue;
             }
 
-            Symphony::Database()->delete('tbl_entries_data_' . $field_id, " `entry_id` = '$entry_id'");
+            Symphony::Database()->delete('tbl_entries_data_' . $field_id, sprintf("
+                `entry_id` = %d", $entry_id
+            ));
 
             $data = array(
                 'entry_id' => $entry_id
@@ -175,7 +177,9 @@ class EntryManager
             }
 
             try {
-                Symphony::Database()->delete('tbl_entries_data_' . $field_id, sprintf(' `entry_id` = %d', $entry->get('id')));
+                Symphony::Database()->delete('tbl_entries_data_' . $field_id, sprintf(" `
+                    entry_id` = %d", $entry->get('id')
+                ));
             } catch (Exception $e) {
                 // Discard?
             }
@@ -409,19 +413,27 @@ class EntryManager
             $entry_id = array($entry_id);
         }
 
-        $sql = "
-            SELECT  ".($group ? 'DISTINCT ' : '')."`e`.id,
-                    `e`.section_id, e.`author_id`,
-                    UNIX_TIMESTAMP(e.`creation_date`) AS `creation_date`,
-                    UNIX_TIMESTAMP(e.`modification_date`) AS `modification_date`
+        $sql = sprintf("
+            SELECT  %s`e`.id, `e`.section_id, e.`author_id`,
+                UNIX_TIMESTAMP(e.`creation_date`) AS `creation_date`,
+                UNIX_TIMESTAMP(e.`modification_date`) AS `modification_date`
             FROM `tbl_entries` AS `e`
-            $joins
+            %s
             WHERE 1
-            ".($entry_id ? "AND `e`.`id` IN ('".implode("', '", $entry_id)."') " : '')."
-            ".($section_id ? "AND `e`.`section_id` = '$section_id' " : '')."
-            $where
-            $sort
-            ".($limit ? 'LIMIT ' . intval($start) . ', ' . intval($limit) : '');
+            %s
+            %s
+            %s
+            %s
+            %s
+            ", 
+            $group ? 'DISTINCT ' : '',
+            $joins,
+            $entry_id ? "AND `e`.`id` IN ('".implode("', '", $entry_id)."') " : '',
+            $section_id ? sprintf("AND `e`.`section_id` = %d", $section_id) : '',
+            $where,
+            $sort,
+            $limit ? sprintf('LIMIT %d, %d', $start, $limit) : ''
+        );
 
         $rows = Symphony::Database()->fetch($sql);
 
@@ -568,7 +580,10 @@ class EntryManager
      */
     public static function fetchEntrySectionID($entry_id)
     {
-        return Symphony::Database()->fetchVar('section_id', 0, "SELECT `section_id` FROM `tbl_entries` WHERE `id` = '$entry_id' LIMIT 1");
+        return Symphony::Database()->fetchVar('section_id', 0, sprintf("
+            SELECT `section_id` FROM `tbl_entries` WHERE `id` = %d LIMIT 1",
+            $entry_id
+        ));
     }
 
     /**
@@ -596,15 +611,18 @@ class EntryManager
             return false;
         }
 
-        return Symphony::Database()->fetchVar(
-            'count',
-            0,
-            "SELECT count(".($group ? 'DISTINCT ' : '')."`e`.id) as `count`
-            FROM `tbl_entries` AS `e`
-            $joins
-            WHERE `e`.`section_id` = '$section_id'
-            $where"
-        );
+        return Symphony::Database()->fetchVar('count', 0, sprintf("
+                SELECT COUNT(%s`e`.id) as `count`
+                FROM `tbl_entries` AS `e`
+                %s
+                WHERE `e`.`section_id` = %d
+                %s
+            ",
+            $group ? 'DISTINCT ' : '',
+            $joins,
+            $section_id,
+            $where
+        ));
     }
 
     /**

symphony/lib/toolkit/class.sectionmanager.php
@@ -58,7 +58,7 @@ class SectionManager
      */
     public static function edit($section_id, array $settings)
     {
-        if (!Symphony::Database()->update($settings, 'tbl_sections', " `id` = $section_id")) {
+        if (!Symphony::Database()->update($settings, 'tbl_sections', sprintf(" `id` = %d", $section_id))) {
             return false;
         }
 
@@ -78,7 +78,10 @@ class SectionManager
      */
     public static function delete($section_id)
     {
-        $details = Symphony::Database()->fetchRow(0, "SELECT `sortorder` FROM tbl_sections WHERE `id` = '$section_id'");
+        $details = Symphony::Database()->fetchRow(0, sprintf("
+            SELECT `sortorder` FROM tbl_sections WHERE `id` = %d",
+            $section_id
+        ));
 
         // Delete all the entries
         include_once TOOLKIT . '/class.entrymanager.php';
@@ -95,13 +98,17 @@ class SectionManager
         }
 
         // Delete the section
-        Symphony::Database()->delete('tbl_sections', " `id` = '$section_id'");
+        Symphony::Database()->delete('tbl_sections', sprintf("
+            `id` = %d", $section_id
+        ));
 
         // Update the sort orders
         Symphony::Database()->query("UPDATE tbl_sections SET `sortorder` = (`sortorder` - 1) WHERE `sortorder` > '".$details['sortorder']."'");
 
         // Delete the section associations
-        Symphony::Database()->delete('tbl_sections_association', " `parent_section_id` = '$section_id'");
+        Symphony::Database()->delete('tbl_sections_association', printf("
+            `parent_section_id` = %d", $section_id
+        ));
 
         return true;
     }
@@ -324,8 +331,8 @@ class SectionManager
      */
     public static function fetchChildAssociations($section_id, $respect_visibility = false)
     {
-        return Symphony::Database()->fetch(sprintf(
-            "SELECT *
+        return Symphony::Database()->fetch(sprintf("
+            SELECT *
             FROM `tbl_sections_association` AS `sa`, `tbl_sections` AS `s`
             WHERE `sa`.`parent_section_id` = %d
             AND `s`.`id` = `sa`.`child_section_id`

symphony/content/content.login.php
@@ -218,7 +218,9 @@ class contentLogin extends HTMLPage
                 ));
 
                 if (!empty($author)) {
-                    Symphony::Database()->delete('tbl_forgotpass', " `expiry` < '".DateTimeObj::getGMT('c')."' ");
+                    Symphony::Database()->delete('tbl_forgotpass', sprintf("
+                        `expiry` < %d", DateTimeObj::getGMT('c')
+                    ));
 
                     if (!$token = Symphony::Database()->fetchVar('token', 0, "SELECT `token` FROM `tbl_forgotpass` WHERE `expiry` > '".DateTimeObj::getGMT('c')."' AND `author_id` = ".$author['id'])) {
                         // More secure password token generation

symphony/content/content.systemauthors.php
@@ -704,7 +704,11 @@ class contentSystemAuthors extends AdministrationPage
 
                 // All good, let's save the Author
                 if (is_array($this->_errors) && empty($this->_errors) && $this->_Author->commit()) {
-                    Symphony::Database()->delete('tbl_forgotpass', " `expiry` < '".DateTimeObj::getGMT('c')."' OR `author_id` = '".$author_id."' ");
+                    Symphony::Database()->delete('tbl_forgotpass', sprintf("
+                        `expiry` < %d OR `author_id` = %d", 
+                        DateTimeObj::getGMT('c'),
+                        $author_id
+                    ));
 
                     if ($isOwner) {
                         Administration::instance()->login($this->_Author->get('username'), $this->_Author->get('password'), true);

symphony/lib/core/class.session.php
@@ -270,7 +270,7 @@ class Session
             sprintf(
                 "DELETE
                 FROM `tbl_sessions`
-                WHERE `session_expires` <= '%s'",
+                WHERE `session_expires` <= %d",
                 Symphony::Database()->cleanValue(time() - $max)
             )
         );

symphony/lib/core/class.symphony.php
@@ -470,7 +470,9 @@ abstract class Symphony implements Singleton
                 if (self::isUpgradeAvailable() === false && Cryptography::requiresMigration(self::$Author->get('password'))) {
                     self::$Author->set('password', Cryptography::hash($password));
 
-                    self::Database()->update(array('password' => self::$Author->get('password')), 'tbl_authors', " `id` = '" . self::$Author->get('id') . "'");
+                    self::Database()->update(array('password' => self::$Author->get('password')), 'tbl_authors', sprintf(
+                        " `id` = %d", self::$Author->get('id')
+                    ));
                 }
 
                 self::$Cookie->set('username', $username);
@@ -524,7 +526,7 @@ abstract class Symphony implements Singleton
                 $token
             ));
 
-            self::Database()->delete('tbl_forgotpass', " `token` = '{$token}' ");
+            self::Database()->delete('tbl_forgotpass', sprintf(" `token` = %s ", $token));
         } else {
             $row = self::Database()->fetchRow(0, sprintf(
                 "SELECT `id`, `username`, `password`
@@ -541,7 +543,9 @@ abstract class Symphony implements Singleton
             self::$Author = AuthorManager::fetchByID($row['id']);
             self::$Cookie->set('username', $row['username']);
             self::$Cookie->set('pass', $row['password']);
-            self::Database()->update(array('last_seen' => DateTimeObj::getGMT('Y-m-d H:i:s')), 'tbl_authors', " `id` = '{$row['id']}'");
+            self::Database()->update(array('last_seen' => DateTimeObj::getGMT('Y-m-d H:i:s')), 'tbl_authors', sprintf("
+                `id` = %d", $row['id']
+            ));
 
             return true;
         }

symphony/lib/toolkit/cache/cache.database.php
@@ -155,7 +155,7 @@ class CacheDatabase implements iCache
     public function delete($hash = null)
     {
         if (!is_null($hash)) {
-            $this->Database->delete("`tbl_cache`", "`hash` = '$hash'");
+            $this->Database->delete("`tbl_cache`", sprintf("`hash` = %s", $hash));
         } else {
             $this->Database->delete("`tbl_cache`", "UNIX_TIMESTAMP() > `expiry`");
         }

symphony/lib/toolkit/class.entry.php
@@ -177,7 +177,7 @@ class Entry
 
         // Failed to create entry, cleanup
         if ($status != __ENTRY_OK__ and !is_null($entry_id)) {
-            Symphony::Database()->delete('tbl_entries', " `id` = '$entry_id' ");
+            Symphony::Database()->delete('tbl_entries', sprintf(" `id` = %d ", $entry_id));
         }
 
         return $status;

commit 3fef4d85f7924cd0cf79f3c94424a4f67c749c0d

Fix some typos introduced in 7b74a6e68c89d92c0f7e37daaae821cbe89c3816

symphony/lib/toolkit/class.entrymanager.php
@@ -177,8 +177,8 @@ class EntryManager
             }
 
             try {
-                Symphony::Database()->delete('tbl_entries_data_' . $field_id, sprintf(" `
-                    entry_id` = %d", $entry->get('id')
+                Symphony::Database()->delete('tbl_entries_data_' . $field_id, sprintf("
+                    `entry_id` = %d", $entry->get('id')
                 ));
             } catch (Exception $e) {
                 // Discard?

symphony/lib/toolkit/class.sectionmanager.php
@@ -106,7 +106,7 @@ class SectionManager
         Symphony::Database()->query("UPDATE tbl_sections SET `sortorder` = (`sortorder` - 1) WHERE `sortorder` > '".$details['sortorder']."'");
 
         // Delete the section associations
-        Symphony::Database()->delete('tbl_sections_association', printf("
+        Symphony::Database()->delete('tbl_sections_association', sprintf("
             `parent_section_id` = %d", $section_id
         ));
 