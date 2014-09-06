1d6c79f73e1f64125ffbfcde167723ab5592ce2f
Brendan Abbott <brendan@bloodbone.ws>
2014-09-06 18:10:32 +1000

    Remove System Author grouping from Datasources. It has never worked. RE: #2188

--- symphony/content/content.blueprintsdatasources.php
+++ symphony/content/content.blueprintsdatasources.php
@@ -685,8 +685,6 @@ class contentBlueprintsDatasources extends ResourcesPage
         foreach ($field_groups as $section_id => $section_data) {
             $optgroup = array('label' => $section_data['section']->get('name'), 'data-label' => 'section-' . $section_data['section']->get('id'), 'options' => array());
 
-            $authorOverride = false;
-
             if (is_array($section_data['fields']) && !empty($section_data['fields'])) {
                 foreach ($section_data['fields'] as $input) {
 
@@ -694,16 +692,8 @@ class contentBlueprintsDatasources extends ResourcesPage
                         continue;
                     }
 
-                    if ($input->get('element_name') == 'author') {
-                        $authorOverride = true;
-                    }
-
                     $optgroup['options'][] = array($input->get('id'), ($fields['source'] == $section_id && $fields['group'] == $input->get('id')), $input->get('label'));
                 }
-            }
-
-            if (!$authorOverride) {
-                $optgroup['options'][] = array('author', ($fields['source'] == $section_id && $fields['group'] == 'author'), __('Author'));
             }
 
             $options[] = $optgroup;
