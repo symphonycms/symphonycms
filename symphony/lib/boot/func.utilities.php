ad8b4d9f1262de150fe520d8ec08f1989f4a3f46
Nicolas Brassard <nico@nitriques.com>
2014-09-10 12:28:10 -0400

    Added Status 302 header in redirect()
    I do not know why, but without it, my setup was returning a 500 code if the actual http status was something else (404 in my case).
    
    This fix only makes sure that the 302 status (temporary redirect) will be returned to the client.
    After all, a call to redirect must really mean that we want a redirect.
    
    Note: I did not used the Page class helpers since this is 'utility' code and I did not wanted to couple utility code to the Page class.

--- symphony/lib/boot/func.utilities.php
+++ symphony/lib/boot/func.utilities.php
@@ -28,6 +28,7 @@ function redirect ($url)
         $url  = str_replace($host, idn_to_ascii($host), $url);
     }
 
+    header('Status: 302 Found');
     header('Expires: Mon, 12 Dec 1982 06:00:00 GMT');
     header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
     header('Cache-Control: no-cache, must-revalidate, max-age=0');
