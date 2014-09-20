4c5e7c53be9dd9437bdc53c2e762f7657f4dd505
Brendan Abbott <brendan@bloodbone.ws>
2014-09-20 17:45:18 +1000

    Move the autoload into the index instead of bundle

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
-	// Find out where we are:
-	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
+    // Find out where we are:
+    define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
 
-	// Include the boot script:
-	include DOCROOT . '/symphony/lib/boot/bundle.php';
+    // Include the boot script:
+    require_once DOCROOT . '/symphony/lib/boot/autoload.php';
+    require_once DOCROOT . '/symphony/lib/boot/bundle.php';
 
-	// Begin Symphony proper:
-	symphony(
-		isset($_GET['mode'])
-			? $_GET['mode']
-			: null
-	);
+    // Begin Symphony proper:
+    symphony(
+        isset($_GET['mode'])
+            ? $_GET['mode']
+            : null
+    );
