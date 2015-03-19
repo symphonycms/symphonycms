d5b88699fd5eb1d6262e427dc16f88c7a5f81022
koolaid1551 <koolaid_89_@hotmail.com>
2015-03-19 18:42:17 -0400

    Add the encoding back
    Added <?xml version="1.0" encoding="UTF-8"?> back in

--- symphony/lib/toolkit/class.frontendpage.php
+++ symphony/lib/toolkit/class.frontendpage.php
@@ -536,7 +536,8 @@ class FrontendPage extends XSLTPage
         $xml->prependChild($params);
 
         $this->setXML($xml);
-        $xsl = '<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">' .
+        $xsl = '<?xml version="1.0" encoding="UTF-8"?>' .
+               '<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">' .
                '    <xsl:import href="file:///' . str_replace(array('\\', ' '), array('/', '%20'), $page['filelocation']) . '"/>' .
                '</xsl:stylesheet>';
 
