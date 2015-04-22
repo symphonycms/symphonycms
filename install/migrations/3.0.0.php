31f575535a50b70d70a8947363e858c5e932dc05
Brendan Abbott <brendan@bloodbone.ws>
2015-04-23 09:22:53 +1000

    Add missing migration for #2354. Closes #2391

--- install/migrations/3.0.0.php
+++ install/migrations/3.0.0.php
@@ -12,4 +12,19 @@
         {
             return 'http://getsymphony.com/download/releases/version/3.0.0/';
         }
+
+        static function upgrade()
+        {
+            // Add the delegate order field, RE: #2354
+            try {
+                Symphony::Database()->query("
+                    ALTER TABLE `tbl_extensions_delegates`
+                    ADD `order` int(11) SIGNED NOT NULL DEFAULT '0',
+                ");
+            }
+            catch (Exception $ex) {}
+
+            // Update the version information
+            return parent::upgrade();
+        }
     }
