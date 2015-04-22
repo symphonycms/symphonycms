37c72085238d01fe8f7b4bf9244ec6e1dc4aa514
Brendan Abbott <brendan@bloodbone.ws>
2015-04-22 14:58:41 +1000

    Drop Pragra/Expires response headers
    Unless the request is made using HTTP/1.0. The Cache Control header does what we want.
    
    Fixes #2205
    
    Picked from 56f8dc08a56d006f810efea8ef1c5d11e3b91e13

--- install/lib/class.installerpage.php
+++ install/lib/class.installerpage.php
@@ -25,9 +25,7 @@ class InstallerPage extends HTMLPage
         $this->Html->setAttribute('lang', Lang::get());
 
         $this->addHeaderToPage('Cache-Control', 'no-cache, must-revalidate, max-age=0');
-        $this->addHeaderToPage('Expires', 'Mon, 12 Dec 1982 06:14:00 GMT');
         $this->addHeaderToPage('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT');
-        $this->addHeaderToPage('Pragma', 'no-cache');
 
         $this->setTitle($this->page_title);
         $this->addElementToHead(new XMLElement('meta', null, array('charset' => 'UTF-8')), 1);
