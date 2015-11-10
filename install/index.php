cd6ee3adf51881f9e540011d806531121111534b
nitriques <nico@nitriques.com>
2015-11-10 18:41:18 -0500

    Release 2.6.4

--- install/index.php
+++ install/index.php
@@ -27,7 +27,7 @@
     define('DOCROOT', $clean_path);
 
     // Required boot components
-    define('VERSION', '2.6.3');
+    define('VERSION', '2.6.4');
     define('INSTALL', DOCROOT . '/install');
 
     // Include autoloader:
---
+++ install/migrations/2.6.4.php
@@ -0,0 +1,14 @@
+<?php
+
+    class migration_264 extends Migration
+    {
+        public static function getVersion()
+        {
+            return '2.6.4';
+        }
+
+        public static function getReleaseNotes()
+        {
+            return 'http://getsymphony.com/download/releases/version/2.6.4/';
+        }
+    }
