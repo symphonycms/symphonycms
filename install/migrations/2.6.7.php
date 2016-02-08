1ff58f8c82c5da4900616647c4aa494e651fa6ec
Nicolas Brassard <nico@nitriques.com>
2016-02-08 16:46:01 -0500

    Release 2.6.7

---
+++ install/migrations/2.6.7.php
@@ -0,0 +1,14 @@
+<?php
+
+    class migration_267 extends Migration
+    {
+        public static function getVersion()
+        {
+            return '2.6.7';
+        }
+
+        public static function getReleaseNotes()
+        {
+            return 'http://getsymphony.com/download/releases/version/2.6.7/';
+        }
+    }
