a49019d7e23d4027d556b63af1620abe78041eef
Alexander Rutz <ar@animaux.de>
2016-03-21 09:42:56 +0100

    Current Version of selectize.js with hacked version of remove_button plugin
    n/t

--- symphony/assets/js/lib/selectize.js
+++ symphony/assets/js/lib/selectize.js
@@ -2970,60 +2970,116 @@
 	
 	});
 	
-	Selectize.define('remove_button', function(options) {
-		if (this.settings.mode === 'single') return;
+	// START remove_button
 	
-		options = $.extend({
-			label     : '&times;',
-			title     : 'Remove',
-			className : 'remove',
-			append    : true
-		}, options);
-	
-		var self = this;
-		var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
-	
-		/**
-		 * Appends an element as a child (with raw HTML).
-		 *
-		 * @param {string} html_container
-		 * @param {string} html_element
-		 * @return {string}
-		 */
-		var append = function(html_container, html_element) {
-			var pos = html_container.search(/(<\/[^>]+>\s*)$/);
-			return html_container.substring(0, pos) + html_element + html_container.substring(pos);
-		};
-	
-		this.setup = (function() {
-			var original = self.setup;
-			return function() {
-				// override the item rendering method to add the button to each
-				if (options.append) {
-					var render_item = self.settings.render.item;
-					self.settings.render.item = function(data) {
-						return append(render_item.apply(this, arguments), html);
-					};
-				}
-	
-				original.apply(this, arguments);
-	
-				// add event listener
-				this.$control.on('click', '.' + options.className, function(e) {
-					e.preventDefault();
-					if (self.isLocked) return;
-	
-					var $item = $(e.currentTarget).parent();
-					self.setActiveItem($item);
-					if (self.deleteSelection()) {
-						self.setCaret(self.items.length);
-					}
-				});
-	
-			};
-		})();
-	
-	});
+  Selectize.define('remove_button', function(options) {
+    options = $.extend({
+        label     : '&times;',
+        title     : 'Remove',
+        className : 'remove',
+        append    : true
+      }, options);
+
+    var singleClose = function(thisRef, options) {
+
+        var self = thisRef;
+        var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
+
+        /**
+         * Appends an element as a child (with raw HTML).
+         *
+         * @param {string} html_container
+         * @param {string} html_element
+         * @return {string}
+         */
+        var append = function(html_container, html_element) {
+          var pos = html_container.search(/(<\/[^>]+>\s*)$/);
+          return html_container.substring(0, pos) + html_element + html_container.substring(pos);
+        };
+
+        thisRef.setup = (function() {
+          var original = self.setup;
+          return function() {
+            // override the item rendering method to add the button to each
+            if (options.append) {
+              var id = $(self.$input.context).attr('id');
+              var selectizer = $('#'+id);
+
+              var render_item = self.settings.render.item;
+              self.settings.render.item = function(data) {
+                return append(render_item.apply(thisRef, arguments), html);
+              };
+            }
+
+            original.apply(thisRef, arguments);
+
+            // add event listener
+            thisRef.$control.on('click', '.' + options.className, function(e) {
+              e.preventDefault();
+              if (self.isLocked) return;
+
+              self.clear();
+            });
+
+          };
+        })();
+      };
+
+      var multiClose = function(thisRef, options) {
+
+        var self = thisRef;
+        var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
+
+        /**
+         * Appends an element as a child (with raw HTML).
+         *
+         * @param {string} html_container
+         * @param {string} html_element
+         * @return {string}
+         */
+        var append = function(html_container, html_element) {
+          var pos = html_container.search(/(<\/[^>]+>\s*)$/);
+          return html_container.substring(0, pos) + html_element + html_container.substring(pos);
+        };
+
+        thisRef.setup = (function() {
+          var original = self.setup;
+          return function() {
+            // override the item rendering method to add the button to each
+            if (options.append) {
+              var render_item = self.settings.render.item;
+              self.settings.render.item = function(data) {
+                return append(render_item.apply(thisRef, arguments), html);
+              };
+            }
+
+            original.apply(thisRef, arguments);
+
+            // add event listener
+            thisRef.$control.on('click', '.' + options.className, function(e) {
+              e.preventDefault();
+              if (self.isLocked) return;
+
+              var $item = $(e.currentTarget).parent();
+              self.setActiveItem($item);
+              if (self.deleteSelection()) {
+                self.setCaret(self.items.length);
+              }
+            });
+
+          };
+        })();
+      };
+
+      if (this.settings.mode === 'single') {
+        singleClose(this, options);
+        return;
+      } else {
+        multiClose(this, options);
+      }
+  });
+  
+  // /ENDE remove_button
 	
 	Selectize.define('restore_on_backspace', function(options) {
 		var self = this;
