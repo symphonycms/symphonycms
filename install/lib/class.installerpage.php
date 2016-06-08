5c5ffa2a8547fbd24e1461f358146e92105503d8
David Oliver <david@doliver.co.uk>
2016-06-08 12:31:39 +0200

    Adjust installation wording on deleting install folder
    The previous wording could be read as inferring that the deletion will automatically
    take place when proceeding.

--- install/lib/class.installerpage.php
+++ install/lib/class.installerpage.php
@@ -208,7 +208,7 @@
 
             $this->Form->appendChild(
                 new XMLElement('p',
-                    __('I think you and I will achieve great things together. Just one last thing: how about we remove the %s folder and secure the safety of our relationship?', array('<code>' . basename(INSTALL) . '</code>'))
+                    __('I think you and I will achieve great things together. Just one last thing: please remove the %s folder to secure the safety of our relationship.', array('<code>' . basename(INSTALL) . '</code>'))
                 )
             );
 
@@ -220,7 +220,7 @@
 
         protected function viewConfiguration()
         {
-            /* -----------------------------------------------
+        /* -----------------------------------------------
          * Populating fields array
          * -----------------------------------------------
          */
