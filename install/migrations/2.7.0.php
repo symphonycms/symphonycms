a850313d3921e1ee593402e15a97b5da9d07d542
ChoboHub <antoine_lucas_999@Hotmail.com>
2016-07-27 11:12:14 -0400

    Update `pre_populate` when updating to 2.7.x
    table `tbl_fields_date` and column `pre_populate` will now update "yes" => "now"
    and "no" => "".
    
    Refactor on Condition and $prepopulate
    Removed the $this->get('pre_populate') !== 'no'
    Modified the ternary operator $prepopulate to get his value
    
    Refactor removing is_null($flagWithError)
    since param $flagWithError = null ... is_null($flagWithError) is obsolete
    since we already have param $data = null ... empty($data)
    
    Doesn't need to verify (is_null($data)) since we already have (empty($data))
    
    Fixes #2541
    
    Picked from 334386b5fb

--- install/migrations/2.7.0.php
+++ install/migrations/2.7.0.php
@@ -4,7 +4,7 @@
     {
         public static function getVersion()
         {
-            return '2.7.0.beta1';
+            return '2.7.0.beta2';
         }
 
         public static function getReleaseNotes()
@@ -14,7 +14,19 @@
 
         public static function upgrade()
         {
-            
+            // Update `pre_populate` replace "yes" with "now"
+            // Update `pre_populate` replace "no" or NULL with ""
+            try {
+                Symphony::Database()->query('
+                        UPDATE `tbl_fields_date` SET `pre_populate` = "now" WHERE `pre_populate`= "yes";
+                ');
+                Symphony::Database()->query('
+                        UPDATE `tbl_fields_date` SET `pre_populate` = NULL WHERE `pre_populate` = "no" OR `pre_populate` = ""
+                ');
+            } catch (Exception $ex) {
+                // ignore
+            }
+
             // Update the version information
             return parent::upgrade();
         }
