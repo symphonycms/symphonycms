05d1f743ad75a155f1b6e04f9f95b5be94c7976a
Nicolas Brassard <n@deuxhuithuit>
2017-04-14 15:57:05 -0400

    Use HTML5 lang attribute instead of xml:lang

--- symphony/template/usererror.generic.php
+++ symphony/template/usererror.generic.php
@@ -5,7 +5,7 @@ $Page = new HTMLPage();
 $Page->Html->setElementStyle('html');
 
 $Page->Html->setDTD('<!DOCTYPE html>');
-$Page->Html->setAttribute('xml:lang', 'en');
+$Page->Html->setAttribute('lang', 'en');
 $Page->addElementToHead(new XMLElement('meta', null, array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=UTF-8')), 0);
 $Page->addStylesheetToHead(ASSETS_URL . '/css/symphony.min.css', 'screen', null, false);
 
