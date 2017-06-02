3c1a65ab76b13b53fb7dbd363012e000cc1e387d
Nicolas Brassard <nitriques@users.noreply.github.com>
2017-06-01 20:49:34 -0400

    Eliminate code duplication in random order check (#2689)
    This eliminates some code duplication.
    Also nice for extensions developers.
    
    Picked from a066d2d1c0

--- symphony/lib/toolkit/fields/field.input.php
+++ symphony/lib/toolkit/fields/field.input.php
@@ -348,7 +348,7 @@ class FieldInput extends Field implements ExportableField, ImportableField
 
     public function buildSortingSQL(&$joins, &$where, &$sort, $order = 'ASC')
     {
-        if (in_array(strtolower($order), array('random', 'rand'))) {
+        if ($this->isRandomOrder($order)) {
             $sort = 'ORDER BY RAND()';
         } else {
             $sort = sprintf(
