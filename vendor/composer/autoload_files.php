205a79fdd8bbf39bbda93edc4226c3b1f6077bdc
Jens Scherbl <jens@scherbl.com>
2017-06-22 22:46:06 +0200

    Add `composer.lock` to the repository
    (as recommended)
    
    Picked from 33be3b83dc1ca9fe17014f515749334f24e682e7

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $baseDir . '/symphony/lib/boot/func.utilities.php',
-    $baseDir . '/symphony/lib/boot/defines.php',
-    $baseDir . '/symphony/lib/toolkit/util.validators.php',
+    '2fff7155503ae8062f33ffa21dc0c9d0' => $baseDir . '/symphony/lib/boot/func.utilities.php',
+    '860f97b44e4fec43f50b3b30d589a8c9' => $baseDir . '/symphony/lib/boot/defines.php',
+    'dcba515aa2b31aba0cb05995f45ed9ef' => $baseDir . '/symphony/lib/toolkit/util.validators.php',
 );
