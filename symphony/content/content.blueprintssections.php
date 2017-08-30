symphonycms/symphonycms

commit e5ecf3cbd346f885ce50708dc7ef42196556ca3d

Prevents data loss on save (#2659)
We add a timestamp check before saving records, either in sections
(fields) or entries.

When saving a section, if the data changes between the render of the
page and the form post, it can lead to data loss if a field was added.
The SQL will crash because it assumes the fields did not change.

When saving and entry, the SQL was ok, but still, it can lead to data
loss.

At both places, the fix provided here makes sure we are editing the
lastest version of the record.

Rebased from 81438f64a5~1...0c8c76581e
Rebased from 60e930ee8f~1...1a931a4adc

Fix SQL error when saving a deleted field

This commit makes sure that a request to update a field will not fail if
the field as already been deleted by another user (or the same user in a
different tab/browser).

If the field is found to be non-existant, it is treated as a new field.

Fixes #2703
pick 1c84b2194b

Replace the added fieldset with an alert

@brendo's suggestion is WAY better then the current implementation,
which adds a new UI pattern in the system. Usign links in the alerts
makes it possible to offer the overwrite mode, without gluttering the
UI.

In order to make it possible to submit the form from the alert, a little
of pretty simple javascript was needed.

Rebased from 6f37eafdb6~1...c652b18d2a

Picked from 95fb812365

symphony/content/content.blueprintssections.php
@@ -359,7 +359,6 @@ class contentBlueprintsSections extends AdministrationPage
 
         if (isset($_POST['fields'])) {
             $fields = array();
-
             if (is_array($_POST['fields']) && !empty($_POST['fields'])) {
                 foreach ($_POST['fields'] as $position => $data) {
                     if ($fields[$position] = FieldManager::create($data['type'])) {
@@ -368,9 +367,13 @@ class contentBlueprintsSections extends AdministrationPage
                     }
                 }
             }
+            $timestamp = isset($_POST['action']['timestamp'])
+                ? $_POST['action']['timestamp']
+                : $section->get('modification_date');
         } else {
             $fields = FieldManager::fetch(null, $section_id);
             $fields = array_values($fields);
+            $timestamp = $section->get('modification_date');
         }
 
         if (isset($_POST['meta'])) {
@@ -386,9 +389,8 @@ class contentBlueprintsSections extends AdministrationPage
             'rel' => 'canonical',
             'href' => SYMPHONY_URL . $canonical_link,
         )));
-
         $this->appendSubheading(General::sanitize($meta['name']),
-            Widget::Anchor(__('View Entries'), SYMPHONY_URL . '/publish/' . $section->get('handle'), __('View Section Entries'), 'button')
+            Widget::Anchor(__('View Entries'), SYMPHONY_URL . '/publish/' . $section->get('handle') . '/', __('View Section Entries'), 'button')
         );
         $this->insertBreadcrumbs(array(
             Widget::Anchor(__('Sections'), SYMPHONY_URL . '/blueprints/sections/'),
@@ -524,6 +526,9 @@ class contentBlueprintsSections extends AdministrationPage
         $button->setAttributeArray(array('name' => 'action[delete]', 'class' => 'button confirm delete', 'title' => __('Delete this section'), 'type' => 'submit', 'accesskey' => 'd', 'data-message' => __('Are you sure you want to delete this section?')));
         $div->appendChild($button);
 
+        $div->appendChild(Widget::Input('action[timestamp]', $timestamp, 'hidden'));
+        $div->appendChild(Widget::Input('action[ignore-timestamp]', 'yes', 'checkbox', array('class' => 'irrelevant')));
+
         $this->Form->appendChild($div);
     }
 
@@ -616,6 +621,10 @@ class contentBlueprintsSections extends AdministrationPage
             if ($edit) {
                 $section_id = $this->_context[1];
                 $existing_section = SectionManager::fetch($section_id);
+                $canProceed = $this->validateTimestamp($section_id, true);
+                if (!$canProceed) {
+                    $this->addTimestampValidationPageAlert($this->_errors['timestamp'], $existing_section, 'save');
+                }
             }
 
             // Check handle to ensure it is unique
@@ -788,9 +797,19 @@ class contentBlueprintsSections extends AdministrationPage
                             $field->set('sortorder', (string)$position);
                             $field->set('parent_section', $section_id);
 
-                            $newField = !(boolean)$field->get('id');
+                            // It is possible that the "existing" field has been deleted
+                            // so we are dealing with an invalid id.
+                            // First make sure the field table still exists and that
+                            // there is a field with the corresponding id in it
+                            $newField = !((boolean)$field->get('id') && $field->tableExists() && $field->exists());
+                            // If the field has not been found, erase the id from $_POST
+                            if ($newField) {
+                                $field->set('id', false);
+                            }
 
+                            // Save data
                             $field->commit();
+                            // Get the new id
                             $field_id = $field->get('id');
 
                             if ($field_id) {
@@ -860,26 +879,35 @@ class contentBlueprintsSections extends AdministrationPage
             }
         }
 
-        if (@array_key_exists("delete", $_POST['action'])) {
+        if (@array_key_exists('delete', $_POST['action'])) {
             $section_id = array($this->_context[1]);
+            $canProceed = $this->validateTimestamp($section_id);
 
-            /**
-             * Just prior to calling the Section Manager's delete function
-             *
-             * @delegate SectionPreDelete
-             * @since Symphony 2.2
-             * @param string $context
-             * '/blueprints/sections/'
-             * @param array $section_ids
-             *  An array of Section ID's passed by reference
-             */
-            Symphony::ExtensionManager()->notifyMembers('SectionPreDelete', '/blueprints/sections/', array('section_ids' => &$section_id));
+            if ($canProceed) {
+                /**
+                 * Just prior to calling the Section Manager's delete function
+                 *
+                 * @delegate SectionPreDelete
+                 * @since Symphony 2.2
+                 * @param string $context
+                 * '/blueprints/sections/'
+                 * @param array $section_ids
+                 *  An array of Section ID's passed by reference
+                 */
+                Symphony::ExtensionManager()->notifyMembers('SectionPreDelete', '/blueprints/sections/', array('section_ids' => &$section_id));
 
-            foreach ($section_id as $section) {
-                SectionManager::delete($section);
+                foreach ($section_id as $section) {
+                    SectionManager::delete($section);
+                }
+
+                redirect(SYMPHONY_URL . '/blueprints/sections/');
+            } else {
+                $this->addTimestampValidationPageAlert(
+                    $this->_errors['timestamp'],
+                    SectionManager::fetch($section_id),
+                    'delete'
+                );
             }
-
-            redirect(SYMPHONY_URL . '/blueprints/sections/');
         }
     }
 
@@ -931,5 +959,36 @@ class contentBlueprintsSections extends AdministrationPage
             'meta' => &$meta,
             'errors' => &$this->_errors
         ));
+    }
+
+    /**
+     * Given $_POST values, this function will validate the current timestamp
+     * and set the proper error messages.
+     *
+     * @since Symphony 2.7.0
+     * @param  int $section_id
+     *   The entry id to validate
+     * @return boolean
+     *   True if the timestamp is valid
+     */
+    protected function validateTimestamp($section_id, $checkMissing = false)
+    {
+        if (!isset($_POST['action']['ignore-timestamp'])) {
+            if ($checkMissing && !isset($_POST['action']['timestamp'])) {
+                if (isset($this->_errors) && is_array($this->_errors)) {
+                    $this->_errors['timestamp'] = __('The section could not be saved due to conflicting changes');
+                }
+                return false;
+            } elseif (isset($_POST['action']['timestamp'])) {
+                $tv = new TimestampValidator('sections');
+                if (!$tv->check($section_id, $_POST['action']['timestamp'])) {
+                    if (isset($this->_errors) && is_array($this->_errors)) {
+                        $this->_errors['timestamp'] = __('The section could not be saved due to conflicting changes');
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }

install/includes/install.sql
@@ -229,8 +229,17 @@ CREATE TABLE `tbl_sections` (
   `hidden` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
   `filter` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes',
   `navigation_group` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Content',
+  `author_id` int(11) unsigned NOT NULL DEFAULT 1,
+  `creation_date` datetime NOT NULL,
+  `creation_date_gmt` datetime NOT NULL,
+  `modification_date` datetime NOT NULL,
+  `modification_date_gmt` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `handle` (`handle`)
+  UNIQUE KEY `handle` (`handle`),
+  KEY `creation_date` (`creation_date`),
+  KEY `creation_date_gmt` (`creation_date_gmt`),
+  KEY `modification_date` (`modification_date`),
+  KEY `modification_date_gmt` (`modification_date_gmt`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- *** STRUCTURE: `tbl_sections_association` ***

install/migrations/2.7.0.php
@@ -4,7 +4,7 @@
     {
         public static function getVersion()
         {
-            return '2.7.0.beta2';
+            return '2.7.0.beta3';
         }
 
         public static function getReleaseNotes()
@@ -25,6 +25,24 @@
                 ');
             } catch (Exception $ex) {
                 // ignore
+            }
+
+            if (version_compare(Symphony::Configuration()->get('version', 'symphony'), '2.7.0.beta3', '<')) {
+                // Add dates and author columns
+                $now = DateTimeObj::get('Y-m-d H:i:s');
+                $nowGMT = DateTimeObj::getGMT('Y-m-d H:i:s');
+                Symphony::Database()->query("
+                    ALTER TABLE `tbl_sections`
+                        ADD COLUMN `author_id` int(11) unsigned NOT NULL DEFAULT 1,
+                        ADD COLUMN `creation_date` datetime NOT NULL DEFAULT '$now',
+                        ADD COLUMN `creation_date_gmt` datetime NOT NULL DEFAULT '$nowGMT',
+                        ADD COLUMN `modification_date` datetime NOT NULL DEFAULT '$now',
+                        ADD COLUMN `modification_date_gmt` datetime NOT NULL DEFAULT '$nowGMT',
+                        ADD KEY `creation_date` (`creation_date`),
+                        ADD KEY `creation_date_gmt` (`creation_date_gmt`),
+                        ADD KEY `modification_date` (`modification_date`),
+                        ADD KEY `modification_date_gmt` (`modification_date_gmt`);
+                ");
             }
 
             // Update the version information

symphony/assets/js/src/backend.views.js
@@ -181,6 +181,21 @@ Symphony.View.add('/:context*:', function() {
 		}
 	});
 
+	// Timestamp validation overwrite
+	Symphony.Elements.header.find('.notifier .js-tv-overwrite').on('click.admin', function (e){
+		var action = $(this).attr('data-action') || 'save';
+		var hidden = Symphony.Elements.contents.find('input[name="action[ignore-timestamp]"]');
+		hidden.prop('checked', true);
+		e.preventDefault();
+		e.stopPropagation();
+		e.stopImmediatePropagation();
+		// Click on the action button:
+		// This is needed since we need to send the button's data in the POST
+		Symphony.Elements.contents.find('> form').find('input, button')
+			.filter('[name="action[' + action + ']"]').first().click();
+		return false;
+	});
+
 	// Catch all JavaScript errors and write them to the Symphony Log
 	Symphony.Elements.window.on('error.admin', function(event) {
 		$.ajax({

symphony/content/content.publish.php
@@ -1200,6 +1200,10 @@ class contentPublish extends AdministrationPage
             $entry->set('modification_date', $existingEntry->get('modification_date'));
             $entry->setDataFromPost($fields, $errors, true);
 
+            $timestamp = isset($_POST['action']['timestamp'])
+                ? $_POST['action']['timestamp']
+                : $entry->get('modification_date');
+
             // Editing an entry, so need to create some various objects
         } else {
             $entry = $existingEntry;
@@ -1208,6 +1212,8 @@ class contentPublish extends AdministrationPage
             if (!$section) {
                 $section = SectionManager::fetch($entry->get('section_id'));
             }
+
+            $timestamp = $entry->get('modification_date');
         }
 
         /**
@@ -1365,6 +1371,9 @@ class contentPublish extends AdministrationPage
             $button->setAttributeArray(array('name' => 'action[delete]', 'class' => 'button confirm delete', 'title' => __('Delete this entry'), 'type' => 'submit', 'accesskey' => 'd', 'data-message' => __('Are you sure you want to delete this entry?')));
             $div->appendChild($button);
 
+            $div->appendChild(Widget::Input('action[timestamp]', $timestamp, 'hidden'));
+            $div->appendChild(Widget::Input('action[ignore-timestamp]', 'yes', 'checkbox', array('class' => 'irrelevant')));
+
             $this->Form->appendChild($div);
 
             // Create a Drawer for Associated Sections
@@ -1376,7 +1385,7 @@ class contentPublish extends AdministrationPage
     {
         $entry_id = intval($this->_context['entry_id']);
 
-        if (is_array($_POST['action']) && (array_key_exists('save', $_POST['action']) || array_key_exists("done", $_POST['action']))) {
+        if (is_array($_POST['action']) && (array_key_exists('save', $_POST['action']) || array_key_exists('done', $_POST['action']))) {
             if (!$ret = EntryManager::fetch($entry_id)) {
                 Administration::instance()->throwCustomError(
                     __('The Entry, %s, could not be found.', array($entry_id)),
@@ -1392,12 +1401,18 @@ class contentPublish extends AdministrationPage
             $post = General::getPostData();
             $fields = $post['fields'];
 
-            // Initial checks to see if the Entry is ok
-            if (Entry::__ENTRY_FIELD_ERROR__ == $entry->checkPostData($fields, $this->_errors)) {
+            $canProceed = $this->validateTimestamp($entry_id, true);
+
+            // Timestamp validation
+            if (!$canProceed) {
+                $this->addTimestampValidationPageAlert($this->_errors['timestamp'], $entry, 'save');
+
+                // Initial checks to see if the Entry is ok
+            } else if (Entry::__ENTRY_FIELD_ERROR__ == $entry->checkPostData($fields, $this->_errors)) {
                 $this->pageAlert(__('Some errors were encountered while attempting to save.'), Alert::ERROR);
 
                 // Secondary checks, this will actually process the data and attempt to save
-            } elseif (Entry::__ENTRY_OK__ != $entry->setDataFromPost($fields, $errors)) {
+            } else if (Entry::__ENTRY_OK__ != $entry->setDataFromPost($fields, $errors)) {
                 foreach ($errors as $field_id => $message) {
                     $this->pageAlert($message, Alert::ERROR);
                 }
@@ -1456,22 +1471,31 @@ class contentPublish extends AdministrationPage
             $checked = array($entry_id);
             Symphony::ExtensionManager()->notifyMembers('EntryPreDelete', '/publish/', array('entry_id' => &$checked));
 
-            EntryManager::delete($checked);
+            $canProceed = $this->validateTimestamp($entry_id);
 
-            /**
-             * After the deletion of entries, this delegate provides an array of Entry ID's
-             * that were deleted.
-             *
-             * @since Symphony 2.3
-             * @delegate EntryPostDelete
-             * @param string $context
-             * '/publish/'
-             * @param array $entry_id
-             *  An array of Entry ID's that were deleted.
-             */
-            Symphony::ExtensionManager()->notifyMembers('EntryPostDelete', '/publish/', array('entry_id' => $checked));
+            if ($canProceed) {
+                EntryManager::delete($checked);
 
-            redirect(SYMPHONY_URL . '/publish/'.$this->_context['section_handle'].'/');
+                /**
+                 * After the deletion of entries, this delegate provides an array of Entry ID's
+                 * that were deleted.
+                 *
+                 * @since Symphony 2.3
+                 * @delegate EntryPostDelete
+                 * @param string $context
+                 * '/publish/'
+                 * @param array $entry_id
+                 *  An array of Entry ID's that were deleted.
+                 */
+                Symphony::ExtensionManager()->notifyMembers('EntryPostDelete', '/publish/', array('entry_id' => $checked));
+
+                redirect(SYMPHONY_URL . '/publish/'.$this->_context['section_handle'].'/');
+            } else {
+                if (is_array($ret = EntryManager::fetch($entry_id))) {
+                    $entry = $ret[0];
+                    $this->addTimestampValidationPageAlert($this->_errors['timestamp'], $entry, 'delete');
+                }
+            }
         }
     }
 
@@ -1877,5 +1901,36 @@ class contentPublish extends AdministrationPage
         $filter_querystring = preg_replace("/&amp;$/", '', $filter_querystring);
 
         return $filter_querystring ? '?' . $filter_querystring : null;
+    }
+
+    /**
+     * Given $_POST values, this function will validate the current timestamp
+     * and set the proper error messages.
+     *
+     * @since Symphony 2.7.0
+     * @param int $entry_id
+     *  The entry id to validate
+     * @return boolean
+     *  True if the timestamp is valid
+     */
+    protected function validateTimestamp($entry_id, $checkMissing = false)
+    {
+        if (!isset($_POST['action']['ignore-timestamp'])) {
+            if ($checkMissing && !isset($_POST['action']['timestamp'])) {
+                if (isset($this->_errors) && is_array($this->_errors)) {
+                    $this->_errors['timestamp'] = __('The entry could not be saved due to conflicting changes');
+                }
+                return false;
+            } elseif (isset($_POST['action']['timestamp'])) {
+                $tv = new TimestampValidator('entries');
+                if (!$tv->check($entry_id, $_POST['action']['timestamp'])) {
+                    if (isset($this->_errors) && is_array($this->_errors)) {
+                        $this->_errors['timestamp'] = __('The entry could not be saved due to conflicting changes');
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }

symphony/lib/toolkit/class.administrationpage.php
@@ -1371,4 +1371,54 @@ class AdministrationPage extends HTMLPage
 
         $this->Header->appendChild($ul);
     }
+
+    /**
+     * Adds a localized Alert message for failed timestamp validations.
+     * It also adds meta information about the last author and timestamp.
+     *
+     * @since Symphony 2.7.0
+     * @param string $errorMessage
+     *  The error message to display.
+     * @param Entry|Section $existingObject
+     *  The Entry or section object that failed validation.
+     * @param string $action
+     *  The requested action.
+     */
+    public function addTimestampValidationPageAlert($errorMessage, $existingObject, $action)
+    {
+        $author = AuthorManager::fetchByID($existingObject->get('author_id'));
+
+        $formatteAuthorName = $existingObject->get('author_id') === Symphony::Author()->get('id')
+            ? __('yourself')
+            : (!$author
+                ? __('an unknown user')
+                : $author->get('first_name') . ' ' . $author->get('last_name'));
+
+        $msg = $this->_errors['timestamp'] . ' ' . __(
+            'made by %s at %s.', array(
+                $formatteAuthorName,
+                Widget::Time($existingObject->get('modification_date'))->generate(),
+            )
+        );
+
+        $currentUrl = Administration::instance()->getCurrentPageURL();
+        $overwritelink = Widget::Anchor(
+            __('Replace changes?'),
+            $currentUrl,
+            __('Overwrite'),
+            'js-tv-overwrite',
+            null,
+            array(
+                'data-action' => General::sanitize($action)
+            )
+        );
+        $ignorelink = Widget::Anchor(
+            __('View changes.'),
+            $currentUrl,
+            __('View the updated entry')
+        );
+        $actions = $overwritelink->generate() . ' ' . $ignorelink->generate();
+        
+        $this->pageAlert("$msg $actions", Alert::ERROR);
+    }
 }

symphony/lib/toolkit/class.field.php
@@ -1719,7 +1719,7 @@ class Field
         } elseif ($id = FieldManager::add($fields)) {
             $this->set('id', $id);
             if ($this->requiresTable()) {
-                $this->createTable();
+                return $this->createTable();
             }
             return true;
         }
@@ -1769,6 +1769,42 @@ class Field
     public function requiresTable()
     {
         return true;
+    }
+
+    /**
+     * Checks that we are working with a valid field handle and
+     * that the setting table exists.
+     *
+     * @since Symphony 2.7.0
+     * @return boolean
+     *   true if the table exists, false otherwise
+     */
+    public function tableExists()
+    {
+        if (!$this->_handle) {
+            return false;
+        }
+        return Symphony::Database()->tableExists('tbl_fields_' . $this->_handle);
+    }
+
+    /**
+     * Checks that we are working with a valid field handle and field id, and
+     * checks that the field record exists in the settings table.
+     *
+     * @since Symphony 2.7.0
+     * @return boolean
+     *   true if the field id exists in the table, false otherwise
+     */
+    public function exists()
+    {
+        if (!$this->get('id') || !$this->_handle) {
+            return false;
+        }
+        return !empty(Symphony::Database()->fetch(sprintf(
+            'SELECT `id` FROM `tbl_fields_%s` WHERE `field_id` = %d',
+            $this->_handle,
+            General::intval($this->get('id'))
+        )));
     }
 
     /**

symphony/lib/toolkit/class.sectionmanager.php
@@ -34,6 +34,11 @@ class SectionManager
      */
     public static function add(array $settings)
     {
+        $defaults = array();
+        $defaults['creation_date'] = $defaults['modification_date'] = DateTimeObj::get('Y-m-d H:i:s');
+        $defaults['creation_date_gmt'] = $defaults['modification_date_gmt'] = DateTimeObj::getGMT('Y-m-d H:i:s');
+        $defaults['author_id'] = '1';
+        $settings = array_replace($defaults, $settings);
         if (!Symphony::Database()->insert($settings, 'tbl_sections')) {
             return false;
         }
@@ -57,6 +62,11 @@ class SectionManager
      */
     public static function edit($section_id, array $settings)
     {
+        $defaults = array();
+        $defaults['modification_date'] = DateTimeObj::get('Y-m-d H:i:s');
+        $defaults['modification_date_gmt'] = DateTimeObj::getGMT('Y-m-d H:i:s');
+        $defaults['author_id'] = 1;
+        $settings = array_replace($defaults, $settings);
         if (!Symphony::Database()->update($settings, 'tbl_sections', sprintf(" `id` = %d", $section_id))) {
             return false;
         }
@@ -175,6 +185,14 @@ class SectionManager
 
             foreach ($s as $name => $value) {
                 $obj->set($name, $value);
+            }
+
+            $obj->set('creation_date', DateTimeObj::get('c', $obj->get('creation_date')));
+
+            if (!empty($obj->get('modification_date'))) {
+                $obj->set('modification_date', DateTimeObj::get('c', $obj->get('modification_date')));
+            } else {
+                $obj->set('modification_date', $obj->get('creation_date'));
             }
 
             self::$_pool[$obj->get('id')] = $obj;

commit 80e00a591ff13a6359f6cfc79cc4655b7583f4d4

Fix broken pass by ref in SectionPreDelete (#2743)
This bug was introduced in e5ecf3cbd346f885ce50708dc7ef42196556ca3d

* Fixes breaking change in hotfix 	b1bcff2
* Reduces cyclomatic complexity   	f33e57e

symphony/content/content.blueprintssections.php
@@ -891,29 +891,34 @@ class contentBlueprintsSections extends AdministrationPage
             $section_id = $this->_context[1];
             $canProceed = $this->validateTimestamp($section_id);
 
-            if ($canProceed) {
-                /**
-                 * Just prior to calling the Section Manager's delete function
-                 *
-                 * @delegate SectionPreDelete
-                 * @since Symphony 2.2
-                 * @param string $context
-                 * '/blueprints/sections/'
-                 * @param array $section_ids
-                 *  An array of Section ID's passed by reference
-                 */
-                Symphony::ExtensionManager()->notifyMembers('SectionPreDelete', '/blueprints/sections/', array('section_ids' => array(&$section_id)));
-
-                SectionManager::delete($section_id);
-
-                redirect(SYMPHONY_URL . '/blueprints/sections/');
-            } else {
+            if (!$canProceed) {
                 $this->addTimestampValidationPageAlert(
                     $this->_errors['timestamp'],
                     SectionManager::fetch($section_id),
                     'delete'
                 );
+                return;
             }
+
+            $section_ids = array($section_id);
+
+            /**
+             * Just prior to calling the Section Manager's delete function
+             *
+             * @delegate SectionPreDelete
+             * @since Symphony 2.2
+             * @param string $context
+             * '/blueprints/sections/'
+             * @param array $section_ids
+             *  An array of Section ID's passed by reference
+             */
+            Symphony::ExtensionManager()->notifyMembers('SectionPreDelete', '/blueprints/sections/', array('section_ids' => &$section_ids));
+
+            foreach ($section_ids as $section) {
+                SectionManager::delete($section);
+            }
+
+            redirect(SYMPHONY_URL . '/blueprints/sections/');
         }
     }
 