0614ff538bcf966ef6d5952cccde393d707d46e1
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-02-22 15:23:59 -0500

    Redistribute responsabilities between Page/Process
    We used to store the xml and xsl values both in the XSLTPage AND in the
    XSLTProcess. This is redundant and totally not needed.
    This change makes only the Page responsible to hold the xml and xslt
    data, which are simply passed when the XSLTProcess needs to do it job.
    
    The only reason why the Process would need to hold the data is when
    there is an error. For this purpose, the "context" is managed manually
    instead of exposing it publicly.
    
    On the other hand, the Page was holding data belonging to the Process:
    params and functions. Since the page does not use the parameters anymore
    and it does not register any functions, this data has been moved to the
    process. Upon instanciation, the Page gets a process, so developers can
    just forward their Page calss to Page->Proc.
    
    The end goal was to not expose any XSLT parameters or function in the
    XSLTPage, which is now acheived.

--- symphony/lib/toolkit/class.xsltpage.php
+++ symphony/lib/toolkit/class.xsltpage.php
@@ -30,20 +30,6 @@ class XSLTPage extends Page
     protected $_xsl;
 
     /**
-     * An array of all the parameters to be made available during the XSLT
-     * transform
-     * @var array
-     */
-    protected $_param = array();
-
-    /**
-     * An array of the PHP functions to be made available during the XSLT
-     * transform
-     * @var array
-     */
-    protected $_registered_php_functions = array();
-
-    /**
      * The constructor for the `XSLTPage` ensures that an `XSLTProcessor`
      * is available, and then sets an instance of it to `$this->Proc`, otherwise
      * it will throw a `SymphonyErrorPage` exception.
@@ -104,22 +90,6 @@ class XSLTPage extends Page
     }
 
     /**
-     * Sets the parameters that will output with the resulting page
-     * and be accessible in the XSLT. This function translates all ' into
-     * `&apos;`, with the tradeoff being that a <xsl:value-of select='$param' />
-     * that has a ' will output `&apos;` but the benefit that ' and " can be
-     * in the params
-     *
-     * @link http://www.php.net/manual/en/xsltprocessor.setparameter.php#81077
-     * @param array $param
-     *  An associative array of params for this page
-     */
-    public function setRuntimeParam($param)
-    {
-        $this->_param = str_replace("'", "&apos;", $param);
-    }
-
-    /**
      * Returns an iterator of errors from the `XsltProcess`. Use this function
      * inside a loop to get all the errors that occurring when transforming
      * `$this->_xml` with `$this->_xsl`.
@@ -134,25 +104,6 @@ class XSLTPage extends Page
     }
 
     /**
-     * Allows the registration of PHP functions to be used on the Frontend
-     * by passing the function name or an array of function names
-     *
-     * @param mixed $function
-     *  Either an array of function names, or just the function name as a
-     *  string
-     */
-    public function registerPHPFunction($function)
-    {
-        if (is_array($function)) {
-            $this->_registered_php_functions = array_unique(
-              array_merge($this->_registered_php_functions, $function)
-            );
-        } else {
-            $this->_registered_php_functions[] = $function;
-        }
-    }
-
-    /**
      * The generate function calls on the `XsltProcess` to transform the
      * XML with the given XSLT passing any parameters or functions
      * If no errors occur, the parent generate function is called to add
@@ -164,7 +115,7 @@ class XSLTPage extends Page
      */
     public function generate($page = null)
     {
-        $result = $this->Proc->process($this->_xml, $this->_xsl, $this->_param, $this->_registered_php_functions);
+        $result = $this->Proc->process($this->_xml, $this->_xsl);
 
         if ($this->Proc->isErrors()) {
             $this->setHttpStatus(Page::HTTP_STATUS_ERROR);
