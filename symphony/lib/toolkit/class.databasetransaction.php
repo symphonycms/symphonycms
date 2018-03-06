7e4e8c49713478df8e3d16611d53b40cc3b3dc1c
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-03-06 00:01:10 -0500

    Add DatabaseTransaction as first class citizen
    In order to make it easier to use transaction, this commits adds new
    classes to be able to use the same construct (API) as all the other
    DatabaseStatements. It provide a DatabaseTransaction class (analogous to
    DatabaseStatement) and DatabaseTransactionResult class (analogous to
    DatabaseStatementResult).
    
    Picked from 85731828d7
    Picked from 5d621563c5
    Picked from cf73a208d9

---
+++ symphony/lib/toolkit/class.databasetransaction.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @package toolkit
+ */
+
+/**
+ * This class holds all the required data to facilitate management of database transactions.
+ *
+ * @see DatabaseTransactionException
+ */
+class DatabaseTransaction
+{
+    /**
+     * Database object reference
+     * @var Database
+     */
+    private $db;
+
+    /**
+     * The code to execute in the transaction
+     * @var callable
+     */
+    private $tx;
+
+    /**
+     * Creates a new DatabaseStatement object, linked to the $db parameter
+     * and containing the optional $statement.
+     *
+     * @param Database $db
+     *  The Database reference
+     * @param callable $tx
+     *  The code to execute in the transaction
+     */
+    public function __construct(Database $db, $tx)
+    {
+        if (!is_callable($tx)) {
+            throw new DatabaseTransactionException('Parameter $tx must be callable');
+        }
+        $this->db = $db;
+        $this->tx = $tx;
+    }
+
+    /**
+     * Getter for the underlying database object.
+     *
+     * @return Database
+     */
+    final protected function getDB()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Send the transaction operation to the  the server for execution.
+     *
+     * @return DatabaseTransactionResult
+     * @throws DatabaseTransactionException
+     */
+    final public function execute()
+    {
+        $success = false;
+        if (!$this->getDB()->beginTransaction()) {
+            throw new DatabaseTransactionException('Failed to begin the transaction');
+        }
+        try {
+            call_user_func($this->tx);
+            $success = $this->getDB()->commit();
+        } catch (Throwable $ex) {
+            $this->getDB()->rollBack();
+            throw $ex;
+        }
+        return new DatabaseTransactionResult($success);
+    }
+}
---
+++ tests/lib/toolkit/DatabaseTransactionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers DatabaseTransaction
+ */
+final class DatabaseTransactionTest extends TestCase
+{
+    private $db;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->db = $this->createMock(Database::class);
+        $this->db
+            ->method('beginTransaction')
+            ->willReturn(true);
+        $this->db
+            ->method('commit')
+            ->willReturn(true);
+        $this->db
+            ->method('rollBack')
+            ->willReturn(true);
+    }
+
+    public function testExecute()
+    {
+        $called = false;
+        $tx = function () use (&$called) {
+            $called = true;
+        };
+        $tx = new DatabaseTransaction($this->db, $tx);
+        $ret = $tx->execute();
+        $this->assertEquals(true, $ret->success(), 'The transaction is successful');
+        $this->assertEquals(true, $called, 'The transaction callable was called');
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testRollback()
+    {
+        $tx = function () {
+            throw new Exception('Crashed!');
+        };
+        $tx = new DatabaseTransaction($this->db, $tx);
+        $ret = $tx->execute();
+        $this->assertEquals(false, $ret->success(), 'The transaction is unsuccessful');
+    }
+}
