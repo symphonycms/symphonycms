b0c6cfd5639a1e12257163c77c3f3106c52a1f2f
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-03-30 14:49:43 -0400

    Add EQFA for Author Field
    The new implementation supports exact match, 'regexp:', 'sql:'
    and 'author-id:' filtering modes.
    
    The old implementation simply converted numeric values in 'and'
    operation to a check on author ids. This is not supported anymore.
    
    Picked from b7cff7baa7

---
+++ symphony/lib/toolkit/class.entryqueryauthoradapter.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ * @package toolkit
+ */
+/**
+ * Specialized EntryQueryFieldAdapter that facilitate creation of queries filtering/sorting data from
+ * an author Field.
+ * @see FieldAuthor
+ * @since Symphony 3.0.0
+ */
+class EntryQueryAuthorAdapter extends EntryQueryFieldAdapter
+{
+    /**
+     * Since this class includes the fixed alias in column names,
+     * this implementation simply returns the $col value.
+     *
+     * @see getFilterColumns()
+     * @see getSortColumns()
+     * @param string $col
+     *  The column name
+     * @param int $field_id
+     *  The field id
+     * @return string
+     *  The aliased column name
+     */
+    public function formatColumn($col, $field_id)
+    {
+        return $col;
+    }
+
+    /**
+     * @internal Returns the columns to use when filtering
+     *
+     * @return array
+     */
+    public function getFilterColumns()
+    {
+        return [
+            'af.username',
+            'af.first_name',
+            'af.last_name',
+            'af.full_name',
+        ];
+    }
+
+    /**
+     * Test whether the input string is a author id filter, i.e. `author-id:`.
+     *
+     * @param string $string
+     *  The string to test.
+     * @return boolean
+     *  true if the string is prefixed with `author-id:`, false otherwise.
+     */
+    public function isFilterAuthorId($string)
+    {
+        if (preg_match('/^author-id:\s*/i', $string)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Builds a simply author id filter.
+     *
+     * @param string $filter
+     *  The full filter string
+     * @param array $columns
+     *  The array of columns that need the given `$filter` applied to.
+     *  The conditions will be added using `AND`.
+     * @return void
+     */
+    public function createFilterAuthorId($filter, array $columns)
+    {
+        $field_id = General::intval($this->field->get('id'));
+        $filter = preg_replace('/^author-id:\s*/i', null, $filter);
+        $filter = $this->field->cleanValue($filter);
+        $op = '=';
+
+        $conditions = [];
+        foreach ($columns as $key => $col) {
+            $conditions[] = [$this->formatColumn($col, $field_id) => [$op => $filter]];
+        }
+        if (count($conditions) < 2) {
+            return $conditions;
+        }
+        return ['and' => $conditions];
+    }
+
+    /**
+     * Appends the required WHERE clauses based on the requested filters and the reference field.
+     * Filters allows developers to filter using a simplified syntax that is primarily used
+     * in data sources.
+     *
+     * This implementation supports exact match, 'regexp:', 'sql:' and
+     * 'author-id:' filtering modes.
+     *
+     * @see EntryQuery::filter()
+     * @uses EntryQuery::whereField()
+     * @param EntryQuery $query
+     *  The EntryQuery to operate on
+     * @param array $filters
+     *  The array of all filter values
+     * @param string $operator
+     *  The operation to use when there are multiple filters
+     * @return void
+     * @throws DatabaseStatementException
+     */
+    public function filter(EntryQuery $query, array $filters, $operator = 'or')
+    {
+        General::ensureType([
+            'operator' => ['var' => $operator, 'type' => 'string'],
+        ]);
+        $field_id = General::intval($this->field->get('id'));
+        $projectionAdded = false;
+        $addProjection = function () use ($projectionAdded, $query) {
+            if ($projectionAdded) {
+                return;
+            }
+            $query->projection([
+                'CONCAT_WS(:af_space_char, af.first_name, af.last_name)' => 'af.full_name'
+            ])->setValue('af_space_char', ' ');
+            $projectionAdded = true;
+        };
+        $conditions = [$operator => []];
+        foreach ($filters as $filter) {
+            $fc = null;
+            if ($this->isFilterAuthorId($filter)) {
+                $fc = $this->createFilterAuthorId($filter, [parent::formatColumn('author_id', $field_id)]);
+            } elseif ($this->isFilterRegex($filter)) {
+                $addProjection();
+                $fc = $this->createFilterRegexp($filter, $this->getFilterColumns());
+            } elseif ($this->isFilterSQL($filter)) {
+                $addProjection();
+                $fc = $this->createFilterSQL($filter, $this->getFilterColumns());
+            } else {
+                $addProjection();
+                $fc = $this->createFilterEquality($filter, $this->getFilterColumns());
+            }
+            if (is_array($fc)) {
+                $conditions[$operator][] = $fc;
+            }
+        }
+        // Remove extra ()
+        if (count($conditions) === 1) {
+            $conditions = current($conditions);
+        }
+        $query->whereField($field_id, $conditions);
+    }
+
+    /**
+     * @internal Returns the columns to use when sorting
+     *
+     * @return array
+     */
+    public function getSortColumns()
+    {
+        return ['as.first_name', 'as.last_name'];
+    }
+
+    /**
+     * Appends the required ORDER BY clauses based on the requested sort and the reference author field.
+     * It will append also join the `tbl_authors` table to be able to filter on actual text values.
+     * In order to make MySQL strict mode work, values used in sort are added to the projection.
+     *
+     * This implementation supports 'asc', 'desc', 'random' sort directions.
+     *
+     * @param EntryQuery $query
+     *  The EntryQuery to operate on
+     * @param string $direction
+     *  The default direction to use.
+     *  Supports ASC, DESC and RAND
+     *  Defaults to ASC.
+     * @return void
+     * @throws DatabaseStatementExceptions
+     */
+    public function sort(EntryQuery $query, $direction = 'ASC')
+    {
+        General::ensureType([
+            'direction' => ['var' => $direction, 'type' => 'string'],
+        ]);
+        $field_id = General::intval($this->field->get('id'));
+        if ($this->isRandomOrder($direction)) {
+            $query->orderBy('RAND()');
+            return;
+        }
+        $query->leftJoinField($field_id)
+            ->innerJoin('tbl_authors', 'as')
+            ->on(["f{$field_id}.author_id" => '$as.id'])
+            ->projection($this->getSortColumns());
+        foreach ($this->getSortColumns() as $column) {
+            $query->orderBy($column, $direction);
+        }
+    }
+}
---
+++ tests/lib/toolkit/EntryQueryAuthorAdapterTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Symphony\DAL\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers EntryQueryAuthorAdapter
+ */
+final class EntryQueryAuthorAdapterTest extends TestCase
+{
+    private $db;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->db = new \Database();
+    }
+
+    /**
+     * @param Field $f
+     * @return EntryQueryAuthorAdapter
+     */
+    private function createAdapter(Field $f = null)
+    {
+        if (!$f) {
+            $f = new \FieldAuthor();
+            $f->set('id', 1);
+        }
+        return new \EntryQueryAuthorAdapter($f);
+    }
+
+    public function testExactFilter()
+    {
+        $q = (new \EntryQuery($this->db));
+        $o = $this->createAdapter();
+        $o->filter($q, ['test']);
+        $this->assertEquals(
+            "SELECT SQL_NO_CACHE CONCAT_WS(:af_space_char, `af`.`first_name`, `af`.`last_name`) AS `af`.`full_name` FROM `entries` AS `e` LEFT JOIN `entries_data_1` AS `f1` ON `e`.`id` = `f1`.`entry_id` WHERE (`af`.`username` = :af_username OR `af`.`first_name` = :af_first_name OR `af`.`last_name` = :af_last_name OR `af`.`full_name` = :af_full_name)",
+            $q->generateSQL(),
+            'Simple exact match ->filter([test])'
+        );
+        $values = $q->getValues();
+        $this->assertEquals('test', $values['af_first_name'], 'af_first_name is test');
+        $this->assertEquals('test', $values['af_last_name'], 'af_last_name is test');
+        $this->assertEquals('test', $values['af_full_name'], 'af_full_name is test');
+        $this->assertEquals('test', $values['af_username'], 'af_username is test');
+        $this->assertEquals(' ', $values['af_space_char'], 'af_space_char is space');
+        $this->assertEquals(5, count($values), '5 values');
+    }
+
+    public function testAuthorIdFilter()
+    {
+        $q = (new \EntryQuery($this->db));
+        $o = $this->createAdapter();
+        $o->filter($q, ['author-id: 4']);
+        $this->assertEquals(
+            "SELECT SQL_NO_CACHE FROM `entries` AS `e` LEFT JOIN `entries_data_1` AS `f1` ON `e`.`id` = `f1`.`entry_id` WHERE `f1`.`author_id` = :f1_author_id",
+            $q->generateSQL(),
+            'Simple exact match ->filter([test])'
+        );
+        $values = $q->getValues();
+        $this->assertEquals('4', $values['f1_author_id'], 'f1_author_id is 4');
+        $this->assertEquals(1, count($values), '1 value');
+    }
+
+    public function testSortAsc()
+    {
+        $q = (new \EntryQuery($this->db));
+        $o = $this->createAdapter();
+        $o->sort($q, 'asc');
+        $this->assertEquals(
+            "SELECT SQL_NO_CACHE `as`.`first_name`, `as`.`last_name` FROM `entries` AS `e` LEFT JOIN `entries_data_1` AS `f1` ON `e`.`id` = `f1`.`entry_id` INNER JOIN `authors` AS `as` ON `f1`.`author_id` = `as`.`id` ORDER BY `as`.`first_name` ASC , `as`.`last_name` ASC",
+            $q->generateSQL(),
+            'Simple asc sort ->sort(asc)'
+        );
+        $values = $q->getValues();
+        $this->assertEquals(0, count($values), '0 value');
+    }
+}
