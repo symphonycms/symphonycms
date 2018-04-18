symphonycms/symphonycms

commit b7e0b7415fd4af941a5a15b3dd6b3244c7c353f3

Double encode value in displayPublishPanel() (#2822)
This makes sure the user will actually see the entity.

Picked from 28f5696
Fixes #2819

symphony/lib/toolkit/class.general.php
@@ -14,6 +14,7 @@ class General
      * Symphony 2.3, this function assumes UTF-8 and will not double
      * encode strings.
      *
+     * @uses htmlspecialchars()
      * @param string $source
      *  a string to operate on.
      * @return string
@@ -22,6 +23,24 @@ class General
     public static function sanitize($source)
     {
         $source = htmlspecialchars($source, ENT_COMPAT, 'UTF-8', false);
+
+        return $source;
+    }
+
+    /**
+     * Convert any special characters into their entity equivalents.
+     * Contrary to `sanitize()`, this version does double encode existing entities.
+     *
+     * @since Symphony 2.7.5
+     * @uses htmlspecialchars()
+     * @param string $source
+     *  a string to operate on.
+     * @return string
+     *  the fully encoded version of the string.
+     */
+    public static function sanitizeDouble($source)
+    {
+        $source = htmlspecialchars($value, ENT_COMPAT, 'UTF-8', true);
 
         return $source;
     }

symphony/lib/toolkit/fields/field.textarea.php
@@ -151,7 +151,7 @@ class fieldTextarea extends Field implements ExportableField, ImportableField
         }
 
         $value = isset($data['value']) ? $data['value'] : null;
-        $textarea = Widget::Textarea('fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix, (int)$this->get('size'), 50, (strlen($value) != 0 ? General::sanitize($value) : null));
+        $textarea = Widget::Textarea('fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix, (int)$this->get('size'), 50, (strlen($value) != 0 ? General::sanitizeDouble($value) : null));
 
         if ($this->get('formatter') != 'none') {
             $textarea->setAttribute('class', $this->get('formatter'));

commit 8d218d7f0a1307f8605fb750a419e786cbda9e78

Rename the undefined var (#2823)
Fixes #2824 which is a regression from #2822

symphony/lib/toolkit/class.general.php
@@ -40,7 +40,7 @@ class General
      */
     public static function sanitizeDouble($source)
     {
-        $source = htmlspecialchars($value, ENT_COMPAT, 'UTF-8', true);
+        $source = htmlspecialchars($source, ENT_COMPAT, 'UTF-8', true);
 
         return $source;
     }