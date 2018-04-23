d46883ce3e4cdc4a0a433fd270274a30a9235c02
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-04-23 18:09:15 -0400

    Add a test boot script
    This is needed to make sure we can run the test without creating a
    formal config.php file.
    
    It also make sure VERSION is defined.
    
    Picked from 095bc39b7b
    Picked from 985d967e51

---
+++ tests/boot.php
@@ -0,0 +1,15 @@
+<?php
+
+// Define version
+$package = json_decode(file_get_contents('composer.json'));
+define_safe('VERSION', $package->version);
+
+// Insure a manifest directory
+if (!file_exists(MANIFEST)) {
+    mkdir(MANIFEST);
+}
+
+// Insure a config file
+if (!file_exists(CONFIG)) {
+    copy(INSTALL . '/includes/config_default.php', CONFIG);
+}
