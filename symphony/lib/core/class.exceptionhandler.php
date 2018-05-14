88aaa4a891bbad31f5f57602df96e002a186968d
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-05-14 17:40:35 -0400

    Extract ExceptionRenderer from ExceptionHandler
    We can split the responsabilities into seperate classes.
    
    Picked from 81f3f021e4

--- symphony/lib/core/class.exceptionhandler.php
+++ symphony/lib/core/class.exceptionhandler.php
@@ -1,4 +1,4 @@
-i<?php
+<?php
 /**
  * @package core
  */
@@ -7,8 +7,8 @@ i<?php
  * ExceptionHandler will handle any uncaught exceptions thrown in
  * Symphony. Additionally, all errors in Symphony that are raised to Exceptions
  * will be handled by this class.
- * It is possible for Exceptions to be caught by their own `ExceptionHandler` which can
- * override the `render` function so that it can be displayed to the user appropriately.
+ * It is possible for Exceptions to be caught by their own `ExceptionRenderer` which can
+ * provide the `render` function so that it can be displayed to the user appropriately.
  */
 class ExceptionHandler
 {
@@ -66,28 +66,6 @@ class ExceptionHandler
     }
 
     /**
-     * Retrieves a window of lines before and after the line where the error
-     * occurred so that a developer can help debug the exception
-     *
-     * @param integer $line
-     *  The line where the error occurred.
-     * @param string $file
-     *  The file that holds the logic that caused the error.
-     * @param integer $window
-     *  The number of lines either side of the line where the error occurred
-     *  to show
-     * @return array
-     */
-    protected static function getNearbyLines($line, $file, $window = 5)
-    {
-        if (!file_exists($file)) {
-            return array();
-        }
-
-        return array_slice(file($file), ($line - 1) - $window, $window * 2, true);
-    }
-
-    /**
      * This function's goal is to validate the `$e` parameter in order to ensure
      * that the object is an `Exception` or a `Throwable` instance.
      * @since Symphony 2.7.0
@@ -125,7 +103,6 @@ class ExceptionHandler
         $output = '';
 
         try {
-
             // Validate the type, resolve to a 404 if not valid
             if (!static::isValidThrowable($e)) {
                 $e = new FrontendPageNotFoundException();
@@ -135,8 +112,10 @@ class ExceptionHandler
 
             if (class_exists("{$exception_type}Handler") && method_exists("{$exception_type}Handler", 'render')) {
                 $class = "{$exception_type}Handler";
+            } elseif (class_exists("{$exception_type}Renderer") && method_exists("{$exception_type}Renderer", 'render')) {
+                $class = "{$exception_type}Renderer";
             } else {
-                $class = __CLASS__;
+                $class = 'ExceptionRenderer';
             }
 
             // Exceptions should be logged if they are not caught.
@@ -144,7 +123,7 @@ class ExceptionHandler
                 self::$log->pushExceptionToLog($e, true);
             }
 
-            $output = call_user_func(array($class, 'render'), $e);
+            $output = call_user_func([$class, 'render'], $e);
 
         // If an exception was raised trying to render the exception, fall back
         // to the generic exception handler
@@ -209,108 +188,6 @@ class ExceptionHandler
     }
 
     /**
-     * Returns the path to the error-template by looking at the `WORKSPACE/template/`
-     * directory, then at the `TEMPLATES`  directory for the convention `*.tpl`. If
-     * the template is not found, `false` is returned
-     *
-     * @since Symphony 2.3
-     * @param string $name
-     *  Name of the template
-     * @return string|false
-     *  String, which is the path to the template if the template is found,
-     *  false otherwise
-     */
-    public static function getTemplate($name)
-    {
-        $format = '%s/%s.tpl';
-
-        if (!self::$enabled) {
-            if (!file_exists($template = sprintf($format, TEMPLATE, 'fatalerror.disabled'))) {
-                return false;
-            }
-            return $template;
-        }
-        else if (file_exists($template = sprintf($format, WORKSPACE . '/template', $name))) {
-            return $template;
-        }
-        else if (file_exists($template = sprintf($format, TEMPLATE, $name))) {
-            return $template;
-        }
-        else {
-            return false;
-        }
-    }
-
-    /**
-     * The render function will take an Throwable and output a HTML page
-     *
-     * @since Symphony 2.7.0
-     *  This function works with both Exception and Throwable
-     *
-     * @param Throwable $e
-     *  The Throwable object
-     * @return string
-     *  An HTML string
-     */
-    public static function render($e)
-    {
-        $lines = null;
-
-        foreach (self::getNearbyLines($e->getLine(), $e->getFile()) as $line => $string) {
-            $lines .= sprintf(
-                '<li%s><strong>%d</strong> <code>%s</code></li>',
-                (($line+1) == $e->getLine() ? ' class="error"' : null),
-                ++$line,
-                str_replace("\t", '&nbsp;&nbsp;&nbsp;&nbsp;', htmlspecialchars($string))
-            );
-        }
-
-        $trace = null;
-
-        foreach ($e->getTrace() as $t) {
-            $trace .= sprintf(
-                '<li><code><em>[%s:%d]</em></code></li><li><code>&#160;&#160;&#160;&#160;%s%s%s();</code></li>',
-                (isset($t['file']) ? $t['file'] : null),
-                (isset($t['line']) ? $t['line'] : null),
-                (isset($t['class']) ? $t['class'] : null),
-                (isset($t['type']) ? $t['type'] : null),
-                $t['function']
-            );
-        }
-
-        $queries = null;
-
-        if (is_object(Symphony::Database())) {
-            $debug = Symphony::Database()->getLogs();
-
-            if (!empty($debug)) {
-                foreach ($debug as $query) {
-                    $queries .= sprintf(
-                        '<li><em>[%01.4f]</em><code> %s;</code> </li>',
-                        (isset($query['execution_time']) ? $query['execution_time'] : null),
-                        htmlspecialchars($query['query'])
-                    );
-                }
-            }
-        }
-
-        return self::renderHtml(
-            'fatalerror.generic',
-            ($e instanceof ErrorException ? ErrorHandler::$errorTypeStrings[$e->getSeverity()] : 'Fatal Error'),
-            $e->getMessage() .
-                ($e->getPrevious()
-                    ? '<br />' . __('Previous exception: ') . $e->getPrevious()->getMessage()
-                    : ''
-                ),
-            $e->getFile(),
-            $e->getLine(),
-            $lines,
-            $trace,
-            $queries
-        );
-    }
-
-    /**
      * The shutdown function will capture any fatal errors and format them as a
      * usual Symphony page.
      *
@@ -348,7 +225,7 @@ class ExceptionHandler
                 ob_clean();
 
                 // Display the error message
-                echo self::renderHtml(
+                echo ExceptionRenderer::renderHtml(
                     'fatalerror.fatal',
                     'Fatal Error',
                     $message,
@@ -364,54 +241,5 @@ class ExceptionHandler
                 echo "</pre>";
             }
         }
-    }
-
-    /**
-     * This function will fetch the desired `$template`, and output the
-     * Throwable in a user friendly way.
-     *
-     * @since Symphony 2.4
-     * @since Symphony 2.6.4 the method is protected
-     * @param string $template
-     *  The template name, which should correspond to something in the TEMPLATE
-     *  directory, eg `fatalerror.fatal`.
-     *
-     * @since Symphony 2.7.0
-     *  This function works with both Exception and Throwable
-     *
-     * @param string $heading
-     * @param string $message
-     * @param string $file
-     * @param string $line
-     * @param string $lines
-     * @param string $trace
-     * @param string $queries
-     * @return string
-     *  The HTML of the formatted error message.
-     */
-    protected static function renderHtml($template, $heading, $message, $file = null, $line = null, $lines = null, $trace = null, $queries = null)
-    {
-        $html = sprintf(
-            file_get_contents(self::getTemplate($template)),
-            $heading,
-            !self::$enabled ? 'Something unexpected occurred.' : General::unwrapCDATA($message),
-            !self::$enabled ? '' : $file,
-            !self::$enabled ? '' : $line,
-            !self::$enabled ? null : $lines,
-            !self::$enabled ? null : $trace,
-            !self::$enabled ? null : $queries
-        );
-
-        $html = str_replace('{ASSETS_URL}', ASSETS_URL, $html);
-        $html = str_replace('{SYMPHONY_URL}', SYMPHONY_URL, $html);
-        $html = str_replace('{URL}', URL, $html);
-        $html = str_replace('{PHP}', PHP_VERSION, $html);
-        $html = str_replace(
-            '{MYSQL}',
-            !Symphony::Database() ? 'N/A' : Symphony::Database()->getVersion(),
-            $html
-        );
-
-        return $html;
     }
 }
