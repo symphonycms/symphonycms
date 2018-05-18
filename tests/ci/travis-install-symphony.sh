fb9841ee6d29be0616aa37bfd033b5b53fabe846
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-05-17 21:51:46 -0400

    Add echos for failures
    Picked from bfc7783aa5
    Picked from 842c69c811
    Picked from 2d0a712ef6

--- tests/ci/travis-install-symphony.sh
+++ tests/ci/travis-install-symphony.sh
@@ -3,6 +3,7 @@
 # test for redirect on install
 INDEX="$(curl --retry 0 -sI http://localhost:80/ | grep '302 Found')"
 if [ -z "$INDEX" ]; then
+    echo 'Failed to get redirected on index';
     exit 100;
 else
     echo 'Redirect to installer';
@@ -13,6 +14,7 @@ cp tests/ci/unattend.php manifest/
 # test for install http status
 INSTALL="$(curl --retry 0 -sI http://localhost:80/install/ | grep '200 OK')"
 if [ -z "$INSTALL" ]; then
+    echo 'Failed to load installer page';
     exit 200;
 else
     echo 'Install returned 200';
@@ -20,6 +22,7 @@ fi;
 # test install complete in the install log
 COMPLETED="$(grep "INSTALLATION COMPLETED" manifest/logs/install)"
 if [ -z "$COMPLETED" ]; then
+    echo 'Installation failed';
     exit 300;
 else
     echo 'INSTALLATION COMPLETED';
@@ -27,6 +30,7 @@ fi;
 # test index now returns a 404
 INDEXNOTFOUND="$(curl --retry 0 -sI http://localhost:80/ | grep '404 Not Found')"
 if [ -z "$INDEXNOTFOUND" ]; then
+    echo 'Failed to get a 404 on index';
     exit 404;
 else
     echo 'Index returned 404';
@@ -34,6 +38,7 @@ fi;
 # test admin login returns a 200
 ADMIN="$(curl --retry 0 -sI http://localhost:80/symphony/ | grep '200 OK')"
 if [ -z "$INDEXNOTFOUND" ]; then
+    echo 'Failed to load login page';
     exit 500;
 else
     echo 'Admin returns 200';
