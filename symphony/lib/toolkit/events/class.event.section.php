1f625d5105f9cfb11de7ace2c7c28c455aedbaf6
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-06-27 23:23:03 -0400

    Move EventMessages into its own file

--- symphony/lib/toolkit/events/class.event.section.php
+++ symphony/lib/toolkit/events/class.event.section.php
@@ -748,31 +748,3 @@ abstract class SectionEvent extends Event
         return $result;
     }
 }
-
-/**
- * Basic lookup class for Event messages, allows for frontend developers
- * to localise and change event messages without relying on string
- * comparision.
- *
- * @since Symphony 2.4
- */
-class EventMessages
-{
-    const UNKNOWN_ERROR = 0;
-
-    const ENTRY_CREATED_SUCCESS = 100;
-    const ENTRY_EDITED_SUCCESS = 101;
-    const ENTRY_ERRORS = 102;
-    const ENTRY_MISSING = 103;
-    const ENTRY_NOT_UNIQUE = 104;
-
-    const SECTION_MISSING = 200;
-
-    const FIELD_MISSING = 301;
-    const FIELD_INVALID = 302;
-    const FIELD_NOT_UNIQUE = 303;
-
-    const FILTER_FAILED = 400;
-
-    const SECURITY_XSRF = 500;
-}
