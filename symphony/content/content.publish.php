687390f4618d3802fc14653d3aecf35a75fce8a2
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-06-27 23:06:43 -0400

    Fix undefined $section_id variable

--- symphony/content/content.publish.php
+++ symphony/content/content.publish.php
@@ -1961,7 +1961,7 @@ class contentPublish extends AdministrationPage
                     $prepopulate = '';
                     $entry = (new EntryManager)
                         ->select($schema)
-                        ->section($section_id)
+                        ->section($section->get('id'))
                         ->entry($entry_id)
                         ->execute()
                         ->next();
