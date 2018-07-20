c45a31af00bf09aee217dcede6ff0f56464df739
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-07-20 15:15:56 -0400

    Add AuthorLogout delegate

--- symphony/content/content.logout.php
+++ symphony/content/content.logout.php
@@ -16,6 +16,26 @@ class contentLogout extends HTMLPage
     public function view()
     {
         Administration::instance()->logout();
-        redirect(URL);
+        $redirectUrl = URL;
+        /**
+         * A successful logout attempt into the Symphony backend
+         *
+         * @delegate AuthorLogout
+         * @since Symphony 3.0.0
+         * @param string $context
+         * '/logout/'
+         * @param integer $author_id
+         *  The ID of Author ID that is about to be deleted
+         * @param Author $author
+         *  The Author object.
+         * @param string $redirect_url
+         *  The url to which the author will be redirected
+         */
+        Symphony::ExtensionManager()->notifyMembers('AuthorLogout', '/logout/', [
+            'author' => Symphony::Author(),
+            'author_id' => Symphony::Author()->get('id'),
+            'redirect_url' => &$redirectUrl,
+        ]);
+        redirect($redirectUrl);
     }
 }
