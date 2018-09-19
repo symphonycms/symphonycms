symphonycms/symphonycms

commit a4b12aed99762f54c800ee1106e9aec28b65a42b

Update last_seen default value properly
This fix was missing from the 2.7.0 update

Re 1c074d23712ec5ed

Fixes #2594 for 3.0.0, since our requirement is to be at 2.7.0, we
cannot be sure that the 2.7.8 upgrade ran.

install/migrations/3.0.0.php
@@ -224,6 +224,17 @@ final class migration_300 extends Migration
             unset($activeTokenAuthors);
         }
 
+        // Update the default value for last_seen
+        // This change was made in 2.7.0, but the update process never took care of it
+        // Re: #2594
+        Symphony::Database()
+            ->alter('tbl_authors')
+            ->modify('last_seen', [
+                'type' => 'datetime',
+                'default' => '1000-01-01 00:00:00',
+            ])
+            ->execute();
+
         // Re-save all data sources
         foreach (DatasourceManager::listAll() as $ds) {
             if (!$ds['can_parse']) {

commit 20ae0fdb1b3e37d6145925e86fc7d3765e55832c

Fix broken updater
Was broken in a4b12aed99

install/migrations/3.0.0.php
@@ -134,7 +134,7 @@ final class migration_300 extends Migration
         // Make parent_section unsigned and sortorder signed
         Symphony::Database()
             ->alter('tbl_fields')
-            ->modify( ['parent_section' => [
+            ->modify(['parent_section' => [
                 'type' => 'int(11)',
                 'signed' => false,
             ]])
@@ -229,10 +229,10 @@ final class migration_300 extends Migration
         // Re: #2594
         Symphony::Database()
             ->alter('tbl_authors')
-            ->modify('last_seen', [
+            ->modify(['last_seen' => [
                 'type' => 'datetime',
                 'default' => '1000-01-01 00:00:00',
-            ])
+            ]])
             ->execute();
 
         // Re-save all data sources