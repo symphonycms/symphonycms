17ac18a94e7bb5c165eea8b71ca5191303b69fa6
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-10-09 13:20:45 -0400

    Fix error introduced by code rewrite
    next() always returns an entry object or null, never an array.
    
    This problem was introduced in 154c161b39e52236054fc2bd656ecad428743de6
    Re #2659
    
    Picked from 119f3fe630
    Picked from d9992e996f

--- symphony/content/content.publish.php
+++ symphony/content/content.publish.php
@@ -1667,9 +1667,8 @@ class contentPublish extends AdministrationPage
 
                 redirect(SYMPHONY_URL . '/publish/'.$this->_context['section_handle'].'/');
             } else {
-                $ret = (new EntryManager)->select()->entry($entry_id)->execute()->next();
-                if (!empty($ret)) {
-                    $entry = $ret[0];
+                $entry = (new EntryManager)->select()->entry($entry_id)->execute()->next();
+                if (!empty($entry)) {
                     $this->addTimestampValidationPageAlert($this->_errors['timestamp'], $entry, 'delete');
                 }
             }
