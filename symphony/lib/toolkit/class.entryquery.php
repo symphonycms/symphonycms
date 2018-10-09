symphonycms/symphonycms

commit 01be59c91e2d0f10b4eecbacce9e7a47a6db5472

Fix mixup about field id vs name filtering
- We first need to try to convert the $field into a int, then check if it
is a string or an object. Comments were right, not the actual code(!).

- Standardize the $field value to be used in error messages.

- Fix the maybe wrong usage of $fields vs $f

symphony/lib/toolkit/class.entryquery.php
@@ -436,16 +436,23 @@ class EntryQuery extends DatabaseQuery
             return $this->where($conditions);
 
         // Handle when the filter field is a field id
-        } elseif (is_string($field)) {
+        } elseif (General::intval($field) > 0) {
             $f = (new FieldManager)->select()->field($field)->execute()->next();
+            if ($f) {
+                $field = $f->name();
+            }
 
         // Handle when the filter field is a field name
-        } elseif (General::intval($field) > 0) {
+        } elseif (is_string($field)) {
             $f = (new FieldManager)->select()->name($field)->execute()->next();
+            if ($f) {
+                $field = $f->name();
+            }
 
         // Handle when the filter field is a field object
         } elseif ($field instanceof Field) {
             $f = $field;
+            $field = $f->name();
         }
 
         if (!$f) {
@@ -460,7 +467,7 @@ class EntryQuery extends DatabaseQuery
             $this->distinct();
         }
 
-        $field->getEntryQueryFieldAdapter()->filter($this, $values, $operator);
+        $f->getEntryQueryFieldAdapter()->filter($this, $values, $operator);
 
         return $this;
     }

commit d4ee4bb98ed92dc8748781a63188d9516eef8415

Fix broken sort by id, when id is a string
This restore sorting in the backend.

Bug introduced in 01be59c91e2d0f10b4eecbacce9e7a47a6db5472

symphony/lib/toolkit/class.entryquery.php
@@ -522,7 +522,7 @@ class EntryQuery extends DatabaseQuery
 
         // Handle either by id or by handle when the sort field is an actual Field
         } elseif (!empty($field)) {
-            if (is_string($field)) {
+            if (is_string($field) && General::intval($field) === -1) {
                 if (!$this->sectionId) {
                     throw new DatabaseStatementException('Can not sort with a field name without a section');
                 }