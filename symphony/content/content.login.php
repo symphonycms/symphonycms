symphonycms/symphonycms

commit 8ec158b362ab9d2e7fa92fd792dc6ee2eb96b6c5

Move the logic to parsing the context
* From the Administration class to the actual pages themselves
Picked from 6c11395ce897c6dab7b1a5f34d523b965667eb9f
* Moved the context parsing to the login page
Picked from d496f6e0658c635ecd44cee9bf0262720b40726b

Re 7757b69eb0ae78e671045b36aa10432844242f51

symphony/content/content.login.php
@@ -46,6 +46,32 @@ class contentLogin extends HTMLPage
         // Prevent stylesheet injection by extensions
     }
 
+    /**
+     * The Sections page has /action/id/flag/ context.
+     * eg. /edit/1/saved/
+     *
+     * @param array $context
+     * @param array $parts
+     * @return array
+     */
+    public function parseContext(array &$context, array $parts)
+    {
+        if (isset($parts[1])) {
+            if ($parts[1] === 'retrieve-password') {
+                $context['action'] = $parts[1];
+            } else {
+                $context['token'] = $parts[1];
+            }
+        }
+        if (empty($context['action'])) {
+            if (isset($parts[0])) {
+                $context['action'] = $parts[0];
+            } else {
+                $context['action'] = 'login';
+            }
+        }
+    }
+
     public function build($context = null)
     {
         if ($context) {
@@ -61,15 +87,15 @@ class contentLogin extends HTMLPage
 
     public function view()
     {
-        if (isset($this->_context[1]) && $this->_context[0] === 'reset-password') {
-            if (Administration::instance()->loginFromToken($this->_context[1])) {
+        if (isset($this->_context['token']) && $this->_context['action'] === 'reset-password') {
+            if (Administration::instance()->loginFromToken($this->_context['token'])) {
                 if (Administration::instance()->isLoggedIn()) {
                     // Redirect to the Author's profile. RE: #1801
                     redirect(SYMPHONY_URL . '/system/authors/edit/' . Symphony::Author()->get('id') . '/reset-password/');
                 }
             }
-        } elseif (isset($this->_context[0])) {
-            if (Administration::instance()->loginFromToken($this->_context[0])) {
+        } elseif (isset($this->_context['token']) && $this->_context['action'] === 'login') {
+            if (Administration::instance()->loginFromToken($this->_context['token'])) {
                 if (Administration::instance()->isLoggedIn()) {
                     // Regular token-based login
                     redirect(SYMPHONY_URL . '/');
@@ -84,7 +110,7 @@ class contentLogin extends HTMLPage
         $fieldset = new XMLElement('fieldset');
 
         // Display retrieve password UI
-        if (isset($this->_context[0]) && $this->_context[0] == 'retrieve-password') {
+        if ($this->_context['action'] == 'retrieve-password') {
             $this->Form->setAttribute('action', SYMPHONY_URL.'/login/retrieve-password/');
 
             // Successful reset

symphony/content/content.blueprintsdatasources.php
@@ -42,10 +42,10 @@ class contentBlueprintsDatasources extends ResourcesPage
             );
 
             // These alerts are only valid if the form doesn't have errors
-        } elseif (isset($this->_context[2])) {
+        } elseif (isset($this->_context['flag'])) {
             $time = Widget::Time();
 
-            switch ($this->_context[2]) {
+            switch ($this->_context['flag']) {
                 case 'saved':
                     $message = __('Data Source updated at %s.', array($time->generate()));
                     break;
@@ -66,9 +66,9 @@ class contentBlueprintsDatasources extends ResourcesPage
 
         $providers = Symphony::ExtensionManager()->getProvidersOf(iProvider::DATASOURCE);
         $canonical_link = null;
-        $isEditing = false;
+        $isEditing = $this->_context['action'] === 'edit';
         $about = $handle = null;
-        $fields = array(
+        $fields = [
             'name' => null,
             'source' => null,
             'filter'=> null,
@@ -76,7 +76,7 @@ class contentBlueprintsDatasources extends ResourcesPage
             'negate_url_param' => null,
             'param' => null,
             'paramxml' => null,
-        );
+        ];
 
         if (isset($_POST['fields'])) {
             $fields = $_POST['fields'];
@@ -99,12 +99,8 @@ class contentBlueprintsDatasources extends ResourcesPage
                 $fields['xml_elements'] = array();
             }
 
-            if ($this->_context[0] == 'edit') {
-                $isEditing = true;
-            }
-        } elseif ($this->_context[0] == 'edit') {
-            $isEditing = true;
-            $handle = $this->_context[1];
+        } elseif ($isEditing) {
+            $handle = $this->_context['handle'];
             $existing = DatasourceManager::create($handle, array(), false);
             $order = isset($existing->dsParamORDER) ? stripslashes($existing->dsParamORDER) : 'asc';
             $canonical_link = '/blueprints/datasources/edit/' . $handle . '/';
@@ -191,7 +187,14 @@ class contentBlueprintsDatasources extends ResourcesPage
         }
 
         $this->setPageType('form');
-        $this->setTitle(__(($isEditing ? '%1$s &ndash; %2$s &ndash; %3$s' : '%2$s &ndash; %3$s'), array($name, __('Data Sources'), __('Symphony'))));
+        $this->setTitle(
+            __(
+                $isEditing
+                    ? '%1$s &ndash; %2$s &ndash; %3$s'
+                    : '%2$s &ndash; %3$s',
+                [$name, __('Data Sources'), __('Symphony')]
+            )
+        );
         if ($canonical_link) {
             $this->addElementToHead(new XMLElement('link', null, array(
                 'rel' => 'canonical',
@@ -857,7 +860,7 @@ class contentBlueprintsDatasources extends ResourcesPage
 
         // Parameters
         $label = Widget::Label(__('Output Parameters'));
-        $prefix = '$ds-' . (isset($this->_context[1]) ? Lang::createHandle($fields['name']) : __('untitled')) . '.';
+        $prefix = '$ds-' . (isset($this->_context['handle']) ? Lang::createHandle($fields['name']) : __('untitled')) . '.';
 
         $options = array(
             array('label' => __('Authors'), 'data-label' => 'authors', 'options' => array())
@@ -1024,11 +1027,11 @@ class contentBlueprintsDatasources extends ResourcesPage
     {
         $this->setPageType('form');
 
-        $datasource = DatasourceManager::create($this->_context[1], array(), false);
+        $datasource = DatasourceManager::create($this->_context['handle'], array(), false);
         $about = General::array_map_recursive('stripslashes', $datasource->about());
 
         $this->setTitle(__('%1$s &ndash; %2$s &ndash; %3$s', array($about['name'], __('Data Source'), __('Symphony'))));
-        $this->appendSubheading((($this->_context[0] == 'info') ? $about['name'] : __('Untitled')));
+        $this->appendSubheading((($this->_context['action'] === 'info') ? $about['name'] : __('Untitled')));
         $this->insertBreadcrumbs(array(
             Widget::Anchor(__('Data Sources'), SYMPHONY_URL . '/blueprints/datasources/'),
         ));
@@ -1056,7 +1059,7 @@ class contentBlueprintsDatasources extends ResourcesPage
                 case 'version':
                     $fieldset = new XMLElement('fieldset');
                     $fieldset->appendChild(new XMLElement('legend', __('Version')));
-                    $release_date = array_key_exists('release-date', $about) ? $about['release-date'] : filemtime(DatasourceManager::__getDriverPath($this->_context[1]));
+                    $release_date = array_key_exists('release-date', $about) ? $about['release-date'] : filemtime(DatasourceManager::__getDriverPath($this->_context['handle']));
 
                     if (preg_match('/^\d+(\.\d+)*$/', $value)) {
                         $fieldset->appendChild(new XMLElement('p', __('%1$s released on %2$s', array($value, DateTimeObj::format($release_date, __SYM_DATE_FORMAT__)))));
@@ -1094,7 +1097,7 @@ class contentBlueprintsDatasources extends ResourcesPage
         }
 
         // Display source
-        $file = DatasourceManager::__getClassPath($this->_context[1]) . '/data.' . $this->_context[1] . '.php';
+        $file = DatasourceManager::__getClassPath($this->_context['handle']) . '/data.' . $this->_context['handle'] . '.php';
 
         if (file_exists($file)) {
             $fieldset = new XMLElement('fieldset');
@@ -1135,21 +1138,21 @@ class contentBlueprintsDatasources extends ResourcesPage
              *  The handle of the Datasource
              */
             Symphony::ExtensionManager()->notifyMembers('DatasourcePreDelete', '/blueprints/datasources/', array(
-                'file' => DATASOURCES . "/data." . $this->_context[1] . ".php",
-                'handle' => $this->_context[1],
+                'file' => DATASOURCES . "/data." . $this->_context['handle'] . ".php",
+                'handle' => $this->_context['handle'],
             ));
 
-            if (!General::deleteFile(DATASOURCES . '/data.' . $this->_context[1] . '.php')) {
+            if (!General::deleteFile(DATASOURCES . '/data.' . $this->_context['handle'] . '.php')) {
                 $this->pageAlert(
-                    __('Failed to delete %s.', array('<code>' . $this->_context[1] . '</code>'))
+                    __('Failed to delete %s.', array('<code>' . $this->_context['handle'] . '</code>'))
                     . ' ' . __('Please check permissions on %s.', array('<code>/workspace/data-sources</code>')),
                     Alert::ERROR
                 );
             } else {
-                $pages = ResourceManager::getAttachedPages(ResourceManager::RESOURCE_TYPE_DS, $this->_context[1]);
+                $pages = ResourceManager::getAttachedPages(ResourceManager::RESOURCE_TYPE_DS, $this->_context['handle']);
 
                 foreach ($pages as $page) {
-                    ResourceManager::detach(ResourceManager::RESOURCE_TYPE_DS, $this->_context[1], $page['id']);
+                    ResourceManager::detach(ResourceManager::RESOURCE_TYPE_DS, $this->_context['handle'], $page['id']);
                 }
 
                 /**
@@ -1165,8 +1168,8 @@ class contentBlueprintsDatasources extends ResourcesPage
                  *  The handle of the Datasource
                  */
                 Symphony::ExtensionManager()->notifyMembers('DatasourcePostDelete', '/blueprints/datasources/', array(
-                    'file' => DATASOURCES . "/data." . $this->_context[1] . ".php",
-                    'handle' => $this->_context[1],
+                    'file' => DATASOURCES . "/data." . $this->_context['handle'] . ".php",
+                    'handle' => $this->_context['handle'],
                 ));
 
                 redirect(SYMPHONY_URL . '/blueprints/datasources/');
@@ -1250,10 +1253,10 @@ class contentBlueprintsDatasources extends ResourcesPage
         $isDuplicate = false;
         $queueForDeletion = null;
 
-        if ($this->_context[0] == 'new' && is_file($file)) {
+        if ($this->_context['action'] === 'new' && is_file($file)) {
             $isDuplicate = true;
-        } elseif ($this->_context[0] == 'edit') {
-            $existing_handle = $this->_context[1];
+        } elseif ($this->_context['action'] === 'edit') {
+            $existing_handle = $this->_context['handle'];
 
             if ($classname != $existing_handle && is_file($file)) {
                 $isDuplicate = true;
@@ -1396,7 +1399,7 @@ class contentBlueprintsDatasources extends ResourcesPage
                 $dsShell = str_replace('<!-- SOURCE -->', addslashes($source), $dsShell);
             }
 
-            if ($this->_context[0] == 'new') {
+            if ($this->_context['action'] === 'new') {
                 /**
                  * Prior to creating the Datasource, the file path where it will be written to
                  * is provided and well as the contents of that file.
@@ -1507,7 +1510,7 @@ class contentBlueprintsDatasources extends ResourcesPage
                     }
                 }
 
-                if ($this->_context[0] == 'new') {
+                if ($this->_context['action'] === 'new') {
                     /**
                      * After creating the Datasource, the path to the Datasource file is provided
                      *
@@ -1542,7 +1545,7 @@ class contentBlueprintsDatasources extends ResourcesPage
                     ));
                 }
 
-                redirect(SYMPHONY_URL . '/blueprints/datasources/edit/'.$classname.'/'.($this->_context[0] == 'new' ? 'created' : 'saved') . '/');
+                redirect(SYMPHONY_URL . '/blueprints/datasources/edit/'.$classname.'/'.($this->_context['action'] === 'new' ? 'created' : 'saved') . '/');
             }
         }
     }

symphony/content/content.blueprintsevents.php
@@ -46,10 +46,10 @@ class contentBlueprintsEvents extends ResourcesPage
             );
 
             // These alerts are only valid if the form doesn't have errors
-        } elseif (isset($this->_context[2])) {
+        } elseif (isset($this->_context['flag'])) {
             $time = Widget::Time();
 
-            switch ($this->_context[2]) {
+            switch ($this->_context['flag']) {
                 case 'saved':
                     $message = __('Event updated at %s.', array($time->generate()));
                     break;
@@ -77,16 +77,16 @@ class contentBlueprintsEvents extends ResourcesPage
         if (isset($_POST['fields'])) {
             $fields = $_POST['fields'];
 
-            if ($this->_context[0] == 'edit') {
+            if ($this->_context['action'] === 'edit') {
                 $isEditing = true;
             }
-        } elseif ($this->_context[0] == 'edit' || $this->_context[0] == 'info') {
+        } elseif ($this->_context['action'] === 'edit' || $this->_context['action'] === 'info') {
             $isEditing = true;
-            $handle = $this->_context[1];
+            $handle = $this->_context['handle'];
             $existing = EventManager::create($handle);
             $about = General::array_map_recursive('stripslashes', $existing->about());
 
-            if ($this->_context[0] == 'edit' && !$existing->allowEditorToParse()) {
+            if ($this->_context['action'] === 'edit' && !$existing->allowEditorToParse()) {
                 redirect(SYMPHONY_URL . '/blueprints/events/info/' . $handle . '/');
             }
 
@@ -110,7 +110,7 @@ class contentBlueprintsEvents extends ResourcesPage
                 }
             }
 
-            $canonical_link = '/blueprints/events/' . $this->_context[0] . '/' . $handle . '/';
+            $canonical_link = '/blueprints/events/' . $this->_context['action'] . '/' . $handle . '/';
         }
 
         // Handle name on edited changes, or from reading an edited datasource
@@ -121,7 +121,14 @@ class contentBlueprintsEvents extends ResourcesPage
         }
 
         $this->setPageType('form');
-        $this->setTitle(__(($isEditing ? '%1$s &ndash; %2$s &ndash; %3$s' : '%2$s &ndash; %3$s'), array($name, __('Events'), __('Symphony'))));
+        $this->setTitle(
+            __(
+                $isEditing
+                    ? '%1$s &ndash; %2$s &ndash; %3$s'
+                    : '%2$s &ndash; %3$s',
+                [$name, __('Events'), __('Symphony')]
+            )
+        );
         if ($canonical_link) {
             $this->addElementToHead(new XMLElement('link', null, array(
                 'rel' => 'canonical',
@@ -230,10 +237,14 @@ class contentBlueprintsEvents extends ResourcesPage
              * @param array $options
              *  An array of all the filters that are available, passed by reference
              */
-            Symphony::ExtensionManager()->notifyMembers('AppendEventFilter', '/blueprints/events/' . $this->_context[0] . '/', array(
-                'selected' => $filters,
-                'options' => &$options
-            ));
+            Symphony::ExtensionManager()->notifyMembers(
+                'AppendEventFilter',
+                '/blueprints/events/' . $this->_context['action'] . '/',
+                [
+                    'selected' => $filters,
+                    'options' => &$options,
+                ]
+            );
 
             $fieldset->appendChild(Widget::Select('fields[filters][]', $options, array('multiple' => 'multiple', 'id' => 'event-filters')));
             $this->Form->appendChild($fieldset);
@@ -379,22 +390,33 @@ class contentBlueprintsEvents extends ResourcesPage
              *  @since Symphony 3.0.0
              *  The handle of the Event
              */
-            Symphony::ExtensionManager()->notifyMembers('EventPreDelete', '/blueprints/events/', array(
-                'file' => EVENTS . "/event." . $this->_context[1] . ".php",
-                'handle' => $this->_context[1],
-            ));
+            Symphony::ExtensionManager()->notifyMembers(
+                'EventPreDelete',
+                '/blueprints/events/',
+                [
+                    'file' => EVENTS . "/event." . $this->_context['handle'] . ".php",
+                    'handle' => $this->_context['handle'],
+                ]
+            );
 
-            if (!General::deleteFile(EVENTS . '/event.' . $this->_context[1] . '.php')) {
+            if (!General::deleteFile(EVENTS . '/event.' . $this->_context['handle'] . '.php')) {
                 $this->pageAlert(
-                    __('Failed to delete %s.', array('<code>' . $this->_context[1] . '</code>'))
+                    __('Failed to delete %s.', array('<code>' . $this->_context['handle'] . '</code>'))
                     . ' ' . __('Please check permissions on %s.', array('<code>/workspace/events</code>')),
                     Alert::ERROR
                 );
             } else {
-                $pages = ResourceManager::getAttachedPages(ResourceManager::RESOURCE_TYPE_EVENT, $this->_context[1]);
+                $pages = ResourceManager::getAttachedPages(
+                    ResourceManager::RESOURCE_TYPE_EVENT,
+                    $this->_context['handle']
+                );
 
                 foreach ($pages as $page) {
-                    ResourceManager::detach(ResourceManager::RESOURCE_TYPE_EVENT, $this->_context[1], $page['id']);
+                    ResourceManager::detach(
+                        ResourceManager::RESOURCE_TYPE_EVENT,
+                        $this->_context['handle'],
+                        $page['id']
+                    );
                 }
 
                 /**
@@ -409,10 +431,14 @@ class contentBlueprintsEvents extends ResourcesPage
                  * @param string $handle
                  *  The handle of the Event
                  */
-                Symphony::ExtensionManager()->notifyMembers('EventPostDelete', '/blueprints/events/', array(
-                    'file' => EVENTS . "/event." . $this->_context[1] . ".php",
-                    'handle' => $this->_context[1],
-                ));
+                Symphony::ExtensionManager()->notifyMembers(
+                    'EventPostDelete',
+                    '/blueprints/events/',
+                    [
+                        'file' => EVENTS . "/event." . $this->_context['handle'] . ".php",
+                        'handle' => $this->_context['handle'],
+                    ]
+                );
 
                 redirect(SYMPHONY_URL . '/blueprints/events/');
             }
@@ -468,10 +494,10 @@ class contentBlueprintsEvents extends ResourcesPage
         $isDuplicate = false;
         $queueForDeletion = null;
 
-        if ($this->_context[0] == 'new' && is_file($file)) {
+        if ($this->_context['action'] === 'new' && is_file($file)) {
             $isDuplicate = true;
-        } elseif ($this->_context[0] == 'edit') {
-            $existing_handle = $this->_context[1];
+        } elseif ($this->_context['action'] === 'edit') {
+            $existing_handle = $this->_context['handle'];
 
             if ($classname != $existing_handle && is_file($file)) {
                 $isDuplicate = true;
@@ -569,7 +595,7 @@ class contentBlueprintsEvents extends ResourcesPage
             // Remove left over placeholders
             $eventShell = preg_replace(array('/<!--[\w ]++-->/'), '', $eventShell);
 
-            if ($this->_context[0] == 'new') {
+            if ($this->_context['action'] === 'new') {
                 /**
                  * Prior to creating an Event, the file path where it will be written to
                  * is provided and well as the contents of that file.
@@ -648,7 +674,7 @@ class contentBlueprintsEvents extends ResourcesPage
                     }
                 }
 
-                if ($this->_context[0] == 'new') {
+                if ($this->_context['action'] === 'new') {
                     /**
                      * After creating the Event, the path to the Event file is provided
                      *
@@ -683,7 +709,7 @@ class contentBlueprintsEvents extends ResourcesPage
                     ));
                 }
 
-                redirect(SYMPHONY_URL . '/blueprints/events/edit/'.$classname.'/'.($this->_context[0] == 'new' ? 'created' : 'saved') . '/');
+                redirect(SYMPHONY_URL . '/blueprints/events/edit/'.$classname.'/'.($this->_context['action'] === 'new' ? 'created' : 'saved') . '/');
             }
         }
     }

commit 9f487a4c80d7b2a449e09099fb6c920ecd01a645

Fix url parameter parsing and reset password url
Broken in 8ec158b362ab9d2e7fa92fd792dc6ee2eb96b6c5

symphony/content/content.login.php
@@ -12,7 +12,7 @@
 class contentLogin extends HTMLPage
 {
     public $failedLoginAttempt = false;
-    private $_email_sent = false;
+    private $_email_sent;
     private $_email_error;
     private $_email_sent_to;
 
@@ -50,8 +50,11 @@ class contentLogin extends HTMLPage
     }
 
     /**
-     * The Sections page has /action/id/flag/ context.
-     * eg. /edit/1/saved/
+     * The Login page has /action/sub-action/token/ context.
+     * /login/
+     * /login/retrieve-password/
+     * /login/{$token}/
+     * /login/{$token}/reset-password/
      *
      * @param array $context
      * @param array $parts
@@ -67,10 +70,10 @@ class contentLogin extends HTMLPage
             }
         }
         if (empty($context['action'])) {
-            if (isset($parts[0])) {
-                $context['action'] = $parts[0];
+            if (isset($parts[2])) {
+                $context['action'] = $parts[2];
             } else {
-                $context['action'] = 'login';
+                $context['action'] = $parts[0]; // should always be login
             }
         }
     }
@@ -99,6 +102,8 @@ class contentLogin extends HTMLPage
                     redirect(SYMPHONY_URL . '/system/authors/edit/' . Symphony::Author()->get('id') . '/reset-password/');
                 }
             }
+            // Somehow, the login failed...
+            redirect(SYMPHONY_URL . '/login/');
         } elseif (isset($this->_context['token']) && $this->_context['action'] === 'login') {
             if (Administration::instance()->loginFromToken($this->_context['token'])) {
                 if (Administration::instance()->isLoggedIn()) {
@@ -106,6 +111,9 @@ class contentLogin extends HTMLPage
                     redirect(SYMPHONY_URL . '/');
                 }
             }
+        } elseif (isset($this->_context['token'])) {
+            // Token with invalid action
+            redirect(SYMPHONY_URL . '/login/');
         }
 
         $this->Form = Widget::Form(SYMPHONY_URL . '/login/', 'post');
@@ -133,7 +141,7 @@ class contentLogin extends HTMLPage
                 $label = Widget::Label(__('Email Address or Username'));
                 $label->appendChild(Widget::Input('email', General::sanitize($_POST['email']), 'text', array('autofocus' => 'autofocus')));
 
-                if (!$this->_email_sent) {
+                if ($this->_email_sent === false) {
                     $label = Widget::Error($label, __('Unfortunately no account was found using this information.'));
                 } else {
                     // Email exception
@@ -314,7 +322,7 @@ class contentLogin extends HTMLPage
                         $email->subject = __('New Symphony Account Password');
                         $email->text_plain = __('Hi %s,', array($author['first_name'])) . PHP_EOL .
                                 __('A new password has been requested for your account. Login using the following link, and change your password via the Authors area:') . PHP_EOL .
-                                PHP_EOL . '    ' . SYMPHONY_URL . "/login/reset-password/{$token}/" . PHP_EOL . PHP_EOL .
+                                PHP_EOL . '    ' . SYMPHONY_URL . "/login/{$token}/reset-password/" . PHP_EOL . PHP_EOL .
                                 __('It will expire in 2 hours. If you did not ask for a new password, please disregard this email.') . PHP_EOL . PHP_EOL .
                                 __('Best Regards,') . PHP_EOL .
                                 __('The Symphony Team');