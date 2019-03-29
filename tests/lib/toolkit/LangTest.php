535e9eda759ec08649202b66d1c646bec0aa2460
Nicolas Brassard <nitriques@users.noreply.github.com>
2019-03-29 14:38:51 -0400

    Fix broken non-breaking space replacement
    475c198acd (#2631) added support for non-breaking space, but github
    editor converted it to a regular space.
    
    This breaks with the new way we create handles so we need to convert
    non-breaking space into an actual breaking (regular) space to allow
    custom delimiter to work.
    
    The commit also ports all createHandle tests from GeneralTest which
    should continue to work + adds transliterations tests
    
    Picked from 988fe33302
    Picked from 7a582dff32

---
+++ tests/lib/toolkit/LangTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Symphony\DAL\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Lang
+ */
+final class LangTest extends TestCase
+{
+    /*
+     * \Lang::createHandle() tests
+     */
+    public function testCreateHandleDefaultValues()
+    {
+        $this->assertEquals('test', \Lang::createHandle('test'));
+        $this->assertEquals('this-is-a-test', \Lang::createHandle('This is a test'));
+        $this->assertEquals('this-is-a-test', \Lang::createHandle('This    is---a    test'));
+        //$this->assertEquals('this-is-a-pooh-emoji', \Lang::createHandle("This is a 	\xd8\x3d\xdc\xa9 pooh emoji"));
+        $this->assertEquals('this-is-a-test-test', \Lang::createHandle('- This ,   is-,-a. ! test   test '));
+    }
+
+    public function testCreateLoooooooongHandleDefaultValues()
+    {
+        $longHandle = \Lang::createHandle('TThis is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test');
+        $this->assertEquals(
+            'tthis-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test',
+            $longHandle
+        );
+        $this->assertEquals(255, strlen($longHandle));
+
+        $longHandleWithDelimAtThenEnd = \Lang::createHandle('This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test-This is a test');
+        $this->assertEquals(
+            'this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test-this-is-a-test',
+            $longHandleWithDelimAtThenEnd
+        );
+        $this->assertEquals(254, strlen($longHandleWithDelimAtThenEnd));
+    }
+
+    public function testCreateShortHandleShortLimit()
+    {
+        $this->assertEquals('r', \Lang::createHandle('r', 20));
+        $this->assertEquals('this', \Lang::createHandle('This is a test', 5));
+        $this->assertEquals('this-is', \Lang::createHandle('This    is---a    test', 7));
+    }
+
+    public function testCreateHandleCustomDelimiter()
+    {
+        $this->assertEquals('this_is_a', \Lang::createHandle('This is a test', 10, '_'));
+        $this->assertEquals('this@is', \Lang::createHandle('This    is---a    test', 7, '@'));
+        $this->assertEquals('this&is', \Lang::createHandle('This    is---a    test', 7, '&'));
+    }
+
+
+    public function testCreateHandleUriEncode()
+    {
+        $this->assertEquals('this_is_a', \Lang::createHandle('This is a test', 10, '_', true));
+        $this->assertEquals('this+is+a', \Lang::createHandle('This is a test', 10, ' ', true));
+        $this->assertEquals('this%2bis%2ba', \Lang::createHandle('This is a test', 10, '+', true));
+    }
+
+    public function testCreateHandleCustomRules()
+    {
+        $this->assertEquals('that+is+a+', \Lang::createHandle('This is a test', 10, '_', false, false, [
+            '/_/' => '+',
+            '/this/i' => 'That',
+        ]));
+    }
+
+    public function testCreateHandleWithTransliterations()
+    {
+        $this->assertEquals('this_is_a', \Lang::createHandle('This is a test', 10, '_', false, true));
+        $this->assertEquals('this_is_a', \Lang::createHandle('This ïs à test', 10, '_', false, true));
+        $this->assertEquals('this-is-and-test', \Lang::createHandle('This is & test', 17, '-', false, true));
+        // Non-breaking space
+        $this->assertEquals('this_is_a', \Lang::createHandle('This is a test', 10, '_', false, true));
+        $this->assertEquals('this_is_a', \Lang::createHandle('This is a test', 10, '_', false, false));
+    }
+}
