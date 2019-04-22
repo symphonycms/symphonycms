1431e8829bae3072ffa02aab9048608d44113ed5
Nicolas Brassard <nitriques@users.noreply.github.com>
2019-04-22 17:54:37 -0400

    Add EntryQueryIntegrationTest

---
+++ tests/int/lib/toolkit/EntryQueryIntegrationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require './tests/lib/toolkit/EntryQueryTest.php';
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers EntryQuery
+ */
+final class EntryQueryIntegrationTest extends EntryQuery
+{
+    private $db;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->db = Symphony::Database();
+    }
+
+    /**
+     * @expectedException DatabaseStatementException
+     */
+    public function testSortInvalid()
+    {
+        $q = (new \EntryQuery($this->db))->sort('<invalid>');
+        $this->assertEquals(
+            "SELECT SQL_NO_CACHE FROM `entries` AS `e` ORDER BY `e`.`id` ASC",
+            $q->generateSQL(),
+            'new EntryQuery with ->sort(<invalid>)'
+        );
+        $values = $q->getValues();
+        $this->assertEquals(0, count($values), '0 value');
+    }
+}
