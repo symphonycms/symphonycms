c9dd8cbe4e7269f312edbfef0665b1c6f0ea93ef
Nicolas Brassard <nitriques@users.noreply.github.com>
2020-04-24 16:46:14 -0400

    (ci) Test if the bug is in coverage

--- tests/lib/boot/UtilitiesTest.php
+++ tests/lib/boot/UtilitiesTest.php
@@ -5,7 +5,7 @@ namespace Symphony\Boot\Tests;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \
+ * @covers func.utilities.php
  */
 final class UtilitiesTest extends TestCase
 {
@@ -14,8 +14,7 @@ final class UtilitiesTest extends TestCase
         $utf8 = 'accent-aigu-é.com';
         $idn = 'xn--accent-aigu--meb.com';
         $test = idn_to_utf8_safe($idn);
-        //$this->assertContains($test, [$utf8, $idn], 'test is either result or the original');
-        var_dump($test);
+        //$this->assertContains($test, [$utf8, $idn], 'test is either utf8 or the original');
         $this->assertTrue(
             $test === $utf8 || $test === $idn,
             'test is either utf8 or the original'
@@ -27,8 +26,7 @@ final class UtilitiesTest extends TestCase
         $utf8 = 'accent-aigu-é.com';
         $idn = 'xn--accent-aigu--meb.com';
         $test = idn_to_ascii_safe($utf8);
-        //$this->assertContains($test, [$utf8, $idn], 'test is either result or the original');
-        var_dump($test);
+        //$this->assertContains($test, [$utf8, $idn], 'test is either idn or the original');
         $this->assertTrue(
             $test === $utf8 || $test === $idn,
             'test is either idn or the original'
